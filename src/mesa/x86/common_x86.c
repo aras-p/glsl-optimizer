d8aec9b55b0180384e96cddff48fc48751bbc320
Brian Paul <brian.paul@tungstengraphics.com>
2000-01-25 17:04:47 +0000

    only print info if MESA_DEBUG is set

--- src/mesa/x86/common_x86.c
+++ src/mesa/x86/common_x86.c
@@ -1,9 +1,10 @@
+/* $Id: common_x86.c,v 1.6 2000/01/25 17:04:47 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
- * Version:  3.1
+ * Version:  3.3
  * 
- * Copyright (C) 1999  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2000  Brian Paul   All Rights Reserved.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -39,10 +40,15 @@
 
 int gl_x86_cpu_features = 0;
 
+static void message(const char *msg)
+{
+   if (getenv("MESA_DEBUG"))
+      fprintf(stderr, "%s\n", msg);
+}
+
 
 void gl_init_all_x86_asm (void)
 {
-
 #ifdef USE_X86_ASM
    gl_x86_cpu_features = gl_identify_x86_cpu_features ();
    gl_x86_cpu_features |= GL_CPU_AnyX86;
@@ -51,29 +57,30 @@ void gl_init_all_x86_asm (void)
       gl_x86_cpu_features = 0;
 
    if (gl_x86_cpu_features & GL_CPU_GenuineIntel) {
-      fprintf (stderr, "GenuineIntel cpu detected.\n");
+      message("GenuineIntel cpu detected.");
    }
 
    if (gl_x86_cpu_features) {
-     gl_init_x86_asm_transforms ();
+      gl_init_x86_asm_transforms ();
    }
 
 #ifdef USE_MMX_ASM
    if (gl_x86_cpu_features & GL_CPU_MMX) {
       char *s = getenv( "MESA_NO_MMX" );
       if (s == NULL) { 
-         fprintf (stderr, "MMX cpu detected.\n");
+         message("MMX cpu detected.");
       } else {
          gl_x86_cpu_features &= (~GL_CPU_MMX); 
       }
    }
 #endif
 
+
 #ifdef USE_3DNOW_ASM
    if (gl_x86_cpu_features & GL_CPU_3Dnow) {
       char *s = getenv( "MESA_NO_3DNOW" );
       if (s == NULL) {
-         fprintf (stderr, "3Dnow cpu detected.\n");
+         message("3Dnow cpu detected.");
          gl_init_3dnow_asm_transforms ();
       } else {
          gl_x86_cpu_features &= (~GL_CPU_3Dnow); 
@@ -86,7 +93,7 @@ void gl_init_all_x86_asm (void)
    if (gl_x86_cpu_features & GL_CPU_Katmai) {
       char *s = getenv( "MESA_NO_KATMAI" );
       if (s == NULL) {
-         fprintf (stderr, "Katmai cpu detected.\n");
+         message("Katmai cpu detected.");
          gl_init_katmai_asm_transforms ();
       } else {
          gl_x86_cpu_features &= (~GL_CPU_Katmai); 
