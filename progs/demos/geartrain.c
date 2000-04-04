a74394c3a8090ba95b9ceffa63da94e281d78e2f
Brian Paul <brian.paul@tungstengraphics.com>
2000-04-04 15:20:17 +0000

    added copyright info

--- progs/demos/geartrain.c
+++ progs/demos/geartrain.c
@@ -1,9 +1,28 @@
-/* $Id: geartrain.c,v 1.3 2000/03/29 17:56:02 brianp Exp $ */
+/* $Id: geartrain.c,v 1.4 2000/04/04 15:20:17 brianp Exp $ */
 
 /*
- * Geartrain simulation 
+ * GearTrain Simulator * Version:  1.00
  *
- * Contributed by Shobhan Kumar Dutta <skdutta@del3.vsnl.net.in>
+ * Copyright (C) 1999  Shobhan Kumar Dutta  All Rights Reserved.
+ * <skdutta@del3.vsnl.net.in>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * SHOBHAN KUMAR DUTTA BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+ * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 
