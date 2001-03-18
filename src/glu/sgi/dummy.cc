2f9757f344a05fa0ecb758f8b6a8ee1d631b0478
pesco <pesco>
2001-03-18 13:06:19 +0000

    Forgot to add newly created files.

---
+++ src/glu/sgi/dummy.cc
@@ -0,0 +1,5 @@
+/* $Id: dummy.cc,v 1.1 2001/03/18 13:06:19 pesco Exp $ */
+/*
+ * This file contains nothing. It's just there so there's at least a single
+ * source file for libGLU.la in this directory.
+ */
