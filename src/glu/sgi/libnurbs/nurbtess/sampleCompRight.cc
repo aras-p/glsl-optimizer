3427e3547ed67096d6ae966da6deddc82c9f66fd
Jouk Jansen <joukj@hrem.stm.tudelft.nl>
2001-03-22 11:41:40 +0000

    Modified Files:  	Mesa/si-glu/libnurbs/nurbtess/monoTriangulation.cc  	Mesa/si-glu/libnurbs/nurbtess/sampleCompRight.cc
     gluos.h included in order to bypass compiler warnings on unreachable code on
     VMS systems.
    ----------------------------------------------------------------------

--- src/glu/sgi/libnurbs/nurbtess/sampleCompRight.cc
+++ src/glu/sgi/libnurbs/nurbtess/sampleCompRight.cc
@@ -31,14 +31,15 @@
 ** published by SGI, but has not been independently verified as being
 ** compliant with the OpenGL(R) version 1.2.1 Specification.
 **
-** $Date: 2001/03/17 00:25:41 $ $Revision: 1.1 $
+** $Date: 2001/03/22 11:41:40 $ $Revision: 1.2 $
 */
 /*
-** $Header: /home/krh/git/sync/mesa-cvs-repo/Mesa/src/glu/sgi/libnurbs/nurbtess/sampleCompRight.cc,v 1.1 2001/03/17 00:25:41 brianp Exp $
+** $Header: /home/krh/git/sync/mesa-cvs-repo/Mesa/src/glu/sgi/libnurbs/nurbtess/sampleCompRight.cc,v 1.2 2001/03/22 11:41:40 joukj Exp $
 */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "gluos.h"
 #include "glimports.h"
 #include "zlassert.h"
 #include "sampleCompRight.h"
