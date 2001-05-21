e9482d004f7b4bdc99312a89c33c061f31552bda
Brian Paul <brian.paul@tungstengraphics.com>
2001-05-21 14:46:33 +0000

    s/Height/height/ (Jeff Potter)

--- src/mesa/drivers/windows/wmesa.c
+++ src/mesa/drivers/windows/wmesa.c
@@ -1,4 +1,4 @@
-/* $Id: wmesa.c,v 1.16 2001/05/09 20:42:37 brianp Exp $ */
+/* $Id: wmesa.c,v 1.17 2001/05/21 14:46:33 brianp Exp $ */
 
 /*
  * Windows (Win32) device driver for Mesa 3.4
@@ -1050,7 +1050,7 @@ static void read_rgba_pixels( const GLcontext* ctx,
    assert(Current->rgb_flag==GL_TRUE);
    for (i=0; i<n; i++) {
       if (mask[i]) {
-         GLint y2 = Current->Height - y[i] - 1;
+         GLint y2 = Current->height - y[i] - 1;
          Color=GetPixel(DC,x[i],y2);
          rgba[i][RCOMP] = GetRValue(Color);
          rgba[i][GCOMP] = GetGValue(Color);
