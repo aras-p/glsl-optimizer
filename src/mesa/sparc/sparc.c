04a6c35819f0a386c98d65bf5d4bf0e5e776f04c
Brian Paul <brian.paul@tungstengraphics.com>
2001-05-23 14:42:47 +0000

    updated copyright

--- src/mesa/sparc/sparc.c
+++ src/mesa/sparc/sparc.c
@@ -1,10 +1,10 @@
-/* $Id: sparc.c,v 1.1 2001/05/23 14:27:03 brianp Exp $ */
+/* $Id: sparc.c,v 1.2 2001/05/23 14:42:47 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
- * Version:  3.1
+ * Version:  3.5
  * 
- * Copyright (C) 1999  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2001  Brian Paul   All Rights Reserved.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
