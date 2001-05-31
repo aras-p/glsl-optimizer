4097590bc6e13c1903760dbfb1d60123ee0f527d
Brian Paul <brian.paul@tungstengraphics.com>
2001-05-31 23:06:01 +0000

    replaced #ifdef 0 with #if 0

--- src/mesa/x86/x86.c
+++ src/mesa/x86/x86.c
@@ -1,4 +1,4 @@
-/* $Id: x86.c,v 1.23 2001/05/21 16:33:41 gareth Exp $ */
+/* $Id: x86.c,v 1.24 2001/05/31 23:06:01 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -101,7 +101,7 @@ void _mesa_init_x86_vertex_asm( void )
    _mesa_xform_points3_v16_general = _mesa_v16_x86_general_xform;
    _mesa_cliptest_points4_v16 = _mesa_v16_x86_cliptest_points4;
 
-#ifdef 0 /* DEBUG */
+#if 0 /* DEBUG */
    _math_test_all_vertex_functions( "x86" );
 #endif
 #endif
