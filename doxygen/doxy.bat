ac9aa2217423431c38d9d37496355e85666b5ebb
Brian Paul <brian.paul@tungstengraphics.com>
2002-01-21 19:29:55 +0000

    files to support doxygen (source code documentation)

---
+++ doxygen/doxy.bat
@@ -0,0 +1,14 @@
+doxygen tnl_dd.doxy
+doxygen array_cache.doxy
+doxygen math.doxy
+doxygen swrast.doxy
+doxygen swrast_setup.doxy
+doxygen tnl.doxy
+doxygen core.doxy
+echo Building again, to resolve tags
+doxygen tnl_dd.doxy
+doxygen array_cache.doxy
+doxygen math.doxy
+doxygen swrast.doxy
+doxygen swrast_setup.doxy
+doxygen tnl.doxy
