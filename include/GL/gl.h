001a3012ac66c279ba8076a13c59d71d025f2ed9
Brian Paul <brian.paul@tungstengraphics.com>
2003-02-28 16:31:32 +0000

    updated version

--- include/GL/gl.h
+++ include/GL/gl.h
@@ -1,10 +1,10 @@
-/* $Id: gl.h,v 1.74 2003/02/10 20:22:08 alanh Exp $ */
+/* $Id: gl.h,v 1.75 2003/02/28 16:31:32 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
- * Version:  4.1
+ * Version:  5.1
  *
- * Copyright (C) 1999-2002  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2003  Brian Paul   All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
