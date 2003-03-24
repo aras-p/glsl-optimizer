ee2b34215fc91b1c8b36b0b998df4ec9d9526fb8
Brian Paul <brian.paul@tungstengraphics.com>
2003-03-24 20:00:09 +0000

    add 2, not 4, in _mesa_get_compressed_formats()

--- src/mesa/main/texcompress.c
+++ src/mesa/main/texcompress.c
@@ -1,10 +1,10 @@
-/* $Id: texcompress.c,v 1.3 2002/10/24 23:57:21 brianp Exp $ */
+/* $Id: texcompress.c,v 1.4 2003/03/24 20:00:09 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
- * Version:  4.1
+ * Version:  5.1
  *
- * Copyright (C) 1999-2002  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2003  Brian Paul   All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -49,7 +49,7 @@ _mesa_get_compressed_formats( GLcontext *ctx, GLint *formats )
             formats[n++] = GL_COMPRESSED_RGBA_FXT1_3DFX;
          }
          else {
-            n += 4;
+            n += 2;
          }
       }
    }
