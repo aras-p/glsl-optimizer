ef393cbb54f49e229df623ab7c91b4dbc78b6df7
Brian Paul <brian.paul@tungstengraphics.com>
2003-04-09 02:32:12 +0000

    Added GL_ARB_vertex_buffer_object tokens

--- include/GL/glext.h
+++ include/GL/glext.h
@@ -5032,6 +5032,38 @@ typedef void (APIENTRY * PFNGLACTIVESTENCILFACEEXTPROC) (GLenum face);
 typedef long int GLsizeiptrARB;
 typedef long int GLintptrARB;
 
+#define GL_ARRAY_BUFFER_ARB                             0x8892
+#define GL_ELEMENT_ARRAY_BUFFER_ARB                     0x8893
+#define GL_ARRAY_BUFFER_BINDING_ARB                     0x8894
+#define GL_ELEMENT_ARRAY_BUFFER_BINDING_ARB             0x8895
+#define GL_VERTEX_ARRAY_BUFFER_BINDING_ARB              0x8896
+#define GL_NORMAL_ARRAY_BUFFER_BINDING_ARB              0x8897
+#define GL_COLOR_ARRAY_BUFFER_BINDING_ARB               0x8898
+#define GL_INDEX_ARRAY_BUFFER_BINDING_ARB               0x8899
+#define GL_TEXTURE_COORD_ARRAY_BUFFER_BINDING_ARB       0x889A
+#define GL_EDGE_FLAG_ARRAY_BUFFER_BINDING_ARB           0x889B
+#define GL_SECONDARY_COLOR_ARRAY_BUFFER_BINDING_ARB     0x889C
+#define GL_FOG_COORDINATE_ARRAY_BUFFER_BINDING_ARB      0x889D
+#define GL_WEIGHT_ARRAY_BUFFER_BINDING_ARB              0x889E
+#define GL_VERTEX_ATTRIB_ARRAY_BUFFER_BINDING_ARB       0x889F
+#define GL_STREAM_DRAW_ARB                              0x88E0
+#define GL_STREAM_READ_ARB                              0x88E1
+#define GL_STREAM_COPY_ARB                              0x88E2
+#define GL_STATIC_DRAW_ARB                              0x88E4
+#define GL_STATIC_READ_ARB                              0x88E5
+#define GL_STATIC_COPY_ARB                              0x88E6
+#define GL_DYNAMIC_DRAW_ARB                             0x88E8
+#define GL_DYNAMIC_READ_ARB                             0x88E9
+#define GL_DYNAMIC_COPY_ARB                             0x88EA
+#define GL_READ_ONLY_ARB                                0x88B8
+#define GL_WRITE_ONLY_ARB                               0x88B9
+#define GL_READ_WRITE_ARB                               0x88BA
+#define GL_BUFFER_SIZE_ARB                              0x8764
+#define GL_BUFFER_USAGE_ARB                             0x8765
+#define GL_BUFFER_ACCESS_ARB                            0x88BB
+#define GL_BUFFER_MAPPED_ARB                            0x88BC
+#define GL_BUFFER_MAP_POINTER_ARB                       0x88BD
+
 extern void glBindBufferARB(GLenum target, GLuint buffer);
 extern void glDeleteBuffersARB(GLsizei n, const GLuint * buffer);
 extern void glGenBuffersARB(GLsizei n, GLuint * buffer);
