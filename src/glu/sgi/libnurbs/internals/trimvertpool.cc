6636a6141800fd367689e9d3bcdd61c298df64ff
Brian Paul <brian.paul@tungstengraphics.com>
2003-05-08 15:47:00 +0000

    fixed delete statement (bug 721765)

--- src/glu/sgi/libnurbs/internals/trimvertpool.cc
+++ src/glu/sgi/libnurbs/internals/trimvertpool.cc
@@ -35,8 +35,8 @@
 /*
  * trimvertexpool.c++
  *
- * $Date: 2001/03/17 00:25:41 $ $Revision: 1.1 $
- * $Header: /home/krh/git/sync/mesa-cvs-repo/Mesa/src/glu/sgi/libnurbs/internals/trimvertpool.cc,v 1.1 2001/03/17 00:25:41 brianp Exp $
+ * $Date: 2003/05/08 15:47:00 $ $Revision: 1.2 $
+ * $Header: /home/krh/git/sync/mesa-cvs-repo/Mesa/src/glu/sgi/libnurbs/internals/trimvertpool.cc,v 1.2 2003/05/08 15:47:00 brianp Exp $
  */
 
 #include "glimports.h"
@@ -68,7 +68,7 @@ TrimVertexPool::~TrimVertexPool( void )
 {
     // free all arrays of TrimVertices vertices
     while( nextvlistslot ) {
-	delete vlist[--nextvlistslot];
+	delete [] vlist[--nextvlistslot];
     }
 
     // reallocate space for array of pointers to vertex lists
@@ -87,7 +87,7 @@ TrimVertexPool::clear( void )
 
     // free all arrays of TrimVertices vertices
     while( nextvlistslot ) {
-	delete vlist[--nextvlistslot];
+	delete [] vlist[--nextvlistslot];
 	vlist[nextvlistslot] = 0;
     }
 
