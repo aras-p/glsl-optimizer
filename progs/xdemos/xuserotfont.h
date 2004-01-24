b4b35bcc42f230fcf56af228b127d4943d4a2c1b
Brian Paul <brian.paul@tungstengraphics.com>
2004-01-24 16:50:35 +0000

    New glXUseRotatedXFontMESA() function.  Like glXUseXFont(), but takes a rotation parameter (either 0, 90, 180 or 270 degrees). Also, a demo program.

---
+++ progs/xdemos/xuserotfont.h
@@ -0,0 +1,12 @@
+#ifndef XUSEROTFONT_H
+#define XUSEROTFONT_H
+
+#include <X11/Xlib.h>
+
+
+extern void
+glXUseRotatedXFontMESA(Font font, int first, int count, int listbase,
+                       int rotation);
+
+
+#endif
