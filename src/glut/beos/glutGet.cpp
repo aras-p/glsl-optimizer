479b3d074589b1582e85aec90e53b82154ebcc2d
Philippe Houdoin <phoudoin@freedesktop.org>
2004-08-16 08:46:38 +0000

    Add glutGetProcAddress(). Not all GLUT version 5 APIs are supported yet, thought. -> #define GLUT_API_VERSION 4.5 ? ;-)

--- src/glut/beos/glutGet.cpp
+++ src/glut/beos/glutGet.cpp
@@ -209,39 +209,3 @@ int glutGetModifiers() {
 	return gState.modifierKeys;
 }
 
-/***********************************************************
- *	FUNCTION:	glutExtensionSupported (9.5)
- *
- *	DESCRIPTION:  is an OpenGL extension supported (from glut_ext.c)
- ***********************************************************/
-int glutExtensionSupported(const char *extension) {
-  static const GLubyte *extensions = NULL;
-  const GLubyte *start;
-  GLubyte *where, *terminator;
-
-  /* Extension names should not have spaces. */
-  where = (GLubyte *) strchr(extension, ' ');
-  if (where || *extension == '\0')
-    return 0;
-
-  if (!extensions)
-    extensions = glGetString(GL_EXTENSIONS);
-  /* It takes a bit of care to be fool-proof about parsing the
-     OpenGL extensions string.  Don't be fooled by sub-strings, 
-
-     etc. */
-  start = extensions;
-  for (;;) {
-    where = (GLubyte *) strstr((const char *) start, extension);
-    if (!where)
-      break;
-    terminator = where + strlen(extension);
-    if (where == start || *(where - 1) == ' ') {
-      if (*terminator == ' ' || *terminator == '\0') {
-        return 1;
-      }
-    }
-    start = terminator;
-  }
-  return 0;
-}
