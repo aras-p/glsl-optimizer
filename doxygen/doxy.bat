7e5c452efbee3f4df406d3c702e25c1d02285971
Brian Paul <brian.paul@tungstengraphics.com>
2004-09-10 00:44:09 +0000

    added glapi and shader modules, misc updates

--- doxygen/doxy.bat
+++ doxygen/doxy.bat
@@ -5,6 +5,9 @@ doxygen swrast.doxy
 doxygen swrast_setup.doxy
 doxygen tnl.doxy
 doxygen core.doxy
+doxygen glapi.doxy
+doxygen shader.doxy
+
 echo Building again, to resolve tags
 doxygen tnl_dd.doxy
 doxygen array_cache.doxy
@@ -12,3 +15,5 @@ doxygen math.doxy
 doxygen swrast.doxy
 doxygen swrast_setup.doxy
 doxygen tnl.doxy
+doxygen glapi.doxy
+doxygen shader.doxy
