baa832d311cf36785bbe300412b946781d15d5c2
Eric Anholt <anholt@FreeBSD.org>
2005-02-14 06:57:27 +0000

    Change another usage of __AMD64__ to the standard __amd64__, and remove __AMD64__ from the linux configs so it won't creep back in as easily.  Fixes hang on FreeBSD on the first lock grab.

--- src/mesa/drivers/dri/dri_client/imports/xf86drm.h
+++ src/mesa/drivers/dri/dri_client/imports/xf86drm.h
@@ -285,7 +285,7 @@ typedef struct _drmSetVersion {
 #define DRM_LOCK_CONT  0x40000000 /**< Hardware lock is contended */
 
 #if defined(__GNUC__) && (__GNUC__ >= 2)
-# if defined(__i386) || defined(__AMD64__)
+# if defined(__i386) || defined(__amd64__)
 				/* Reflect changes here to drmP.h */
 #define DRM_CAS(lock,old,new,__ret)                                    \
 	do {                                                           \
