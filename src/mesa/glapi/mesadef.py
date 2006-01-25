166b0ff973b695c4a46b1825899c8bf10d1494b9
Brian Paul <brian.paul@tungstengraphics.com>
2006-01-25 15:05:36 +0000

    added wglGetExtensionsStringARB

--- src/mesa/glapi/mesadef.py
+++ src/mesa/glapi/mesadef.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# $Id: mesadef.py,v 1.3 2005/09/05 14:09:25 kschultz Exp $
+# $Id: mesadef.py,v 1.4 2006/01/25 15:05:36 brianp Exp $
 
 # Mesa 3-D graphics library
 # Version:  4.1
@@ -89,6 +89,7 @@ def PrintTail():
 	print '\twglDescribePixelFormat'
 	print '\twglGetCurrentContext'
 	print '\twglGetCurrentDC'
+	print '\twglGetExtensionsStringARB'
 	print '\twglGetLayerPaletteEntries'
 	print '\twglGetPixelFormat'
 	print '\twglGetProcAddress'
