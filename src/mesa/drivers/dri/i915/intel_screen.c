b77a225250ce1855b8a6d577b53ca836c33e671b
Alan Hourihane <alanh@tungstengraphics.com>
2006-02-08 22:05:42 +0000

    Check DDX for 1.5.0

--- src/mesa/drivers/dri/i915/intel_screen.c
+++ src/mesa/drivers/dri/i915/intel_screen.c
@@ -642,7 +642,7 @@ void * __driCreateNewScreen_20050727( __DRInativeDisplay *dpy, int scrn, __DRIsc
 			     
 {
    __DRIscreenPrivate *psp;
-   static const __DRIversion ddx_expected = { 1, 4, 0 };
+   static const __DRIversion ddx_expected = { 1, 5, 0 };
    static const __DRIversion dri_expected = { 4, 0, 0 };
    static const __DRIversion drm_expected = { 1, 4, 0 };
 
