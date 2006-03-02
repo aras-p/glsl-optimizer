14d3ab96629907622bd25a103b3dadcfccb40b4c
Brian Paul <brian.paul@tungstengraphics.com>
2006-03-02 03:51:04 +0000

    improved error msg

--- src/mesa/main/renderbuffer.c
+++ src/mesa/main/renderbuffer.c
@@ -1154,7 +1154,9 @@ soft_renderbuffer_storage(GLcontext *ctx, struct gl_renderbuffer *rb,
    if (rb->Data == NULL) {
       rb->Width = 0;
       rb->Height = 0;
-      _mesa_error(ctx, GL_OUT_OF_MEMORY, "software renderbuffer allocation");
+      _mesa_error(ctx, GL_OUT_OF_MEMORY,
+                  "software renderbuffer allocation (%d x %d x %d)",
+                  width, height, pixelSize);
       return GL_FALSE;
    }
 
