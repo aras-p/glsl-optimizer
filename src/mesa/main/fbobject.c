2e01918b553ad50f6df788bb762f487d618ad37e
Brian Paul <brian.paul@tungstengraphics.com>
2006-03-07 01:43:52 +0000

    call _mesa_update_framebuffer_visual() in _mesa_FramebufferRenderbufferEXT() to fix some minor glitches

--- src/mesa/main/fbobject.c
+++ src/mesa/main/fbobject.c
@@ -2,7 +2,7 @@
  * Mesa 3-D graphics library
  * Version:  6.5
  *
- * Copyright (C) 1999-2005  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2006  Brian Paul   All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -1373,6 +1373,11 @@ _mesa_FramebufferRenderbufferEXT(GLenum target, GLenum attachment,
 
    assert(ctx->Driver.FramebufferRenderbuffer);
    ctx->Driver.FramebufferRenderbuffer(ctx, fb, attachment, rb);
+
+   /* Some subsequent GL commands may depend on the framebuffer's visual
+    * after the binding is updated.  Update visual info now.
+    */
+   _mesa_update_framebuffer_visual(fb);
 }
 
 
