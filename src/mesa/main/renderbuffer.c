19b83221696f124bfe1fa7e72842c62c3f72ee43
Brian Paul <brian.paul@tungstengraphics.com>
2006-03-14 22:39:43 +0000

    init Red/Green/EtcBits = 0 in soft_renderbuffer_storage()

--- src/mesa/main/renderbuffer.c
+++ src/mesa/main/renderbuffer.c
@@ -2,7 +2,7 @@
  * Mesa 3-D graphics library
  * Version:  6.5
  *
- * Copyright (C) 1999-2005  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2006  Brian Paul   All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -924,6 +924,15 @@ soft_renderbuffer_storage(GLcontext *ctx, struct gl_renderbuffer *rb,
                           GLenum internalFormat, GLuint width, GLuint height)
 {
    GLuint pixelSize;
+
+   /* first clear these fields */
+   rb->RedBits =
+   rb->GreenBits =
+   rb->BlueBits =
+   rb->AlphaBits =
+   rb->IndexBits =
+   rb->DepthBits =
+   rb->StencilBits = 0;
 
    switch (internalFormat) {
    case GL_RGB:
