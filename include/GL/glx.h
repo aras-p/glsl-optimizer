9305cba6a7dfe539e5c3b01f7b16fcf60cf20900
Brian Paul <brian.paul@tungstengraphics.com>
2006-03-31 17:31:22 +0000

    bump version/date

--- include/GL/glx.h
+++ include/GL/glx.h
@@ -2,7 +2,7 @@
  * Mesa 3-D graphics library
  * Version:  6.5
  * 
- * Copyright (C) 1999-2005  Brian Paul   All Rights Reserved.
+ * Copyright (C) 1999-2006  Brian Paul   All Rights Reserved.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
