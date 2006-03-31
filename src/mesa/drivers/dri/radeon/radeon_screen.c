6652dc8aac79265190efa0a90ebadab8831c6cca
Aapo Tahkola <aet@rasterburn.org>
2006-03-31 14:11:41 +0000

    free rs300 and ban r350(according to r300.sf.net)

--- src/mesa/drivers/dri/radeon/radeon_screen.c
+++ src/mesa/drivers/dri/radeon/radeon_screen.c
@@ -637,10 +637,10 @@ radeonCreateScreen( __DRIscreenPrivate *sPriv )
 	      dri_priv->deviceID);
       return NULL;
    }
-   if (screen->chip_family == CHIP_FAMILY_RS300 || 
+   if (screen->chip_family == CHIP_FAMILY_R350 || 
        screen->chip_family == CHIP_FAMILY_R300) {
 	   if (getenv("R300_FORCE_R300") == NULL) {
-		   fprintf(stderr, "Radeon 9500/9700 cards are not currently stable.\n");
+		   fprintf(stderr, "Radeon 9500/9700/9800 cards are not currently stable.\n");
 		   fprintf(stderr, "More details can be found at https://bugs.freedesktop.org/show_bug.cgi?id=6318\n");
 		   return NULL;
 	   }
