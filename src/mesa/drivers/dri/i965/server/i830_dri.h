2332812644d5ab98d884cd0083e9d68c8e09b4cf
Eric Anholt <anholt@FreeBSD.org>
2006-08-10 21:26:50 +0000

    Add padding to match the current xf86-video-intel DDX->DRI interface.

--- src/mesa/drivers/dri/i965/server/i830_dri.h
+++ src/mesa/drivers/dri/i965/server/i830_dri.h
@@ -18,12 +18,32 @@ typedef struct _I830DRIRec {
    drm_handle_t regs;
    drmSize regsSize;
 
+   drmSize unused1; /* backbufferSize */
+   drm_handle_t unused2; /* backbuffer */
+
+   drmSize unused3; /* depthbufferSize */
+   drm_handle_t unused4; /* depthbuffer */
+
+   drmSize unused5; /* rotatedSize /*/
+   drm_handle_t unused6; /* rotatedbuffer */
+
+   drm_handle_t unused7; /* textures */
+   int unused8; /* textureSize */
+
+   drm_handle_t unused9; /* agp_buffers */
+   drmSize unused10; /* agp_buf_size */
+
    int deviceID;
    int width;
    int height;
    int mem;
    int cpp;
    int bitsPerPixel;
+
+   int unused11[8]; /* was front/back/depth/rotated offset/pitch */
+
+   int unused12; /* logTextureGranularity */
+   int unused13; /* textureOffset */
 
    int irq;
    int sarea_priv_offset;
