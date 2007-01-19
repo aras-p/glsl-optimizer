669fefd3da55e3946b4b65a5d1104540c04717cb
Ben Skeggs <darktama@iinet.net.au>
2007-01-19 15:39:36 +1100

    nouveau: bump drm patchlevel

--- src/mesa/drivers/dri/nouveau/nouveau_screen.c
+++ src/mesa/drivers/dri/nouveau/nouveau_screen.c
@@ -327,7 +327,7 @@ void * __driCreateNewScreen_20050727( __DRInativeDisplay *dpy, int scrn, __DRIsc
 	__DRIscreenPrivate *psp;
 	static const __DRIversion ddx_expected = { 1, 2, 0 };
 	static const __DRIversion dri_expected = { 4, 0, 0 };
-	static const __DRIversion drm_expected = { 0, 0, 2 };
+	static const __DRIversion drm_expected = { 0, 0, 3 };
 
 	dri_interface = interface;
 
