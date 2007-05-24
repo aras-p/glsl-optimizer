7234550cf759fdf3ab325465090c7f5eb3b5d320
Brian <brian.paul@tungstengraphics.com>
2007-05-24 16:49:27 -0600

    #ifndef SP_DEFINES_H protection, copyright

--- src/mesa/softpipe/sp_defines.h
+++ src/mesa/softpipe/sp_defines.h
@@ -1,3 +1,32 @@
+/**************************************************************************
+ * 
+ * Copyright 2007 Tungsten Graphics, Inc., Cedar Park, Texas.
+ * All Rights Reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL TUNGSTEN GRAPHICS AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ **************************************************************************/
+
+#ifndef SP_DEFINES_H
+#define SP_DEFINES_H
 
 #define SP_BLENDFACTOR_ONE                 0x1
 #define SP_BLENDFACTOR_SRC_COLOR           0x2
@@ -25,7 +54,6 @@
 #define SP_BLEND_MIN               3
 #define SP_BLEND_MAX               4
 
-
 #define SP_LOGICOP_CLEAR            0
 #define SP_LOGICOP_NOR              1
 #define SP_LOGICOP_AND_INVERTED     2
@@ -43,3 +71,4 @@
 #define SP_LOGICOP_OR               14
 #define SP_LOGICOP_SET              15  
 
+#endif
