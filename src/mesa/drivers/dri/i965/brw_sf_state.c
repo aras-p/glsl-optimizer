a6dad044e24358af9fe5764ca7b342f4ed4804db
Zou Nan hai <nanhai.zou@intel.com>
2007-08-01 11:08:05 +0800

    fix fd.o bug #11788, max point size

--- src/mesa/drivers/dri/i965/brw_sf_state.c
+++ src/mesa/drivers/dri/i965/brw_sf_state.c
@@ -185,7 +185,7 @@ static void upload_sf_unit( struct brw_context *brw )
    /* _NEW_POINT */
    sf.sf6.point_rast_rule = 1;	/* opengl conventions */
    /* XXX clamp max depends on AA vs. non-AA */
-   sf.sf7.point_size = CLAMP(brw->attribs.Point->Size, 1.0, 3.0) * (1<<3);
+   sf.sf7.point_size = CLAMP(brw->attribs.Point->Size, 1.0, 255.0) * (1<<3);
    sf.sf7.use_point_size_state = !brw->attribs.Point->_Attenuated;
       
    /* might be BRW_NEW_PRIMITIVE if we have to adjust pv for polygons:
