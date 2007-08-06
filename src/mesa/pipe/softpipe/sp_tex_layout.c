07633371ff4987d3149e78a1f0791da720af4e60
Brian <brian.paul@tungstengraphics.com>
2007-08-06 20:54:03 +0100

    need a softpipe func for texture layout

---
+++ src/mesa/pipe/softpipe/sp_tex_layout.c
@@ -0,0 +1,13 @@
+
+#include "pipe/p_state.h"
+#include "sp_tex_layout.h"
+
+
+GLboolean
+softpipe_mipmap_tree_layout(struct pipe_context *pipe,
+                            struct pipe_mipmap_tree *mt)
+{
+   return GL_TRUE;
+}
+
+
---
+++ src/mesa/pipe/softpipe/sp_tex_layout.h
@@ -0,0 +1,16 @@
+#ifndef SP_TEX_LAYOUT_H
+#define SP_TEX_LAYOUT_H
+
+
+struct pipe_context;
+struct pipe_mipmap_tree;
+
+
+extern GLboolean
+softpipe_mipmap_tree_layout(struct pipe_context *pipe,
+                            struct pipe_mipmap_tree *mt);
+
+
+#endif /* SP_TEX_LAYOUT_H */
+
+
