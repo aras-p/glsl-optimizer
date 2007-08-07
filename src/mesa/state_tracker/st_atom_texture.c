28d64d398bb3e04d20a9c03c9a0b92c0a7b9d6d4
Brian <brian@i915.localnet.net>
2007-08-07 15:11:20 -0600

    texture image state handling

---
+++ src/mesa/state_tracker/st_atom_texture.c
@@ -0,0 +1,76 @@
+/**************************************************************************
+ * 
+ * Copyright 2007 Tungsten Graphics, Inc., Cedar Park, Texas.
+ * All Rights Reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL TUNGSTEN GRAPHICS AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ **************************************************************************/
+
+ /*
+  * Authors:
+  *   Keith Whitwell <keith@tungstengraphics.com>
+  *   Brian Paul
+  */
+ 
+
+#include "st_context.h"
+#include "st_atom.h"
+#include "st_cb_texture.h"
+#include "pipe/p_context.h"
+#include "pipe/p_defines.h"
+
+
+
+static void 
+update_textures(struct st_context *st)
+{
+   GLuint u;
+
+   for (u = 0; u < st->ctx->Const.MaxTextureImageUnits; u++) {
+      struct gl_texture_object *texObj
+         = st->ctx->Texture.Unit[u]._Current;
+      if (texObj) {
+         struct pipe_mipmap_tree *mt;
+
+         mt = st_get_texobj_mipmap_tree(texObj);
+
+         st->pipe->set_texture_state(st->pipe, u, mt);
+      }
+      else {
+         st->pipe->set_texture_state(st->pipe, u, NULL);
+      }
+   }
+}
+
+
+const struct st_tracked_state st_update_texture = {
+   .dirty = {
+      .mesa = _NEW_TEXTURE,
+      .st  = 0,
+   },
+   .update = update_textures
+};
+
+
+
+
+
