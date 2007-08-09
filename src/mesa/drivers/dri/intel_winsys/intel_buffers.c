3a035d0d23d2ee7edd48be144c6619d56fbd5469
Brian <brian@i915.localnet.net>
2007-08-09 13:08:43 -0600

    don't include intel_depthstencil.h

--- src/mesa/drivers/dri/intel_winsys/intel_buffers.c
+++ src/mesa/drivers/dri/intel_winsys/intel_buffers.c
@@ -29,7 +29,6 @@
 #include "intel_context.h"
 #include "intel_blit.h"
 #include "intel_buffers.h"
-#include "intel_depthstencil.h"
 #include "intel_batchbuffer.h"
 #include "intel_reg.h"
 #include "context.h"
