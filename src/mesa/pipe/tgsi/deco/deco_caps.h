6b6cc4c0fe04884b22f6f0010be9dcee2b4efa9f
michal <michal@michal-laptop.(none)>
2007-08-22 16:38:00 +0100

    Include headers.

---
+++ src/mesa/pipe/tgsi/deco/deco_caps.h
@@ -0,0 +1,15 @@
+#if !defined DECO_CAPS_H
+#define DECO_CAPS_H
+
+#if defined __cplusplus
+extern "C" {
+#endif // defined __cplusplus
+
+
+
+#if defined __cplusplus
+} // extern "C"
+#endif // defined __cplusplus
+
+#endif // !defined DECO_CAPS_H
+
---
+++ src/mesa/pipe/tgsi/deco/tgsi_deco.h
@@ -0,0 +1,8 @@
+#if !defined TGSI_DECO_H
+#define TGSI_DECO_H
+
+#include "../core/tgsi_core.h"
+#include "deco_caps.h"
+
+#endif // !defined TGSI_DECO_H
+
