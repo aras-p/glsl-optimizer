ce765a7fb77e12ff083a9068ec232a15bcf41f66
Michel Dänzer <michel@tungstengraphics.com>
2007-10-02 13:01:07 +0200

    intel_winsys: Adapt to DRM changes (again).
    It was decided after all to stick to 'pipes' here, even though the actual
    meaning is now 'planes'.

--- src/mesa/drivers/dri/intel_winsys/intel_swapbuffers.c
+++ src/mesa/drivers/dri/intel_winsys/intel_swapbuffers.c
@@ -46,7 +46,7 @@
 #define DRM_VBLANK_FLIP 0x8000000
 
 typedef struct drm_i915_flip {
-   int planes;
+   int pipes;
 } drm_i915_flip_t;
 
 #undef DRM_IOCTL_I915_FLIP
@@ -314,7 +314,7 @@ intelWindowMoved(struct intel_context *intel)
 		  ((intel_fb->pf_current_page + intel_fb->pf_num_pages - 1) %
 		   intel_fb->pf_num_pages) << 2;
 
-	       flip.planes = 0x2;
+	       flip.pipes = 0x2;
 	    } else {
                intel->sarea->pf_current_page =
 		  intel->sarea->pf_current_page & (0x3 << 2);
@@ -322,7 +322,7 @@ intelWindowMoved(struct intel_context *intel)
 		  (intel_fb->pf_current_page + intel_fb->pf_num_pages - 1) %
 		  intel_fb->pf_num_pages;
 
-	       flip.planes = 0x1;
+	       flip.pipes = 0x1;
 	    }
 
 	    drmCommandWrite(intel->driFd, DRM_I915_FLIP, &flip, sizeof(flip));
@@ -449,7 +449,7 @@ intelPageFlip(const __DRIdrawablePrivate * dPriv)
    if (dPriv->numClipRects && intel_fb->pf_active) {
       drm_i915_flip_t flip;
 
-      flip.planes = intel_fb->pf_planes;
+      flip.pipes = intel_fb->pf_planes;
 
       ret = drmCommandWrite(intel->driFd, DRM_I915_FLIP, &flip, sizeof(flip));
    }
