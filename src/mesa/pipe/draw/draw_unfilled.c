495961716430b1ff99078abe1a7ac9bc07dd6116
Brian <brian.paul@tungstengraphics.com>
2007-10-16 17:59:24 -0600

    front/back determination was wrong

--- src/mesa/pipe/draw/draw_unfilled.c
+++ src/mesa/pipe/draw/draw_unfilled.c
@@ -125,7 +125,7 @@ static void unfilled_tri( struct draw_stage *stage,
 			  struct prim_header *header )
 {
    struct unfilled_stage *unfilled = unfilled_stage(stage);
-   unsigned mode = unfilled->mode[header->det < 0.0];
+   unsigned mode = unfilled->mode[header->det >= 0.0];
   
    switch (mode) {
    case PIPE_POLYGON_MODE_FILL:
