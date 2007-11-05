c1933ed23fa72e0d5bffd156c5c52558cc6b3190
Brian <brian.paul@tungstengraphics.com>
2007-11-05 09:04:37 -0700

    remove dead code in intelCreateBuffer()

--- src/mesa/drivers/dri/intel_winsys/intel_screen.c
+++ src/mesa/drivers/dri/intel_winsys/intel_screen.c
@@ -280,67 +280,8 @@ intelCreateBuffer(__DRIscreenPrivate * driScrnPriv,
       return GL_FALSE;          /* not implemented */
    }
    else {
-#if 0
-      GLboolean swStencil = (mesaVis->stencilBits > 0 &&
-                             mesaVis->depthBits != 24);
-      GLenum rgbFormat = (mesaVis->redBits == 5 ? GL_RGB5 : GL_RGBA8);
-
-      struct intel_framebuffer *intel_fb = CALLOC_STRUCT(intel_framebuffer);
-      if (!intel_fb)
-	 return GL_FALSE;
-
-      _mesa_initialize_framebuffer(&intel_fb->Base, mesaVis);
-
-      /*
-       * XXX Create pipe_surfaces for front/back buffers,
-       * hand them to state tracker to create a framebuffer object.
-       */
-
-      {
-	 /* fake frontbuffer */
-	 /* XXX allocation should only happen in the unusual case
-            it's actually needed */
-         struct gl_renderbuffer *rb = st_new_renderbuffer_fb(rgbFormat);
-         _mesa_add_renderbuffer(&intel_fb->Base, BUFFER_FRONT_LEFT, rb);
-      }
-
-      if (mesaVis->doubleBufferMode) {
-         struct gl_renderbuffer *rb = st_new_renderbuffer_fb(rgbFormat);
-         _mesa_add_renderbuffer(&intel_fb->Base, BUFFER_BACK_LEFT, rb);
-      }
-
-      if (mesaVis->depthBits == 24 && mesaVis->stencilBits == 8) {
-         /* combined depth/stencil buffer */
-         struct gl_renderbuffer *depthStencilRb
-            = st_new_renderbuffer_fb(GL_DEPTH24_STENCIL8_EXT);
-         /* note: bind RB to two attachment points */
-         _mesa_add_renderbuffer(&intel_fb->Base, BUFFER_DEPTH, depthStencilRb);
-         _mesa_add_renderbuffer(&intel_fb->Base, BUFFER_STENCIL,depthStencilRb);
-      }
-      else if (mesaVis->depthBits == 16) {
-         /* just 16-bit depth buffer, no hw stencil */
-         struct gl_renderbuffer *depthRb
-            = st_new_renderbuffer_fb(GL_DEPTH_COMPONENT16);
-         _mesa_add_renderbuffer(&intel_fb->Base, BUFFER_DEPTH, depthRb);
-      }
-
-
-      /* now add any/all software-based renderbuffers we may need */
-      _mesa_add_soft_renderbuffers(&intel_fb->Base,
-                                   GL_FALSE, /* never sw color */
-                                   GL_FALSE, /* never sw depth */
-                                   swStencil, mesaVis->accumRedBits > 0,
-                                   GL_FALSE, /* never sw alpha */
-                                   GL_FALSE  /* never sw aux */ );
-      driDrawPriv->driverPrivate = (void *) intel_fb;
-#else
-      struct st_framebuffer *stfb;
-
-      stfb = st_create_framebuffer(mesaVis);
-
+      struct st_framebuffer *stfb = st_create_framebuffer(mesaVis);
       driDrawPriv->driverPrivate = (void *) stfb;
-#endif
-
       return GL_TRUE;
    }
 }
