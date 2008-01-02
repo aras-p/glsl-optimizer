7d6d6652d58c542be07707c9a9becb8163f1ba8a
Brian <brian.paul@tungstengraphics.com>
2008-01-02 07:18:46 -0700

    additional GL_COLOR_ATTACHMENTx_EXT cases (bug 13767)

--- src/mesa/main/buffers.c
+++ src/mesa/main/buffers.c
@@ -275,6 +275,14 @@ draw_buffer_enum_to_bitmask(GLenum buffer)
          return BUFFER_BIT_COLOR2;
       case GL_COLOR_ATTACHMENT3_EXT:
          return BUFFER_BIT_COLOR3;
+      case GL_COLOR_ATTACHMENT4_EXT:
+         return BUFFER_BIT_COLOR4;
+      case GL_COLOR_ATTACHMENT5_EXT:
+         return BUFFER_BIT_COLOR5;
+      case GL_COLOR_ATTACHMENT6_EXT:
+         return BUFFER_BIT_COLOR6;
+      case GL_COLOR_ATTACHMENT7_EXT:
+         return BUFFER_BIT_COLOR7;
       default:
          /* error */
          return BAD_MASK;
