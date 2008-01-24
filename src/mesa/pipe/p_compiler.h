2bca9f6457d8f766ef12891657fc2d10ea72ef54
José Fonseca <jrfonseca@tungstengraphics.com>
2008-01-24 19:37:37 +0900

    gallium: Complete integer types.

--- src/mesa/pipe/p_compiler.h
+++ src/mesa/pipe/p_compiler.h
@@ -52,6 +52,9 @@ typedef unsigned long long uint64;
 
 #if defined(__MSC__)
 
+typedef char               int8_t;
+typedef unsigned char      uint8_t;
+typedef short              int16_t;
 typedef unsigned short     uint16_t;
 typedef long               int32_t;
 typedef unsigned long      uint32_t;
