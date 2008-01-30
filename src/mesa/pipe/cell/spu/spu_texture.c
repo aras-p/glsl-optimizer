1af8e381af83ba1344f063a52e9fbf932a77e5f5
Brian <brian.paul@tungstengraphics.com>
2008-01-30 15:26:51 -0700

    Cell: start to SIMD-ize triangle attribute interpolation
    Using the spu_add(), etc intrinsics.
    About a 15% speed-up with some tests.

--- src/mesa/pipe/cell/spu/spu_texture.c
+++ src/mesa/pipe/cell/spu/spu_texture.c
@@ -128,11 +128,11 @@ get_tex_tile(uint i, uint j)
  * XXX this is extremely primitive for now.
  */
 uint
-sample_texture(const float *texcoord)
+sample_texture(float4 texcoord)
 {
    /* wrap/repeat */
-   uint i = (uint) (texcoord[0] * spu.texture.width) % spu.texture.width;
-   uint j = (uint) (texcoord[1] * spu.texture.height) % spu.texture.height;
+   uint i = (uint) (texcoord.f[0] * spu.texture.width) % spu.texture.width;
+   uint j = (uint) (texcoord.f[1] * spu.texture.height) % spu.texture.height;
    uint pos = get_tex_tile(i, j);
    uint texel = tex_tiles[pos].t32[j % TILE_SIZE][i % TILE_SIZE];
    return texel;
