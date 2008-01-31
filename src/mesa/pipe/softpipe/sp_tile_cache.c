6c59de9a7bcc025ba070b854d79bf4fb8dcccabf
Brian <brian.paul@tungstengraphics.com>
2008-01-31 13:32:29 -0700

    gallium: fix get/put typo regression
    This came from commit f3aa4de034b0d791ce2e38e8aeb3b3abdb4e3b50 on 1/22/08.
    Fixes strange Z buffer glitches seen in progs/glsl/texdemo1.c

--- src/mesa/pipe/softpipe/sp_tile_cache.c
+++ src/mesa/pipe/softpipe/sp_tile_cache.c
@@ -415,8 +415,8 @@ sp_get_cached_tile(struct softpipe_context *softpipe,
          /* put dirty tile back in framebuffer */
          if (tc->depth_stencil) {
             pipe_put_tile_raw(pipe, ps,
-                           tile->x, tile->y, TILE_SIZE, TILE_SIZE,
-                           tile->data.depth32, 0/*STRIDE*/);
+                              tile->x, tile->y, TILE_SIZE, TILE_SIZE,
+                              tile->data.depth32, 0/*STRIDE*/);
          }
          else {
             pipe_put_tile_rgba(pipe, ps,
@@ -441,9 +441,9 @@ sp_get_cached_tile(struct softpipe_context *softpipe,
       else {
          /* get new tile data from surface */
          if (tc->depth_stencil) {
-            pipe_put_tile_raw(pipe, ps,
-                           tile->x, tile->y, TILE_SIZE, TILE_SIZE,
-                           tile->data.depth32, 0/*STRIDE*/);
+            pipe_get_tile_raw(pipe, ps,
+                              tile->x, tile->y, TILE_SIZE, TILE_SIZE,
+                              tile->data.depth32, 0/*STRIDE*/);
          }
          else {
             pipe_get_tile_rgba(pipe, ps,
