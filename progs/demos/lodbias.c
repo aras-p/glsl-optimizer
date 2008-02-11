5b80529b6081bcff42ec20a096506c441729d39c
Brian <brian.paul@tungstengraphics.com>
2008-02-11 09:46:10 -0700

    press 0,1,2,etc keys for specific bias values

--- progs/demos/lodbias.c
+++ progs/demos/lodbias.c
@@ -159,6 +159,18 @@ static void Key( unsigned char key, int x, int y )
       case 'B':
          Bias += 10;
          break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+         Bias = 100.0 * (key - '0');
+         break;
       case 27:
          exit(0);
          break;
