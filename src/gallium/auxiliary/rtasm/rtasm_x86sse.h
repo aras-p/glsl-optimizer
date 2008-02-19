d2f6c9ab10656f6ecda131a6785a60565026d249
José Fonseca <jrfonseca@tungstengraphics.com>
2008-02-19 12:05:32 +0900

    Add copyright headers to all rtasm source files.

--- src/gallium/auxiliary/rtasm/rtasm_x86sse.h
+++ src/gallium/auxiliary/rtasm/rtasm_x86sse.h
@@ -1,6 +1,28 @@
+/**************************************************************************
+ *
+ * Copyright (C) 1999-2005  Brian Paul   All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * BRIAN PAUL BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **************************************************************************/
 
-#ifndef _X86SSE_H_
-#define _X86SSE_H_
+#ifndef _RTASM_X86SSE_H_
+#define _RTASM_X86SSE_H_
 
 #if defined(__i386__) || defined(__386__)
 
