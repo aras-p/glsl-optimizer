6e3dde8a5409dce827e81200ca4ce8e01b1ee0ab
Christoph Brill <egore911@egore911.de>
2008-02-26 22:10:54 +0100

    [r300] fix wow-pinkness regression. Fixed by Markus Amsler

--- src/mesa/drivers/dri/r300/r300_state.c
+++ src/mesa/drivers/dri/r300/r300_state.c
@@ -711,7 +711,7 @@ static void r300Fogfv(GLcontext * ctx, GLenum pname, const GLfloat * param)
 			R300_STATECHANGE(r300, fogs);
 			r300->hw.fogs.cmd[R300_FOGS_STATE] =
 			    (r300->hw.fogs.
-			     cmd[FG_FOG_BLEND] & ~FG_FOG_BLEND_FN_MASK) |
+			     cmd[R300_FOGS_STATE] & ~FG_FOG_BLEND_FN_MASK) |
 			    FG_FOG_BLEND_FN_LINEAR;
 
 			if (ctx->Fog.Start == ctx->Fog.End) {
@@ -729,7 +729,7 @@ static void r300Fogfv(GLcontext * ctx, GLenum pname, const GLfloat * param)
 			R300_STATECHANGE(r300, fogs);
 			r300->hw.fogs.cmd[R300_FOGS_STATE] =
 			    (r300->hw.fogs.
-			     cmd[FG_FOG_BLEND] & ~FG_FOG_BLEND_FN_MASK) |
+			     cmd[R300_FOGS_STATE] & ~FG_FOG_BLEND_FN_MASK) |
 			    FG_FOG_BLEND_FN_EXP;
 			fogScale.f = 0.0933 * ctx->Fog.Density;
 			fogStart.f = 0.0;
@@ -738,7 +738,7 @@ static void r300Fogfv(GLcontext * ctx, GLenum pname, const GLfloat * param)
 			R300_STATECHANGE(r300, fogs);
 			r300->hw.fogs.cmd[R300_FOGS_STATE] =
 			    (r300->hw.fogs.
-			     cmd[FG_FOG_BLEND] & ~FG_FOG_BLEND_FN_MASK) |
+			     cmd[R300_FOGS_STATE] & ~FG_FOG_BLEND_FN_MASK) |
 			    FG_FOG_BLEND_FN_EXP2;
 			fogScale.f = 0.3 * ctx->Fog.Density;
 			fogStart.f = 0.0;
@@ -803,7 +803,7 @@ static void r300SetFogState(GLcontext * ctx, GLboolean state)
 
 	R300_STATECHANGE(r300, fogs);
 	if (state) {
-		r300->hw.fogs.cmd[FG_FOG_BLEND] |= FG_FOG_BLEND_ENABLE;
+		r300->hw.fogs.cmd[R300_FOGS_STATE] |= FG_FOG_BLEND_ENABLE;
 
 		r300Fogfv(ctx, GL_FOG_MODE, NULL);
 		r300Fogfv(ctx, GL_FOG_DENSITY, &ctx->Fog.Density);
@@ -811,7 +811,7 @@ static void r300SetFogState(GLcontext * ctx, GLboolean state)
 		r300Fogfv(ctx, GL_FOG_END, &ctx->Fog.End);
 		r300Fogfv(ctx, GL_FOG_COLOR, ctx->Fog.Color);
 	} else {
-		r300->hw.fogs.cmd[FG_FOG_BLEND] &= ~FG_FOG_BLEND_ENABLE;
+		r300->hw.fogs.cmd[R300_FOGS_STATE] &= ~FG_FOG_BLEND_ENABLE;
 	}
 }
 
