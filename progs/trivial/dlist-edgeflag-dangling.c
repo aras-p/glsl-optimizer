fe1c9c05855e364f7d9e0e2d1f5781440ae16034
Brian <brian.paul@tungstengraphics.com>
2008-04-04 09:37:21 -0600

    mesa: add missing glPush/PopMatrix() calls

--- progs/trivial/dlist-edgeflag-dangling.c
+++ progs/trivial/dlist-edgeflag-dangling.c
@@ -88,6 +88,7 @@ static void Draw(void)
 {
    glClear(GL_COLOR_BUFFER_BIT); 
 
+   glPushMatrix();
    glColor3f(0,.9,0); 
    glEdgeFlag(0);
    glCallList(list);
@@ -95,6 +96,7 @@ static void Draw(void)
    glRotatef(45,0,0,1);
    glColor3f(1,0,1); 
    glCallList(list);
+   glPopMatrix();
 
    glFlush();
 
