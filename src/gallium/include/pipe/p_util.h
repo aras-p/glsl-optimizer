f30f3206121c56e5c50ccafb1467a8301044d411
Keith Whitwell <keith@tungstengraphics.com>
2008-04-21 12:38:14 +0100

    util: add wrappers for float math functions on windows

--- src/gallium/include/pipe/p_util.h
+++ src/gallium/include/pipe/p_util.h
@@ -402,6 +402,52 @@ extern void pipe_copy_rect(ubyte * dst, unsigned cpp, unsigned dst_pitch,
                            int src_pitch, unsigned src_x, int src_y);
 
 
+
+#ifdef WIN32
+
+static INLINE float cosf( float f ) 
+{
+   return (float) cos( (double) f );
+}
+
+static INLINE float sinf( float f ) 
+{
+   return (float) sin( (double) f );
+}
+
+static INLINE float ceilf( float f ) 
+{
+   return (float) ceil( (double) f );
+}
+
+static INLINE float floorf( float f ) 
+{
+   return (float) floor( (double) f );
+}
+
+static INLINE float powf( float f, float g ) 
+{
+   return (float) pow( (double) f, (double) g );
+}
+
+static INLINE float sqrtf( float f ) 
+{
+   return (float) sqrt( (double) f );
+}
+
+static INLINE float fabsf( float f ) 
+{
+   return (float) fabs( (double) f );
+}
+
+static INLINE float logf( float f ) 
+{
+   return (float) cos( (double) f );
+}
+
+#endif
+
+
 #ifdef __cplusplus
 }
 #endif
