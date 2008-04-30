dadb11f5d96354975f1c9f7c8134bb4727129249
José Fonseca <jrfonseca@tungstengraphics.com>
2008-05-01 03:43:19 +0900

    gallium: Fix release build.

--- src/gallium/auxiliary/util/p_debug.c
+++ src/gallium/auxiliary/util/p_debug.c
@@ -413,6 +413,7 @@ char *pf_sprint_name( char *str, enum pipe_format format )
 }
 
 
+#ifdef DEBUG
 void debug_print_format(const char *msg, unsigned fmt )
 {
    char fmtstr[80];
@@ -421,3 +422,4 @@ void debug_print_format(const char *msg, unsigned fmt )
 
    debug_printf("%s: %s\n", msg, fmtstr); 
 }
+#endif
