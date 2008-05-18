5976a6a75cfa6749db306111a601d99c99cc00a4
Brian Paul <brian.paul@tungstengraphics.com>
2008-05-18 15:41:36 -0600

    added div by zero check for Fog.End/Start (from gallium-0.1)

--- src/mesa/shader/prog_statevars.c
+++ src/mesa/shader/prog_statevars.c
@@ -253,7 +253,8 @@ _mesa_fetch_state(GLcontext *ctx, const gl_state_index state[],
       value[0] = ctx->Fog.Density;
       value[1] = ctx->Fog.Start;
       value[2] = ctx->Fog.End;
-      value[3] = 1.0F / (ctx->Fog.End - ctx->Fog.Start);
+      value[3] = (ctx->Fog.End == ctx->Fog.Start)
+         ? 1.0 : 1.0F / (ctx->Fog.End - ctx->Fog.Start);
       return;
    case STATE_CLIPPLANE:
       {
