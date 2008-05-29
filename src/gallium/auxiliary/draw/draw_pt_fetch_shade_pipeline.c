4a7198fdcfe3256bdefff5d1d766327ae7f18b35
José Fonseca <jrfonseca@tungstengraphics.com>
2008-05-29 22:20:51 +0900

    psb: Make msvc happy.
    Conflicts:
    
    	src/gallium/auxiliary/draw/draw_pt_fetch_shade_pipeline.c

--- src/gallium/auxiliary/draw/draw_pt_fetch_shade_pipeline.c
+++ src/gallium/auxiliary/draw/draw_pt_fetch_shade_pipeline.c
@@ -81,9 +81,9 @@ static void fetch_pipeline_prepare( struct draw_pt_middle_end *middle,
     * but gl vs dx9 clip spaces.
     */
    draw_pt_post_vs_prepare( fpme->post_vs,
-			    draw->bypass_clipping,
-			    draw->identity_viewport,
-			    draw->rasterizer->gl_rasterization_rules );
+			    (boolean)draw->bypass_clipping,
+			    (boolean)draw->identity_viewport,
+			    (boolean)draw->rasterizer->gl_rasterization_rules );
 			    
 
    if (!(opt & PT_PIPELINE)) 
