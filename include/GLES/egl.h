df8f9a16906360b60b96a6e099de8d24725dbd2d
Brian Paul <brian.paul@tungstengraphics.com>
2008-06-04 10:03:39 -0600

    egl: created GLES/egl.h backward-compatibility header (see comments inside)

---
+++ include/GLES/egl.h
@@ -0,0 +1,6 @@
+/* For compatibility with early OpenGL ES 1.0 implementations that
+ * included gl.h in egl.h (and apps that only included egl.h)
+ * See: http://www.khronos.org/registry/implementers_guide.html
+ */
+#include <EGL/egl.h>
+#include <GLES/gl.h>
