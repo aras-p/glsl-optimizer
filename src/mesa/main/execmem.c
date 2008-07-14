2f5cd68797ee4a159fa0a5fc4d315e4d3f2f644e
Blair Sadewitz <blair.sadewitz.gmail.com>
2008-07-14 08:16:46 -0600

    mesa: also check for __NetBSD__

--- src/mesa/main/execmem.c
+++ src/mesa/main/execmem.c
@@ -36,7 +36,7 @@
 
 
 
-#if defined(__linux__) || defined(__OpenBSD__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(_NetBSD__)
 
 /*
  * Allocate a large block of memory which can hold code then dole it out
