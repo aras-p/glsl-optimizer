7d13dded5c581c2a1741252b1ef04f7ae4a9627b
Brian Paul <brian.paul@tungstengraphics.com>
2008-07-16 10:23:28 -0600

    mesa: add GL_POLYGON_OFFSET_POINT/LINE/FILL queries, remove GL_TEXTURE_ENV_COLOR, GL_TEXTURE_ENV_MODE
    Issues found by Bob Ellison.

--- src/mesa/main/get.c
+++ src/mesa/main/get.c
@@ -753,6 +753,15 @@ _mesa_GetBooleanv( GLenum pname, GLboolean *params )
       case GL_POLYGON_OFFSET_UNITS:
          params[0] = FLOAT_TO_BOOLEAN(ctx->Polygon.OffsetUnits );
          break;
+      case GL_POLYGON_OFFSET_POINT:
+         params[0] = ctx->Polygon.OffsetPoint;
+         break;
+      case GL_POLYGON_OFFSET_LINE:
+         params[0] = ctx->Polygon.OffsetLine;
+         break;
+      case GL_POLYGON_OFFSET_FILL:
+         params[0] = ctx->Polygon.OffsetFill;
+         break;
       case GL_POLYGON_SMOOTH:
          params[0] = ctx->Polygon.SmoothFlag;
          break;
@@ -894,18 +903,6 @@ _mesa_GetBooleanv( GLenum pname, GLboolean *params )
       case GL_TEXTURE_BINDING_2D_ARRAY_EXT:
          CHECK_EXT1(MESA_texture_array, "GetBooleanv");
          params[0] = INT_TO_BOOLEAN(ctx->Texture.Unit[ctx->Texture.CurrentUnit].Current2DArray->Name);
-         break;
-      case GL_TEXTURE_ENV_COLOR:
-         {
-         const GLfloat *color = ctx->Texture.Unit[ctx->Texture.CurrentUnit].EnvColor;
-         params[0] = FLOAT_TO_BOOLEAN(color[0]);
-         params[1] = FLOAT_TO_BOOLEAN(color[1]);
-         params[2] = FLOAT_TO_BOOLEAN(color[2]);
-         params[3] = FLOAT_TO_BOOLEAN(color[3]);
-         }
-         break;
-      case GL_TEXTURE_ENV_MODE:
-         params[0] = ENUM_TO_BOOLEAN(ctx->Texture.Unit[ctx->Texture.CurrentUnit].EnvMode);
          break;
       case GL_TEXTURE_GEN_S:
          params[0] = ((ctx->Texture.Unit[ctx->Texture.CurrentUnit].TexGenEnabled & S_BIT) ? 1 : 0);
@@ -2604,6 +2601,15 @@ _mesa_GetFloatv( GLenum pname, GLfloat *params )
       case GL_POLYGON_OFFSET_UNITS:
          params[0] = ctx->Polygon.OffsetUnits ;
          break;
+      case GL_POLYGON_OFFSET_POINT:
+         params[0] = BOOLEAN_TO_FLOAT(ctx->Polygon.OffsetPoint);
+         break;
+      case GL_POLYGON_OFFSET_LINE:
+         params[0] = BOOLEAN_TO_FLOAT(ctx->Polygon.OffsetLine);
+         break;
+      case GL_POLYGON_OFFSET_FILL:
+         params[0] = BOOLEAN_TO_FLOAT(ctx->Polygon.OffsetFill);
+         break;
       case GL_POLYGON_SMOOTH:
          params[0] = BOOLEAN_TO_FLOAT(ctx->Polygon.SmoothFlag);
          break;
@@ -2745,18 +2751,6 @@ _mesa_GetFloatv( GLenum pname, GLfloat *params )
       case GL_TEXTURE_BINDING_2D_ARRAY_EXT:
          CHECK_EXT1(MESA_texture_array, "GetFloatv");
          params[0] = (GLfloat)(ctx->Texture.Unit[ctx->Texture.CurrentUnit].Current2DArray->Name);
-         break;
-      case GL_TEXTURE_ENV_COLOR:
-         {
-         const GLfloat *color = ctx->Texture.Unit[ctx->Texture.CurrentUnit].EnvColor;
-         params[0] = color[0];
-         params[1] = color[1];
-         params[2] = color[2];
-         params[3] = color[3];
-         }
-         break;
-      case GL_TEXTURE_ENV_MODE:
-         params[0] = ENUM_TO_FLOAT(ctx->Texture.Unit[ctx->Texture.CurrentUnit].EnvMode);
          break;
       case GL_TEXTURE_GEN_S:
          params[0] = BOOLEAN_TO_FLOAT(((ctx->Texture.Unit[ctx->Texture.CurrentUnit].TexGenEnabled & S_BIT) ? 1 : 0));
@@ -4455,6 +4449,15 @@ _mesa_GetIntegerv( GLenum pname, GLint *params )
       case GL_POLYGON_OFFSET_UNITS:
          params[0] = IROUND(ctx->Polygon.OffsetUnits );
          break;
+      case GL_POLYGON_OFFSET_POINT:
+         params[0] = BOOLEAN_TO_INT(ctx->Polygon.OffsetPoint);
+         break;
+      case GL_POLYGON_OFFSET_LINE:
+         params[0] = BOOLEAN_TO_INT(ctx->Polygon.OffsetLine);
+         break;
+      case GL_POLYGON_OFFSET_FILL:
+         params[0] = BOOLEAN_TO_INT(ctx->Polygon.OffsetFill);
+         break;
       case GL_POLYGON_SMOOTH:
          params[0] = BOOLEAN_TO_INT(ctx->Polygon.SmoothFlag);
          break;
@@ -4596,18 +4599,6 @@ _mesa_GetIntegerv( GLenum pname, GLint *params )
       case GL_TEXTURE_BINDING_2D_ARRAY_EXT:
          CHECK_EXT1(MESA_texture_array, "GetIntegerv");
          params[0] = ctx->Texture.Unit[ctx->Texture.CurrentUnit].Current2DArray->Name;
-         break;
-      case GL_TEXTURE_ENV_COLOR:
-         {
-         const GLfloat *color = ctx->Texture.Unit[ctx->Texture.CurrentUnit].EnvColor;
-         params[0] = FLOAT_TO_INT(color[0]);
-         params[1] = FLOAT_TO_INT(color[1]);
-         params[2] = FLOAT_TO_INT(color[2]);
-         params[3] = FLOAT_TO_INT(color[3]);
-         }
-         break;
-      case GL_TEXTURE_ENV_MODE:
-         params[0] = ENUM_TO_INT(ctx->Texture.Unit[ctx->Texture.CurrentUnit].EnvMode);
          break;
       case GL_TEXTURE_GEN_S:
          params[0] = BOOLEAN_TO_INT(((ctx->Texture.Unit[ctx->Texture.CurrentUnit].TexGenEnabled & S_BIT) ? 1 : 0));
