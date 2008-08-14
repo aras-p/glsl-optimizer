a7ea6bae4e618e28636909ac2db7783632bc81b8
Patrice Mandin <pmandin@caramail.com>
2008-08-14 16:36:52 +0200

    nv30: does not support mirror clamp, only mirror repeat

--- src/gallium/drivers/nv30/nv30_screen.c
+++ src/gallium/drivers/nv30/nv30_screen.c
@@ -55,6 +55,10 @@ nv30_screen_get_param(struct pipe_screen *pscreen, int param)
 		return 10;
 	case PIPE_CAP_MAX_TEXTURE_CUBE_LEVELS:
 		return 13;
+	case PIPE_CAP_TEXTURE_MIRROR_CLAMP:
+		return 0;
+	case PIPE_CAP_TEXTURE_MIRROR_REPEAT:
+		return 1;
 	case NOUVEAU_CAP_HW_VTXBUF:
 	case NOUVEAU_CAP_HW_IDXBUF:
 		return 1;
