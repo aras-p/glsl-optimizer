b36f90657370296b45c27d33d60c89fa31dc1d76
Brian Paul <brian.paul@tungstengraphics.com>
2008-09-01 13:08:12 -0600

    egl: additional error checking in _eglBind/ReleaseTexImage()

--- src/egl/main/eglsurface.h
+++ src/egl/main/eglsurface.h
@@ -16,6 +16,7 @@ struct _egl_surface
    /* May need reference counting here */
    EGLBoolean IsBound;
    EGLBoolean DeletePending;
+   EGLBoolean BoundToTexture;
 
    EGLint Type; /* one of EGL_WINDOW_BIT, EGL_PIXMAP_BIT or EGL_PBUFFER_BIT */
    EGLint Width, Height;
