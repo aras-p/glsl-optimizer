8d41e4536dfde34b55228cab6ddb081ad6252184
Chris Rankin <rankincj@yahoo.com>
2008-09-18 16:48:11 -0600

    mesa: fix asst path/include mistakes in prev commits

--- src/mesa/drivers/dri/swrast/swrast.c
+++ src/mesa/drivers/dri/swrast/swrast.c
@@ -39,7 +39,7 @@
 #include "swrast/swrast.h"
 #include "swrast_setup/swrast_setup.h"
 #include "tnl/tnl.h"
-#include "tnl/tcontext.h"
+#include "tnl/t_context.h"
 #include "tnl/t_pipeline.h"
 #include "vbo/vbo.h"
 #include "drivers/common/driverfuncs.h"
