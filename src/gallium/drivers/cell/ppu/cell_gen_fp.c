15fceac0404f450f026f10bd2f4bdd0c939b5d00
Jonathan White <jwhite@tungstengraphics.com>
2008-09-18 11:11:49 -0600

    cell: Fix bug with complement logic for SGE and SLE

--- src/gallium/drivers/cell/ppu/cell_gen_fp.c
+++ src/gallium/drivers/cell/ppu/cell_gen_fp.c
@@ -716,8 +716,8 @@ emit_SGE(struct codegen *gen, const struct tgsi_full_instruction *inst)
          spe_fcgt(gen->f, d_reg, s2_reg, s1_reg);
 
          /* convert d from 0x0/0xffffffff to 0.0/1.0 */
-         /* d = d & ~one_reg */
-         spe_andc(gen->f, d_reg, d_reg, get_const_one_reg(gen));
+         /* d = ~d & one_reg */
+         spe_andc(gen->f, d_reg, get_const_one_reg(gen), d_reg);
 
          store_dest_reg(gen, d_reg, ch, &inst->FullDstRegisters[0]);
          free_itemps(gen);
@@ -747,8 +747,8 @@ emit_SLE(struct codegen *gen, const struct tgsi_full_instruction *inst)
          spe_fcgt(gen->f, d_reg, s1_reg, s2_reg);
 
          /* convert d from 0x0/0xffffffff to 0.0/1.0 */
-         /* d = d & ~one_reg */
-         spe_andc(gen->f, d_reg, d_reg, get_const_one_reg(gen));
+         /* d = ~d & one_reg */
+         spe_andc(gen->f, d_reg, get_const_one_reg(gen), d_reg);
 
          store_dest_reg(gen, d_reg, ch, &inst->FullDstRegisters[0]);
          free_itemps(gen);
@@ -798,6 +798,38 @@ emit_SNE(struct codegen *gen, const struct tgsi_full_instruction *inst)
    int ch;
 
    spe_comment(gen->f, -4, "SNE:");
+
+   for (ch = 0; ch < 4; ch++) {
+      if (inst->FullDstRegisters[0].DstRegister.WriteMask & (1 << ch)) {
+         int s1_reg = get_src_reg(gen, ch, &inst->FullSrcRegisters[0]);
+         int s2_reg = get_src_reg(gen, ch, &inst->FullSrcRegisters[1]);
+         int d_reg = get_dst_reg(gen, ch, &inst->FullDstRegisters[0]);
+
+         /* d = (s1 != s2) */
+         spe_fceq(gen->f, d_reg, s1_reg, s2_reg);
+         spe_nor(gen->f, d_reg, d_reg, d_reg);
+
+         /* convert d from 0x0/0xffffffff to 0.0/1.0 */
+         /* d = d & one_reg */
+         spe_and(gen->f, d_reg, d_reg, get_const_one_reg(gen));
+
+         store_dest_reg(gen, d_reg, ch, &inst->FullDstRegisters[0]);
+         free_itemps(gen);
+      }
+   }
+
+   return true;
+}
+
+/**
+ * Emit compare.  See emit_SGT for comments.
+ */
+static boolean
+emit_CMP(struct codegen *gen, const struct tgsi_full_instruction *inst)
+{
+   int ch;
+
+   spe_comment(gen->f, -4, "CMP:");
 
    for (ch = 0; ch < 4; ch++) {
       if (inst->FullDstRegisters[0].DstRegister.WriteMask & (1 << ch)) {
@@ -1028,6 +1060,8 @@ emit_instruction(struct codegen *gen,
       return emit_SEQ(gen, inst);
    case TGSI_OPCODE_SNE:
       return emit_SNE(gen, inst);
+   case TGSI_OPCODE_CMP:
+      return emit_CMP(gen, inst);
    case TGSI_OPCODE_MAX:
       return emit_MAX(gen, inst);
    case TGSI_OPCODE_MIN:
