e61793a7c01588f2e6235f1271af4c58fa8adad4
Zack Rusin <zack@tungstengraphics.com>
2008-09-21 18:32:09 -0400

    gallium: cleanup the after merge framebuffer update code
    adjust it and make it behave like it used to

--- src/mesa/state_tracker/st_atom_framebuffer.c
+++ src/mesa/state_tracker/st_atom_framebuffer.c
@@ -144,22 +144,13 @@ update_framebuffer_state( struct st_context *st )
 
    cso_set_framebuffer(st->cso_context, framebuffer);
 
-#if 0
-   if (fb->_ColorDrawBufferMask[0] & BUFFER_BIT_FRONT_LEFT) {
+   if (fb->_ColorDrawBufferIndexes[0] == BUFFER_FRONT_LEFT) {
       if (st->frontbuffer_status == FRONT_STATUS_COPY_OF_BACK) {
          /* XXX copy back buf to front? */
       }
       /* we're assuming we'll really draw to the front buffer */
       st->frontbuffer_status = FRONT_STATUS_DIRTY;
    }
-#else
-#if !defined(PIPE_OS_WINDOWS)
-#warning "fix me"
-#endif
-   if (fb->_ColorDrawBufferIndexes[0] == BUFFER_FRONT_LEFT) {
-      st->frontbuffer_status = FRONT_STATUS_DIRTY;
-   }
-#endif
 }
 
 
