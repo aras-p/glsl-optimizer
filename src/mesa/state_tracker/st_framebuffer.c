6d3e2e7e92ada3c430476de7a5dddf529914b4f0
Alan Hourihane <alanh@tungstengraphics.com>
2008-09-26 15:04:09 +0100

    st: export st_get_framebuffer_dimensions() to retrieve     the width & height of the fb

--- src/mesa/state_tracker/st_framebuffer.c
+++ src/mesa/state_tracker/st_framebuffer.c
@@ -301,3 +301,10 @@ void *st_framebuffer_private( struct st_framebuffer *stfb )
    return stfb->Private;
 }
 
+void st_get_framebuffer_dimensions( struct st_framebuffer *stfb,
+				    int *width,
+				    int *height)
+{
+   *width = stfb->Base.Width;
+   *height = stfb->Base.Height;
+}
