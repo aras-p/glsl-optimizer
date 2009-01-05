38b526999c91a82691a8a5f3c2d0b6f071758dfc
Brian Paul <brianp@vmware.com>
2009-01-04 18:38:42 -0700

    cell: add support for PIPE_CAP_TEXTURE_MIRROR_REPEAT query
    We don't really support this texwrap mode yet, but this enables GL 2.1

--- src/gallium/drivers/cell/ppu/cell_screen.c
+++ src/gallium/drivers/cell/ppu/cell_screen.c
@@ -81,6 +81,10 @@ cell_get_param(struct pipe_screen *screen, int param)
       return 8;  /* max 128x128x128 */
    case PIPE_CAP_MAX_TEXTURE_CUBE_LEVELS:
       return CELL_MAX_TEXTURE_LEVELS;
+   case PIPE_CAP_TEXTURE_MIRROR_REPEAT:
+      return 1; /* XXX not really true */
+   case PIPE_CAP_TEXTURE_MIRROR_CLAMP:
+      return 0; /* XXX to do */
    default:
       return 0;
    }
