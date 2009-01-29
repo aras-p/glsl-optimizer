a3b89a39d94d94c5e7f9f5389f852171ad0aeb1f
Jakob Bornecrantz <wallbraker@gmail.com>
2009-01-29 01:30:37 +0100

    i915: Don't dump batchbuffer on flush in context

--- src/gallium/drivers/i915simple/i915_batch.h
+++ src/gallium/drivers/i915simple/i915_batch.h
@@ -109,7 +109,6 @@ i915_batchbuffer_flush( struct i915_batchbuffer *batch,
    i915_batchbuffer_reloc( i915->batch, buf, flags, delta )
 
 #define FLUSH_BATCH(fence) do {				\
-   i915_dump_batchbuffer(i915);				\
    i915->winsys->batch_flush( i915->winsys, fence );	\
    i915->hardware_dirty = ~0;				\
 } while (0)
