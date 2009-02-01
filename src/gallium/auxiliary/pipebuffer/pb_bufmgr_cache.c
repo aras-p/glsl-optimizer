4ad190c96f2ad4364537e700dcb381c9dceec35c
José Fonseca <jfonseca@vmware.com>
2009-02-01 10:27:54 +0000

    pipebuffer: Drop (most of) pipe winsys stuff.

--- src/gallium/auxiliary/pipebuffer/pb_bufmgr_cache.c
+++ src/gallium/auxiliary/pipebuffer/pb_bufmgr_cache.c
@@ -36,7 +36,6 @@
 
 #include "pipe/p_compiler.h"
 #include "pipe/p_debug.h"
-#include "pipe/internal/p_winsys_screen.h"
 #include "pipe/p_thread.h"
 #include "util/u_memory.h"
 #include "util/u_double_list.h"
--- src/gallium/auxiliary/pipebuffer/pb_bufmgr_debug.c
+++ src/gallium/auxiliary/pipebuffer/pb_bufmgr_debug.c
@@ -35,7 +35,6 @@
 
 #include "pipe/p_compiler.h"
 #include "pipe/p_debug.h"
-#include "pipe/internal/p_winsys_screen.h"
 #include "pipe/p_thread.h"
 #include "util/u_math.h"
 #include "util/u_memory.h"
