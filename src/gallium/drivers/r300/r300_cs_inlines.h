33d798c4eab57293336082c7d011aa27af693bbb
Corbin Simpson <MostAwesomeDude@gmail.com>
2009-02-02 15:39:30 -0800

    r300: Move some registers around.
    This fixes r500 hangs.

--- src/gallium/drivers/r300/r300_cs_inlines.h
+++ src/gallium/drivers/r300/r300_cs_inlines.h
@@ -27,8 +27,10 @@
 #ifdef R300_CS_H
 
 #define R300_PACIFY do { \
+    OUT_CS_REG(R300_SC_SCREENDOOR, 0x0); \
     OUT_CS_REG(RADEON_WAIT_UNTIL, (1 << 15) | (1 << 17) | \
         (1 << 18) | (1 << 31)); \
+    OUT_CS_REG(R300_SC_SCREENDOOR, 0xffffff); \
 } while (0)
 
 
