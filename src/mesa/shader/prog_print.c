6ce0c6e743b27cc284e8f164585cfe5892cd662a
Brian Paul <brianp@vmware.com>
2009-02-06 10:20:33 -0700

    mesa: tweak output of _mesa_write_shader_to_file()
    Enclose GPU code in comments to the file can be re-fed back into GLSL compiler.

--- src/mesa/shader/prog_print.c
+++ src/mesa/shader/prog_print.c
@@ -902,7 +902,9 @@ _mesa_write_shader_to_file(const struct gl_shader *shader)
    }
    else {
       fprintf(f, "/* GPU code */\n");
+      fprintf(f, "/*\n");
       _mesa_fprint_program_opt(f, shader->Program, PROG_PRINT_DEBUG, GL_TRUE);
+      fprintf(f, "*/\n");
    }
 
    fclose(f);
