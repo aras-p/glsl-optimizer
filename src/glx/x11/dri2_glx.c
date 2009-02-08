680c708deeb4d18474cf7ba86c587dede51d3633
Eric Anholt <eric@anholt.net>
2009-02-08 15:22:34 +0100

    dri2: Initialize variables for the getbuffers round-trip reduction.
    Missed setting the initial values which usually didn't hurt at runtime.

--- src/glx/x11/dri2_glx.c
+++ src/glx/x11/dri2_glx.c
@@ -170,6 +170,7 @@ static __GLXDRIdrawable *dri2CreateDrawable(__GLXscreenConfigs *psc,
     pdraw->base.xDrawable = xDrawable;
     pdraw->base.drawable = drawable;
     pdraw->base.psc = psc;
+    pdraw->bufferCount = 0;
     pdraw->configureSeqno = ~0;
 
     DRI2CreateDrawable(psc->dpy, xDrawable);
@@ -440,6 +441,7 @@ _X_HIDDEN __GLXDRIdisplay *dri2CreateDisplay(Display *dpy)
 					  dri2ConfigureNotifyProc);
 
     pdp->driPatch = 0;
+    pdp->configureSeqno = 0;
 
     pdp->base.destroyDisplay = dri2DestroyDisplay;
     pdp->base.createScreen = dri2CreateScreen;
