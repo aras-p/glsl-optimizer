c936a6083e2a0bace5f3937bd2c90371aedd9bea
Brian Paul <brianp@vmware.com>
2009-02-10 10:12:44 -0700

    intel: minor reformatting, comments

--- src/mesa/drivers/dri/intel/intel_swapbuffers.c
+++ src/mesa/drivers/dri/intel/intel_swapbuffers.c
@@ -43,7 +43,6 @@
 GLuint
 intelFixupVblank(struct intel_context *intel, __DRIdrawablePrivate *dPriv)
 {
-
    if (!intel->intelScreen->driScrnPriv->dri2.enabled &&
        intel->intelScreen->driScrnPriv->ddx_version.minor >= 7) {
       volatile drm_i915_sarea_t *sarea = intel->sarea;
@@ -77,11 +76,14 @@ intelFixupVblank(struct intel_context *intel, __DRIdrawablePrivate *dPriv)
 
       return flags;
    } else {
-	return dPriv->vblFlags & ~VBLANK_FLAG_SECONDARY;
+      return dPriv->vblFlags & ~VBLANK_FLAG_SECONDARY;
    }
 }
 
 
+/**
+ * Called from driSwapBuffers()
+ */
 void
 intelSwapBuffers(__DRIdrawablePrivate * dPriv)
 {
@@ -130,7 +132,6 @@ intelSwapBuffers(__DRIdrawablePrivate * dPriv)
 	 intel_fb->swap_ust = ust;
       }
       drmCommandNone(intel->driFd, DRM_I915_GEM_THROTTLE);
-
    }
    else {
       /* XXX this shouldn't be an error but we can't handle it for now */
@@ -138,6 +139,10 @@ intelSwapBuffers(__DRIdrawablePrivate * dPriv)
    }
 }
 
+
+/**
+ * Called from driCopySubBuffer()
+ */
 void
 intelCopySubBuffer(__DRIdrawablePrivate * dPriv, int x, int y, int w, int h)
 {
