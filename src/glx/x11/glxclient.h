11da7e02aa3dba192aa3d95e9debec620133a41c
Michel Dänzer <daenzer@vmware.com>
2009-03-25 15:53:28 +0100

    Revert "dri2: Avoid round-tripping on DRI2GetBuffers for the same set of buffers."
    This scheme breaks when the display connection doesn't receive ConfigureNotify
    events. This caused reporoducible problems (cropped / misplaced output) when
    starting a 3D application in a guest operating system in VMware Workstation.
    
    This reverts commit dd1c68f15123a889a3ce9d2afe724e272d163e32.
    
    Conflicts:
    
    	src/glx/x11/dri2_glx.c

--- src/glx/x11/glxclient.h
+++ src/glx/x11/glxclient.h
@@ -609,7 +609,6 @@ extern void __glXSendLargeCommand(__GLXcontext *, const GLvoid *, GLint,
 				  const GLvoid *, GLint);
 
 /* Initialize the GLX extension for dpy */
-extern __GLXdisplayPrivate * __glXGetPrivateFromDisplay(Display *dpy);
 extern __GLXdisplayPrivate *__glXInitialize(Display*);
 
 extern void __glXPreferEGL(int state);
