aras-p/glsl-optimizer

commit ed4c6cbe017b4e8bacb7e012d4baaf77a20a2c33

r300: Adapt to the removal of _tnl_ProgramCacheInit() and friends.

src/mesa/drivers/dri/r300/r300_context.c
@@ -355,7 +355,7 @@ GLboolean r300CreateContext(const __GLcontextModes * glVisual,
 	ctx->Const.FragmentProgram.MaxNativeTexIndirections =
 	    PFS_MAX_TEX_INDIRECT;
 	ctx->Const.FragmentProgram.MaxNativeAddressRegs = 0;	/* and these are?? */
-	_tnl_ProgramCacheInit(ctx);
+	ctx->VertexProgram._MaintainTnlProgram = GL_TRUE;
 	ctx->FragmentProgram._MaintainTexEnvProgram = GL_TRUE;
 
 	driInitExtensions(ctx, card_extensions, GL_TRUE);
@@ -501,7 +501,6 @@ void r300DestroyContext(__DRIcontextPrivate * driContextPriv)
 		release_texture_heaps =
 		    (r300->radeon.glCtx->Shared->RefCount == 1);
 		_swsetup_DestroyContext(r300->radeon.glCtx);
-		_tnl_ProgramCacheDestroy(r300->radeon.glCtx);
 		_tnl_DestroyContext(r300->radeon.glCtx);
 		_vbo_DestroyContext(r300->radeon.glCtx);
 		_swrast_DestroyContext(r300->radeon.glCtx);

commit 033d1365e6fa6e57bc11b47831af0cb4b7ad2edf

r300: fix performance regression
This performance regression on non TCL hw was introduced by ed4c6cbe017b4e8bacb7e012d4baaf77a20a2c33.
This patch depends on "r300: always route 4 texcoord components to RS" and "r300: add point attenuation stage for TCL fallbacks".

src/mesa/drivers/dri/r300/r300_context.c
@@ -364,7 +364,10 @@ GLboolean r300CreateContext(const __GLcontextModes * glVisual,
 
 	ctx = r300->radeon.glCtx;
 	r300InitConstValues(ctx, screen);
-	ctx->VertexProgram._MaintainTnlProgram = GL_TRUE;
+
+	if (hw_tcl_on)
+		ctx->VertexProgram._MaintainTnlProgram = GL_TRUE;
+
 	ctx->FragmentProgram._MaintainTexEnvProgram = GL_TRUE;
 
 	/* Initialize the software rasterizer and helper modules.