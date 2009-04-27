3ebcf2dd7489ecaf19a7167892069c4d58c285d8
José Fonseca <jfonseca@vmware.com>
2009-04-27 17:13:40 +0100

    util: Remove unix includes.

--- src/gallium/auxiliary/util/u_time.c
+++ src/gallium/auxiliary/util/u_time.c
@@ -218,10 +218,6 @@ void util_time_sleep(unsigned usecs)
 	   end < start && (curr < end || start <= curr));
 }
 #elif defined(PIPE_SUBSYSTEM_WINDOWS_USER)
-#include <unistd.h>
-#include <fcntl.h>
- 
-
 void util_time_sleep(unsigned usecs)
 {
    Sleep((usecs + 999)/ 1000);
