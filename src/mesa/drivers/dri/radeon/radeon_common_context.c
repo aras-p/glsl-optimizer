dbb56687c83845cc20aee7243e437a32e5c79a70
Nicolai Hähnle <nhaehnle@gmail.com>
2009-06-01 22:17:07 +0200

    radeon: Always initialize front and back renderbuffers if present
    This fixes an assertion in glReadPixels from the front buffer.
    
    Signed-off-by: Nicolai Hähnle <nhaehnle@gmail.com>

--- src/mesa/drivers/dri/radeon/radeon_common_context.c
+++ src/mesa/drivers/dri/radeon/radeon_common_context.c
@@ -487,8 +487,7 @@ radeon_update_renderbuffers(__DRIcontext *context, __DRIdrawable *drawable)
 		struct radeon_renderbuffer *stencil_rb;
 
 		i = 0;
-		if ((radeon->is_front_buffer_rendering || !draw->color_rb[1])
-			&& draw->color_rb[0]) {
+		if (draw->color_rb[0]) {
 			attachments[i++] = __DRI_BUFFER_FRONT_LEFT;
 			attachments[i++] = radeon_bits_per_pixel(draw->color_rb[0]);
 		}
