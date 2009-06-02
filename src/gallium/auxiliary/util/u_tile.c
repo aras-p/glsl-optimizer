840af5fd62edc01769cc3818702ea399a0c68c40
José Fonseca <jfonseca@vmware.com>
2009-06-02 11:46:53 -0700

    util: Support Z24S8/Z24X8 -> unsigned conversion.

--- src/gallium/auxiliary/util/u_tile.c
+++ src/gallium/auxiliary/util/u_tile.c
@@ -1133,6 +1133,21 @@ pipe_get_tile_z(struct pipe_transfer *pt,
          }
       }
       break;
+   case PIPE_FORMAT_Z24S8_UNORM:
+   case PIPE_FORMAT_Z24X8_UNORM:
+      {
+         const uint *ptrc
+            = (const uint *)(map + y * pt->stride + x*4);
+         for (i = 0; i < h; i++) {
+            for (j = 0; j < w; j++) {
+               /* convert 24-bit Z to 32-bit Z */
+               pDest[j] = (ptrc[j] & 0xffffff00) | (ptrc[j] & 0xff);
+            }
+            pDest += dstStride;
+            ptrc += pt->stride/4;
+         }
+      }
+      break;
    case PIPE_FORMAT_Z16_UNORM:
       {
          const ushort *ptrc
