38eddf04ed04966db09b67de4cdabef681dc2696
Eric Anholt <eric@anholt.net>
2009-06-06 07:22:00 +0000

    intel: Remove an unneeded hunk that slipped in with texture tiling.
    intel_miptree_pitch_align does this later on.

--- src/mesa/drivers/dri/intel/intel_tex_layout.c
+++ src/mesa/drivers/dri/intel/intel_tex_layout.c
@@ -88,11 +88,6 @@ void i945_miptree_layout_2d( struct intel_context *intel,
 
        if (mip1_width > mt->pitch) {
            mt->pitch = mip1_width;
-
-	   if (tiling == I915_TILING_X)
-	      mt->pitch = ALIGN(mt->pitch * mt->cpp, 512) / mt->cpp;
-	   if (tiling == I915_TILING_Y)
-	      mt->pitch = ALIGN(mt->pitch * mt->cpp, 128) / mt->cpp;
        }
    }
 
