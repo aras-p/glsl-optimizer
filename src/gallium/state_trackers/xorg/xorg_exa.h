21cce6afb03bf9b9adfc6d8a1a446bb3ef22c7a8
Zack Rusin <zack@kde.org>
2009-06-08 00:07:04 -0400

    exa: some infrastucture work for accelerating composite

---
+++ src/gallium/state_trackers/xorg/xorg_exa.h
@@ -0,0 +1,27 @@
+#ifndef XORG_EXA_H
+#define XORG_EXA_H
+
+#include "xorg_tracker.h"
+
+struct exa_context
+{
+    ExaDriverPtr pExa;
+    struct pipe_context *ctx;
+    struct pipe_screen *scrn;
+};
+
+
+struct exa_pixmap_priv
+{
+    int flags;
+    struct pipe_texture *tex;
+    unsigned int color;
+    struct pipe_surface *src_surf; /* for copies */
+
+    struct pipe_transfer *map_transfer;
+    unsigned map_count;
+};
+
+
+
+#endif
--- src/gallium/state_trackers/xorg/xorg_tracker.h
+++ src/gallium/state_trackers/xorg/xorg_tracker.h
@@ -31,11 +31,14 @@
 #ifndef _XORG_TRACKER_H_
 #define _XORG_TRACKER_H_
 
+#include <stdint.h>
 #include <errno.h>
 #include <drm.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
-#include "exa.h"
+#include <xorg-server.h>
+#include <xf86.h>
+#include <exa.h>
 
 #include "pipe/p_screen.h"
 #include "state_tracker/drm_api.h"
