681ede8836746735fbb904edf89b076343507a8b
Dave Airlie <airlied@redhat.com>
2009-07-15 07:10:45 +1000

    radeon: fix unsigned vs signed comparison in stencil code.
    This function takes a GLint not a GLuint, passing in -1
    breaks internally.

--- src/mesa/drivers/dri/radeon/radeon_common.c
+++ src/mesa/drivers/dri/radeon/radeon_common.c
@@ -1482,7 +1482,7 @@ void radeon_clear_tris(GLcontext *ctx, GLbitfield mask)
       if (this_mask & BUFFER_BIT_STENCIL) {
 	 _mesa_Enable(GL_STENCIL_TEST);
 	 _mesa_StencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
-	 _mesa_StencilFuncSeparate(GL_FRONT_AND_BACK, GL_ALWAYS, ctx->Stencil.Clear,
+	 _mesa_StencilFuncSeparate(GL_FRONT_AND_BACK, GL_ALWAYS, (GLint)ctx->Stencil.Clear,
 				   ctx->Stencil.WriteMask[0]);
       } else {
 	 _mesa_Disable(GL_STENCIL_TEST);
