b7042399fd0a1cf66f99340486c03374d15af36c
Patrice Mandin <patmandin@gmail.com>
2009-07-22 22:25:36 +0200

    nouveau: nv30: wrong variable for format

--- src/gallium/drivers/nv30/nv30_screen.c
+++ src/gallium/drivers/nv30/nv30_screen.c
@@ -95,7 +95,7 @@ nv30_screen_surface_format_supported(struct pipe_screen *pscreen,
 		}
 	} else
 	if (tex_usage & PIPE_TEXTURE_USAGE_DEPTH_STENCIL) {
-		switch (tex_usage) {
+		switch (format) {
 		case PIPE_FORMAT_Z24S8_UNORM:
 		case PIPE_FORMAT_Z24X8_UNORM:
 		case PIPE_FORMAT_Z16_UNORM:
