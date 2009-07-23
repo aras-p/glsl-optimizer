ce0ad53281f236424a72ae021f293a3a5ca69217
RALOVICH, Kristóf <tade60@freemail.hu>
2009-07-23 17:03:54 +0200

    glx: cache DRI configs in __GLXscreenConfigsRec

--- src/glx/x11/glxclient.h
+++ src/glx/x11/glxclient.h
@@ -501,6 +501,8 @@ struct __GLXscreenConfigsRec {
 
     __GLXDRIscreen *driScreen;
 
+    const __DRIconfig** driver_configs;
+
 #ifdef __DRI_COPY_SUB_BUFFER
     const __DRIcopySubBufferExtension *driCopySubBuffer;
 #endif
