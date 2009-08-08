a962c07cc3e5fba3be5c08071bc7abc5d840f138
Eric Anholt <eric@anholt.net>
2009-08-07 18:29:56 -0700

    Revert "i965: Disable texture tiling by default."
    This reverts commit b8e638d4895d2d342306bb6443a455f73903ce20.
    
    Now that the known hangs and misrendering issues are fixed, I'm ready to
    start encouraging it by default again.

--- src/mesa/drivers/dri/intel/intel_screen.c
+++ src/mesa/drivers/dri/intel/intel_screen.c
@@ -69,7 +69,11 @@ PUBLIC const char __driConfigOptions[] =
 	 DRI_CONF_DESC_END
       DRI_CONF_OPT_END
 
-      DRI_CONF_TEXTURE_TILING(false)
+#ifdef I915
+     DRI_CONF_TEXTURE_TILING(false)
+#else
+     DRI_CONF_TEXTURE_TILING(true)
+#endif
 
       DRI_CONF_OPT_BEGIN(early_z, bool, false)
 	 DRI_CONF_DESC(en, "Enable early Z in classic mode (unstable, 945-only).")
