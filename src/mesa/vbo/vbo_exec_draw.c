b9b04872d526ed7955f647542399e110ace0325c
Brian Paul <brianp@vmware.com>
2009-08-24 12:43:57 -0600

    vbo: fix divide by zero exception
    Fixes bug 23489.

--- src/mesa/vbo/vbo_exec_draw.c
+++ src/mesa/vbo/vbo_exec_draw.c
@@ -383,7 +383,7 @@ void vbo_exec_vtx_flush( struct vbo_exec_context *exec,
    }
 
 
-   if (unmap) 
+   if (unmap || exec->vtx.vertex_size == 0)
       exec->vtx.max_vert = 0;
    else
       exec->vtx.max_vert = ((VBO_VERT_BUFFER_SIZE - exec->vtx.buffer_used) / 
