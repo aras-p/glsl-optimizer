87f83e193a21d7e4703664504734edd1e71920a4
Pauli Nieminen <suokkos@gmail.com>
2009-08-25 21:39:39 +0300

    r200: Addd missing parameter to debug output.

--- src/mesa/drivers/dri/r200/r200_tcl.c
+++ src/mesa/drivers/dri/r200/r200_tcl.c
@@ -418,7 +418,7 @@ static GLuint r200EnsureEmitSize( GLcontext * ctx , GLubyte* vimap_rev )
   }
 
   if (RADEON_DEBUG & DEBUG_PRIMS)
-     fprintf(stderr, "%s space %u, aos %d\n", space_required, AOS_BUFSZ(nr_aos) );
+     fprintf(stderr, "%s space %u, aos %d\n", __func__, space_required, AOS_BUFSZ(nr_aos) );
   /* flush the buffer in case we need more than is left. */
   if (rcommonEnsureCmdBufSpace(&rmesa->radeon, space_required + state_size, __FUNCTION__))
     return space_required + radeonCountStateEmitSize( &rmesa->radeon );
