7864b0e717755a26688891195e85a62714d50baa
Jakob Bornecrantz <wallbraker@gmail.com>
2009-08-30 20:41:00 +0100

    i915g: Cleanup i915_blit.[c|h]

--- src/gallium/drivers/i915simple/i915_blit.c
+++ src/gallium/drivers/i915simple/i915_blit.c
@@ -26,7 +26,6 @@
  **************************************************************************/
 
 
-#include "i915_context.h"
 #include "i915_winsys.h"
 #include "i915_blit.h"
 #include "i915_reg.h"
@@ -37,33 +36,33 @@
 
 void
 i915_fill_blit(struct i915_context *i915,
-	       unsigned cpp,
-	       unsigned short dst_pitch,
-	       struct pipe_buffer *dst_buffer,
-	       unsigned dst_offset,
-	       short x, short y, 
-	       short w, short h, 
-	       unsigned color)
+               unsigned cpp,
+               unsigned short dst_pitch,
+               struct pipe_buffer *dst_buffer,
+               unsigned dst_offset,
+               short x, short y, 
+               short w, short h, 
+               unsigned color)
 {
    unsigned BR13, CMD;
 
 
    I915_DBG(i915,
-       "%s dst:buf(%p)/%d+%d %d,%d sz:%dx%d\n",
-       __FUNCTION__,
-       dst_buffer, dst_pitch, dst_offset, x, y, w, h);
+      "%s dst:buf(%p)/%d+%d %d,%d sz:%dx%d\n",
+      __FUNCTION__,
+      dst_buffer, dst_pitch, dst_offset, x, y, w, h);
 
    switch (cpp) {
    case 1:
    case 2:
    case 3:
       BR13 = (((int) dst_pitch) & 0xffff) |
-	 (0xF0 << 16) | (1 << 24);
+         (0xF0 << 16) | (1 << 24);
       CMD = XY_COLOR_BLT_CMD;
       break;
    case 4:
       BR13 = (((int) dst_pitch) & 0xffff) |
-	 (0xF0 << 16) | (1 << 24) | (1 << 25);
+         (0xF0 << 16) | (1 << 24) | (1 << 25);
       CMD = (XY_COLOR_BLT_CMD | XY_COLOR_BLT_WRITE_ALPHA |
              XY_COLOR_BLT_WRITE_RGB);
       break;
@@ -79,25 +78,24 @@ i915_fill_blit(struct i915_context *i915,
    OUT_BATCH(BR13);
    OUT_BATCH((y << 16) | x);
    OUT_BATCH(((y + h) << 16) | (x + w));
-   OUT_RELOC( dst_buffer, I915_BUFFER_ACCESS_WRITE, dst_offset);
+   OUT_RELOC(dst_buffer, I915_BUFFER_ACCESS_WRITE, dst_offset);
    OUT_BATCH(color);
    FLUSH_BATCH(NULL);
 }
 
-
 void
-i915_copy_blit( struct i915_context *i915,
-                  unsigned do_flip,
-                  unsigned cpp,
-                  unsigned short src_pitch,
-                  struct pipe_buffer *src_buffer,
-                  unsigned src_offset,
-                  unsigned short dst_pitch,
-                  struct pipe_buffer *dst_buffer,
-                  unsigned dst_offset,
-                  short src_x, short src_y,
-                  short dst_x, short dst_y, 
-		  short w, short h )
+i915_copy_blit(struct i915_context *i915,
+               unsigned do_flip,
+               unsigned cpp,
+               unsigned short src_pitch,
+               struct pipe_buffer *src_buffer,
+               unsigned src_offset,
+               unsigned short dst_pitch,
+               struct pipe_buffer *dst_buffer,
+               unsigned dst_offset,
+               short src_x, short src_y,
+               short dst_x, short dst_y, 
+               short w, short h)
 {
    unsigned CMD, BR13;
    int dst_y2 = dst_y + h;
@@ -105,32 +103,30 @@ i915_copy_blit( struct i915_context *i915,
 
 
    I915_DBG(i915,
-       "%s src:buf(%p)/%d+%d %d,%d dst:buf(%p)/%d+%d %d,%d sz:%dx%d\n",
-       __FUNCTION__,
-       src_buffer, src_pitch, src_offset, src_x, src_y,
-       dst_buffer, dst_pitch, dst_offset, dst_x, dst_y, w, h);
+      "%s src:buf(%p)/%d+%d %d,%d dst:buf(%p)/%d+%d %d,%d sz:%dx%d\n",
+      __FUNCTION__,
+      src_buffer, src_pitch, src_offset, src_x, src_y,
+      dst_buffer, dst_pitch, dst_offset, dst_x, dst_y, w, h);
 
    switch (cpp) {
    case 1:
    case 2:
    case 3:
       BR13 = (((int) dst_pitch) & 0xffff) |
-	 (0xCC << 16) | (1 << 24);
+         (0xCC << 16) | (1 << 24);
       CMD = XY_SRC_COPY_BLT_CMD;
       break;
    case 4:
       BR13 = (((int) dst_pitch) & 0xffff) |
-	 (0xCC << 16) | (1 << 24) | (1 << 25);
-      CMD =
-         (XY_SRC_COPY_BLT_CMD | XY_SRC_COPY_BLT_WRITE_ALPHA |
-          XY_SRC_COPY_BLT_WRITE_RGB);
+             (0xCC << 16) | (1 << 24) | (1 << 25);
+      CMD = (XY_SRC_COPY_BLT_CMD | XY_SRC_COPY_BLT_WRITE_ALPHA |
+            XY_SRC_COPY_BLT_WRITE_RGB);
       break;
    default:
       return;
    }
 
-   if (dst_y2 < dst_y || 
-       dst_x2 < dst_x) {
+   if (dst_y2 < dst_y || dst_x2 < dst_x) {
       return;
    }
 
@@ -139,7 +135,6 @@ i915_copy_blit( struct i915_context *i915,
     * need for either at this stage.
     */
    assert (dst_pitch > 0 && src_pitch > 0);
-
 
    if (!BEGIN_BATCH(8, 2)) {
       FLUSH_BATCH(NULL);
@@ -155,5 +150,3 @@ i915_copy_blit( struct i915_context *i915,
    OUT_RELOC(src_buffer, I915_BUFFER_ACCESS_READ, src_offset);
    FLUSH_BATCH(NULL);
 }
-
-
