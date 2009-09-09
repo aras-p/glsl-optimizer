49c230709cfc588deea19f9ec21763ee00b81e6a
Alex Deucher <alexdeucher@gmail.com>
2009-09-09 11:14:17 -0400

    r600: check if textures are actually enabled before submission
    noticed by taiu on IRC.

--- src/mesa/drivers/dri/r600/r600_texstate.c
+++ src/mesa/drivers/dri/r600/r600_texstate.c
@@ -69,7 +69,7 @@ void r600UpdateTextureState(GLcontext * ctx)
 	for (unit = 0; unit < R700_MAX_TEXTURE_UNITS; unit++) {
 		texUnit = &ctx->Texture.Unit[unit];
 		t = radeon_tex_obj(ctx->Texture.Unit[unit]._Current);
-
+		r700->textures[unit] = NULL;
 		if (texUnit->_ReallyEnabled) {
 			if (!t)
 				continue;
