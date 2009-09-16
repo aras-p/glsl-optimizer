c29905aa318cf9ed782935552fa983b048646984
José Fonseca <jfonseca@vmware.com>
2009-09-16 14:31:14 +0100

    gallium: Deprecate PIPE_CAP_S3TC.
    No longer used. S3TC support is queried via
    pipe_screen::is_format_supported.

--- src/gallium/drivers/nv10/nv10_screen.c
+++ src/gallium/drivers/nv10/nv10_screen.c
@@ -15,8 +15,6 @@ nv10_screen_get_param(struct pipe_screen *screen, int param)
 		return 0;
 	case PIPE_CAP_GLSL:
 		return 0;
-	case PIPE_CAP_S3TC:
-		return 0;
 	case PIPE_CAP_ANISOTROPIC_FILTER:
 		return 1;
 	case PIPE_CAP_POINT_SPRITE:
--- src/gallium/drivers/nv50/nv50_screen.c
+++ src/gallium/drivers/nv50/nv50_screen.c
@@ -87,8 +87,6 @@ nv50_screen_get_param(struct pipe_screen *pscreen, int param)
 		return 1;
 	case PIPE_CAP_GLSL:
 		return 0;
-	case PIPE_CAP_S3TC:
-		return 1;
 	case PIPE_CAP_ANISOTROPIC_FILTER:
 		return 1;
 	case PIPE_CAP_POINT_SPRITE:
