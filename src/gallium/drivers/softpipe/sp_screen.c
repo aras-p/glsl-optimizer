1730b8db1237485778abdc4ae37e122414b5423b
Vinson Lee <vlee@vmware.com>
2009-09-24 15:59:57 -0600

    softpipe: Increase GL_MAX_3D_TEXTURE_SIZE to 256.

--- src/gallium/drivers/softpipe/sp_screen.c
+++ src/gallium/drivers/softpipe/sp_screen.c
@@ -84,7 +84,7 @@ softpipe_get_param(struct pipe_screen *screen, int param)
    case PIPE_CAP_MAX_TEXTURE_2D_LEVELS:
       return 12; /* max 2Kx2K */
    case PIPE_CAP_MAX_TEXTURE_3D_LEVELS:
-      return 8;  /* max 128x128x128 */
+      return 9;  /* max 256x256x256 */
    case PIPE_CAP_MAX_TEXTURE_CUBE_LEVELS:
       return 12; /* max 2Kx2K */
    case PIPE_CAP_TGSI_CONT_SUPPORTED:
