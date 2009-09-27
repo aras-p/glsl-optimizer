55893b9439754c5213a9c182ee84f6c2554a0281
Chia-I Wu <olvaffe@gmail.com>
2009-09-27 16:14:36 +0800

    egl: Add a function to convert __GLcontextModes to _EGLConfig.
    _eglConfigFromContextModesRec is used to convert a __GLcontextModes to a
    _EGLConfig.  Note that the config is not validated.  An invalid mode
    is likely to give an invalid config.
    
    Signed-off-by: Chia-I Wu <olvaffe@gmail.com>

--- src/egl/main/eglconfigutil.h
+++ src/egl/main/eglconfigutil.h
@@ -12,6 +12,11 @@ _eglConfigToContextModesRec(const _EGLConfig *config, __GLcontextModes *mode);
 
 
 extern EGLBoolean
+_eglConfigFromContextModesRec(_EGLConfig *conf, const __GLcontextModes *m,
+                              EGLint conformant, EGLint renderable_type);
+
+
+extern EGLBoolean
 _eglFillInConfigs( _EGLConfig *configs,
                    EGLenum fb_format, EGLenum fb_type,
                    const uint8_t * depth_bits, const uint8_t * stencil_bits,
