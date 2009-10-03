fce2095a90440d1c129583fb8b6c26a93d4bde13
Nicolai Hähnle <nhaehnle@gmail.com>
2009-10-03 17:39:32 +0200

    st/dri: Install APPLE_vertex_array_object functions
    Besides from being necessary to use that extension, it also fixes a crash
    when deleting the currently bound vertex array object.
    
    Signed-off-by: Nicolai Hähnle <nhaehnle@gmail.com>

--- src/gallium/state_trackers/dri/dri_extensions.c
+++ src/gallium/state_trackers/dri/dri_extensions.c
@@ -53,6 +53,7 @@
 #define need_GL_EXT_framebuffer_object
 #define need_GL_EXT_multi_draw_arrays
 #define need_GL_EXT_secondary_color
+#define need_GL_APPLE_vertex_array_object
 #define need_GL_NV_vertex_program
 #define need_GL_VERSION_2_0
 #define need_GL_VERSION_2_1
@@ -105,6 +106,7 @@ const struct dri_extension card_extensions[] = {
    {"GL_EXT_texture_lod_bias", NULL},
    {"GL_3DFX_texture_compression_FXT1", NULL},
    {"GL_APPLE_client_storage", NULL},
+   {"GL_APPLE_vertex_array_object", GL_APPLE_vertex_array_object_functions},
    {"GL_MESA_pack_invert", NULL},
    {"GL_MESA_ycbcr_texture", NULL},
    {"GL_NV_blend_square", NULL},
