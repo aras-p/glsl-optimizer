eb2e41f0c636eb77634ec7ada93b869a43f11e9f
José Fonseca <jfonseca@vmware.com>
2009-10-04 11:36:42 +0100

    llvmpipe: Remove loop testing from format testing.
    Loop building will be rewritten.

--- src/gallium/drivers/llvmpipe/lp_test_format.c
+++ src/gallium/drivers/llvmpipe/lp_test_format.c
@@ -37,7 +37,6 @@
 
 #include "util/u_format.h"
 
-#include "lp_bld_flow.h"
 #include "lp_bld_format.h"
 
 
@@ -103,7 +102,6 @@ add_load_rgba_test(LLVMModuleRef module,
    LLVMBasicBlockRef block;
    LLVMBuilderRef builder;
    LLVMValueRef rgba;
-   struct lp_build_loop_state loop;
 
    args[0] = LLVMPointerType(LLVMInt8Type(), 0);
    args[1] = LLVMPointerType(LLVMVectorType(LLVMFloatType(), 4), 0);
@@ -117,12 +115,8 @@ add_load_rgba_test(LLVMModuleRef module,
    builder = LLVMCreateBuilder();
    LLVMPositionBuilderAtEnd(builder, block);
 
-   lp_build_loop_begin(builder, LLVMConstInt(LLVMInt32Type(), 1, 0), &loop);
-
    rgba = lp_build_load_rgba_aos(builder, format, ptr);
    LLVMBuildStore(builder, rgba, rgba_ptr);
-
-   lp_build_loop_end(builder, LLVMConstInt(LLVMInt32Type(), 4, 0), NULL, &loop);
 
    LLVMBuildRetVoid(builder);
 
