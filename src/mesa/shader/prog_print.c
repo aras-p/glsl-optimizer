dfefde38c7dfe70a3531cb85215e55eeb6407180
Brian Paul <brianp@vmware.com>
2009-10-13 16:18:06 -0600

    mesa: don't print pointer in _mesa_fprint_parameter_list()

--- src/mesa/shader/prog_print.c
+++ src/mesa/shader/prog_print.c
@@ -906,7 +906,8 @@ _mesa_fprint_parameter_list(FILE *f,
    if (!list)
       return;
 
-   _mesa_fprintf(f, "param list %p\n", (void *) list);
+   if (0)
+      _mesa_fprintf(f, "param list %p\n", (void *) list);
    _mesa_fprintf(f, "dirty state flags: 0x%x\n", list->StateFlags);
    for (i = 0; i < list->NumParameters; i++){
       struct gl_program_parameter *param = list->Parameters + i;
