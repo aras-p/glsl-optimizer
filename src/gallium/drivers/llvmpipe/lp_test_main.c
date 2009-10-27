095e66f695ce1d869a824d9e22f63b54c95ca0ac
José Fonseca <jfonseca@vmware.com>
2009-10-27 20:09:53 +0000

    llvmpipe: Implement round() for MSVC.

--- src/gallium/drivers/llvmpipe/lp_test_main.c
+++ src/gallium/drivers/llvmpipe/lp_test_main.c
@@ -40,6 +40,18 @@
 #include "lp_test.h"
 
 
+#ifdef PIPE_CC_MSVC
+static INLINE double
+round(double x)
+{
+   if (x >= 0.0)
+      return floor(x + 0.5);
+   else
+      return ceil(x - 0.5);
+}
+#endif
+
+
 void
 dump_type(FILE *fp,
           struct lp_type type)
