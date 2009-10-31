5cf7c8e2d58a877bfc786f866d4e0d363ddd22aa
Dave Airlie <airlied@linux.ie>
2009-10-31 15:48:59 +1000

    radeon: use _mesa_get_current_tex_unit

--- src/mesa/drivers/dri/radeon/radeon_texstate.c
+++ src/mesa/drivers/dri/radeon/radeon_texstate.c
@@ -661,7 +661,7 @@ void radeonSetTexBuffer2(__DRIcontext *pDRICtx, GLint target, GLint glx_texture_
 	rmesa = pDRICtx->driverPrivate;
 
 	rfb = dPriv->driverPrivate;
-        texUnit = &radeon->glCtx->Texture.Unit[radeon->glCtx->Texture.CurrentUnit];
+        texUnit = _mesa_get_current_tex_unit(radeon->glCtx);
 	texObj = _mesa_select_tex_object(radeon->glCtx, texUnit, target);
         texImage = _mesa_get_tex_image(radeon->glCtx, texObj, target, 0);
 
