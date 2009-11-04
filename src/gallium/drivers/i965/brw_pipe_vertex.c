5f8dde99ed62beaf1c2590515c33ed8b5076ed8d
Keith Whitwell <keithw@vmware.com>
2009-11-04 16:03:52 +0000

    i965g: stubs for brw_pipe_vertex.c

--- src/gallium/drivers/i965/brw_pipe_vertex.c
+++ src/gallium/drivers/i965/brw_pipe_vertex.c
@@ -1,11 +1,25 @@
+#include "brw_context.h"
 
+
+void 
+brw_pipe_vertex_init( struct brw_context *brw )
+{
+}
 
 
 void 
 brw_pipe_vertex_cleanup( struct brw_context *brw )
 {
-   for (i = 0; i < VERT_ATTRIB_MAX; i++) {
+
+   /* Release bound pipe vertex_buffers
+    */
+
+   /* Release some other stuff
+    */
+#if 0
+   for (i = 0; i < PIPE_MAX_ATTRIBS; i++) {
       brw->sws->bo_unreference(brw->vb.inputs[i].bo);
       brw->vb.inputs[i].bo = NULL;
    }
+#endif
 }
