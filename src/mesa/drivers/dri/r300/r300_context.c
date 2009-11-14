aras-p/glsl-optimizer

commit 1d5a06a1f7812c055db1d724e40d21a0e3686dd1

r300: fix condition logic
The s3tc extensions are properly enabled now, when force_s3tc_enable option is set in driconf.

src/mesa/drivers/dri/r300/r300_context.c
@@ -433,7 +433,7 @@ static void r300InitGLExtensions(GLcontext *ctx)
 	if (r300->options.stencil_two_side_disabled)
 		_mesa_disable_extension(ctx, "GL_EXT_stencil_two_side");
 
-	if (ctx->Mesa_DXTn && !r300->options.s3tc_force_enabled) {
+	if (r300->options.s3tc_force_enabled) {
 		_mesa_enable_extension(ctx, "GL_EXT_texture_compression_s3tc");
 		_mesa_enable_extension(ctx, "GL_S3_s3tc");
 	} else if (r300->options.s3tc_force_disabled) {

commit 6e5d473cc16ca2d001df213fc1d907f2943a95bb

r300: fix regression introduced in 1d5a06a1f7812c055db1d724e40d21a0e3686dd1
Spotted by Dave Airlie

src/mesa/drivers/dri/r300/r300_context.c
@@ -439,7 +439,7 @@ static void r300InitGLExtensions(GLcontext *ctx)
 	if (r300->options.stencil_two_side_disabled)
 		_mesa_disable_extension(ctx, "GL_EXT_stencil_two_side");
 
-	if (r300->options.s3tc_force_enabled) {
+	if (ctx->Mesa_DXTn || r300->options.s3tc_force_enabled) {
 		_mesa_enable_extension(ctx, "GL_EXT_texture_compression_s3tc");
 		_mesa_enable_extension(ctx, "GL_S3_s3tc");
 	} else if (r300->options.s3tc_force_disabled) {