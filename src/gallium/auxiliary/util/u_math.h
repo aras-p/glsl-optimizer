144afacc6fc67d37780cbb29ccd298de9959b436
Brian Paul <brianp@vmware.com>
2009-12-02 09:23:15 -0700

    gallium/util: __builtin_bswap32() is in only gcc 4.3 or later

--- src/gallium/auxiliary/util/u_math.h
+++ src/gallium/auxiliary/util/u_math.h
@@ -520,7 +520,7 @@ util_bitcount(unsigned n)
 static INLINE uint32_t
 util_bswap32(uint32_t n)
 {
-#if defined(PIPE_CC_GCC)
+#if defined(PIPE_CC_GCC) && (PIPE_CC_GCC_VERSION >= 403)
    return __builtin_bswap32(n);
 #else
    return (n >> 24) |
