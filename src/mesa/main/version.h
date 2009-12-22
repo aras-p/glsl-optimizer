7e210b93376ab6fef63995c48d7b1766c4335ad8
Ian Romanick <ian.d.romanick@intel.com>
2009-12-21 18:28:32 -0800

    mesa: set version string to 7.7

--- src/mesa/main/version.h
+++ src/mesa/main/version.h
@@ -32,7 +32,7 @@
 #define MESA_MAJOR 7
 #define MESA_MINOR 7
 #define MESA_PATCH 0
-#define MESA_VERSION_STRING "7.7-rc3"
+#define MESA_VERSION_STRING "7.7"
 
 /* To make version comparison easy */
 #define MESA_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
