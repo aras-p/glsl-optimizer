9d3092d1344cac0dafaa4b39d09d987812751e40
Vinson Lee <vlee@vmware.com>
2009-12-27 22:18:18 -0800

    gallium/xlib: Silence unused variable warning.

--- src/gallium/state_trackers/glx/xlib/glx_api.c
+++ src/gallium/state_trackers/glx/xlib/glx_api.c
@@ -644,6 +644,7 @@ register_with_display(Display *dpy)
       XExtCodes *c = XAddExtension(dpy);
       ext = dpy->ext_procs;  /* new extension is at head of list */
       assert(c->extension == ext->codes.extension);
+      (void) c;
       ext->name = _mesa_strdup(extName);
       ext->close_display = close_display_callback;
    }
