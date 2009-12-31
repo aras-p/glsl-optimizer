fcc2e1a729e93bfe64f82d95d007ceab98f3dbc2
Brian Paul <brianp@vmware.com>
2009-12-31 08:46:09 -0700

    mesa: enable GL_EXT_draw_buffers2 for sw drivers

--- src/mesa/main/extensions.c
+++ src/mesa/main/extensions.c
@@ -271,6 +271,7 @@ _mesa_enable_sw_extensions(GLcontext *ctx)
    ctx->Extensions.EXT_blend_subtract = GL_TRUE;
    ctx->Extensions.EXT_convolution = GL_TRUE;
    ctx->Extensions.EXT_depth_bounds_test = GL_TRUE;
+   ctx->Extensions.EXT_draw_buffers2 = GL_TRUE;
    ctx->Extensions.EXT_fog_coord = GL_TRUE;
 #if FEATURE_EXT_framebuffer_object
    ctx->Extensions.EXT_framebuffer_object = GL_TRUE;
