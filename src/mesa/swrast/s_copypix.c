886cffb44e92dcdaeb5396a01745e78598e86a3f
Vinson Lee <vlee@vmware.com>
2010-01-13 22:43:16 -0800

    swrast: Remove unnecessary headers from s_copypix.c.

--- src/mesa/swrast/s_copypix.c
+++ src/mesa/swrast/s_copypix.c
@@ -27,11 +27,9 @@
 #include "main/context.h"
 #include "main/colormac.h"
 #include "main/convolve.h"
-#include "main/histogram.h"
 #include "main/image.h"
 #include "main/macros.h"
 #include "main/imports.h"
-#include "main/pixel.h"
 
 #include "s_context.h"
 #include "s_depth.h"
