a4bbabf494a127dcffc51f9c4734f2ecf75b3961
Jakob Bornecrantz <jakob@vmware.com>
2010-01-17 14:18:26 +0000

    softpipe: Move st/python's malloc winsys to softpipe
    Users of softpipe can still specify thier own winsys
    but now there is a default one also that uses malloc.
    This allows the malloc winsys to be shared with other
    users of softpipe.

--- src/gallium/drivers/softpipe/sp_winsys.h
+++ src/gallium/drivers/softpipe/sp_winsys.h
@@ -49,10 +49,17 @@ struct pipe_buffer;
 
 struct pipe_context *softpipe_create( struct pipe_screen * );
 
+/**
+ * Create a softpipe screen that uses the
+ * given winsys for allocating buffers.
+ */
+struct pipe_screen *softpipe_create_screen( struct pipe_winsys * );
 
-struct pipe_screen *
-softpipe_create_screen(struct pipe_winsys *);
-
+/**
+ * Create a softpipe screen that uses
+ * regular malloc to create all its buffers.
+ */
+struct pipe_screen *softpipe_create_screen_malloc(void);
 
 boolean
 softpipe_get_texture_buffer( struct pipe_texture *texture,
