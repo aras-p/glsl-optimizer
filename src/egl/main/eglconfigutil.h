cf7c1ea4f1a805a3b6003808aa0b034603412870
Chia-I Wu <olvaffe@gmail.com>
2010-01-24 20:44:39 +0800

    egl: Remove unused helper functions.
    _eglFillInConfigs and _eglFindAPIs have no user in Mesa and are unlikely
    to find one soon.  It should be fine to remove them.

--- src/egl/main/eglconfigutil.h
+++ src/egl/main/eglconfigutil.h
@@ -16,14 +16,4 @@ _eglConfigFromContextModesRec(_EGLConfig *conf, const __GLcontextModes *m,
                               EGLint conformant, EGLint renderable_type);
 
 
-PUBLIC EGLBoolean
-_eglFillInConfigs( _EGLConfig *configs,
-                   EGLenum fb_format, EGLenum fb_type,
-                   const uint8_t * depth_bits, const uint8_t * stencil_bits,
-                   unsigned num_depth_stencil_bits,
-                   const EGLenum * db_modes, unsigned num_db_modes,
-                   int visType );
-
-
-
 #endif /* EGLCONFIGUTIL_INCLUDED */
