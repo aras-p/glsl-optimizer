2fa34bf44c7cd7ba03ce8a7e82fe56898a57e9b4
Roland Scheidegger <sroland@vmware.com>
2010-01-28 17:11:49 +0100

    gallium: remove PIPE_CAP_S3TC, and add PIPE_CAP_DUAL_SOURCE_BLEND
    there's no point in keeping deprecated PIPE_CAPS around since there's no
    stable interface (yet).
    Reuse the enum for PIPE_CAP_DUAL_SOURCE_BLEND. Drivers advertizing this
    will accept the pipe blendfactors with SRC1 in them and be able to do dual
    source blending (src color from pixel shader output 0, and blendfactor with
    SRC1 will come from output 1, only one render target may be bound when using
    any of these blend factors).

--- src/gallium/include/pipe/p_defines.h
+++ src/gallium/include/pipe/p_defines.h
@@ -377,7 +377,7 @@ enum pipe_transfer_usage {
 #define PIPE_CAP_NPOT_TEXTURES           2
 #define PIPE_CAP_TWO_SIDED_STENCIL       3
 #define PIPE_CAP_GLSL                    4  /* XXX need something better */
-#define PIPE_CAP_S3TC                    5  /* XXX: deprecated; cap determined via supported sampler formats */
+#define PIPE_CAP_DUAL_SOURCE_BLEND       5  
 #define PIPE_CAP_ANISOTROPIC_FILTER      6
 #define PIPE_CAP_POINT_SPRITE            7
 #define PIPE_CAP_MAX_RENDER_TARGETS      8
