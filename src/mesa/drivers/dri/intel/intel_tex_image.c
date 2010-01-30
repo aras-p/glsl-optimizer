22bbc979944084ddffbb2c42a8178e172beaece0
Eric Anholt <eric@anholt.net>
2010-01-30 15:11:09 -0800

    intel: Respect texture tiling when doing a PBO blit teximage upload.
    Bug #26008.  Fixes piglit pbo-teximage-tiling-2.

--- src/mesa/drivers/dri/intel/intel_tex_image.c
+++ src/mesa/drivers/dri/intel/intel_tex_image.c
@@ -240,7 +240,8 @@ try_pbo_upload(struct intel_context *intel,
       if (!intelEmitCopyBlit(intel,
 			     intelImage->mt->cpp,
 			     src_stride, src_buffer, src_offset, GL_FALSE,
-			     dst_stride, dst_buffer, 0, GL_FALSE,
+			     dst_stride, dst_buffer, 0,
+			     intelImage->mt->region->tiling,
 			     0, 0, dst_x, dst_y, width, height,
 			     GL_COPY)) {
 	 return GL_FALSE;
