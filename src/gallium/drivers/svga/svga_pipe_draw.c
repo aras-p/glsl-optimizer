3703e259d9dd523c6c4104e5f0ac74a184049663
Keith Whitwell <keithw@vmware.com>
2010-02-08 17:13:15 +0000

    svga: remove dead debug sleep

--- src/gallium/drivers/svga/svga_pipe_draw.c
+++ src/gallium/drivers/svga/svga_pipe_draw.c
@@ -216,11 +216,6 @@ svga_draw_range_elements( struct pipe_context *pipe,
    }
 
    if (SVGA_DEBUG & DEBUG_FLUSH) {
-      static unsigned id;
-      debug_printf("%s %d\n", __FUNCTION__, id++);
-      if (id > 1300)
-         util_time_sleep( 2000 );
-
       svga_hwtnl_flush_retry( svga );
       svga_context_flush(svga, NULL);
    }
