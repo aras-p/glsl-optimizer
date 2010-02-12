75910e96dc9704e2ade842fde2647b27393ddcb7
Marek Olšák <maraeo@gmail.com>
2010-02-12 03:51:41 +0100

    r300g: emit INDEPENDENT_COLORFORMAT_ENABLE only on r5xx

--- src/gallium/drivers/r300/r300_emit.c
+++ src/gallium/drivers/r300/r300_emit.c
@@ -408,8 +408,7 @@ void r300_emit_fb_state(struct r300_context* r300, void* state)
                 R300_RB3D_CCTL_INDEPENDENT_COLOR_CHANNEL_MASK_ENABLE);
         } else {
             OUT_CS_REG(R300_RB3D_CCTL,
-                R300_RB3D_CCTL_NUM_MULTIWRITES(fb->nr_cbufs) |
-                R300_RB3D_CCTL_INDEPENDENT_COLORFORMAT_ENABLE_ENABLE);
+                R300_RB3D_CCTL_NUM_MULTIWRITES(fb->nr_cbufs));
         }
     } else {
         OUT_CS_REG(R300_RB3D_CCTL, 0x0);
