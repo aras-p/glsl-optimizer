8ff6790afb9c1b5a53a22fed9d5159581ba42a8f
Vinson Lee <vlee@vmware.com>
2010-02-21 14:26:53 -0800

    r300g: Remove unnecessary header.

--- src/gallium/drivers/r300/r300_emit.c
+++ src/gallium/drivers/r300/r300_emit.c
@@ -32,7 +32,6 @@
 #include "r300_emit.h"
 #include "r300_fs.h"
 #include "r300_screen.h"
-#include "r300_state_inlines.h"
 #include "r300_vs.h"
 
 void r300_emit_blend_state(struct r300_context* r300, void* state)
