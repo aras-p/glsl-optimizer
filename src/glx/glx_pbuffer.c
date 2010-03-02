5f40a7aed12500fd6792e2453f495555c3b5c54d
Brian Paul <brianp@vmware.com>
2010-03-02 07:34:29 -0700

    glx: replace assertion with conditional
    See fd.o bug 26832.

--- src/glx/glx_pbuffer.c
+++ src/glx/glx_pbuffer.c
@@ -379,8 +379,8 @@ CreateDrawable(Display * dpy, const __GLcontextModes * fbconfig,
    req->glxwindow = (GLXWindow) XAllocID(dpy);
    req->numAttribs = (CARD32) i;
 
-   assert(attrib_list);
-   memcpy(data, attrib_list, 8 * i);
+   if (attrib_list)
+      memcpy(data, attrib_list, 8 * i);
 
    UnlockDisplay(dpy);
    SyncHandle();
