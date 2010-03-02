0192a4a825d3d04b1588bdabad629a9888f325d6
Luca Barbieri <luca@luca-barbieri.com>
2010-03-02 22:51:39 +0100

    nvfx: fix viewport state after bypass removal for swtnl
    The adjustment of nv30/nv40 after the removal of bypass incorrectly
    removed the hardware viewport bypass code, which we still need for
    swtnl and also forgot to remove NVFX_NEW_RAST from pipe.

--- src/gallium/drivers/nvfx/nvfx_state_viewport.c
+++ src/gallium/drivers/nvfx/nvfx_state_viewport.c
@@ -13,16 +13,29 @@ nvfx_state_viewport_validate(struct nvfx_context *nvfx)
 	so = so_new(2, 9, 0);
 	so_method(so, nvfx->screen->eng3d,
 		  NV34TCL_VIEWPORT_TRANSLATE_X, 8);
-	so_data  (so, fui(vpt->translate[0]));
-	so_data  (so, fui(vpt->translate[1]));
-	so_data  (so, fui(vpt->translate[2]));
-	so_data  (so, fui(vpt->translate[3]));
-	so_data  (so, fui(vpt->scale[0]));
-	so_data  (so, fui(vpt->scale[1]));
-	so_data  (so, fui(vpt->scale[2]));
-	so_data  (so, fui(vpt->scale[3]));
-	so_method(so, nvfx->screen->eng3d, 0x1d78, 1);
-	so_data  (so, 1);
+	if(nvfx->render_mode == HW) {
+		so_data  (so, fui(vpt->translate[0]));
+		so_data  (so, fui(vpt->translate[1]));
+		so_data  (so, fui(vpt->translate[2]));
+		so_data  (so, fui(vpt->translate[3]));
+		so_data  (so, fui(vpt->scale[0]));
+		so_data  (so, fui(vpt->scale[1]));
+		so_data  (so, fui(vpt->scale[2]));
+		so_data  (so, fui(vpt->scale[3]));
+		so_method(so, nvfx->screen->eng3d, 0x1d78, 1);
+		so_data  (so, 1);
+	} else {
+		so_data  (so, fui(0.0f));
+		so_data  (so, fui(0.0f));
+		so_data  (so, fui(0.0f));
+		so_data  (so, fui(0.0f));
+		so_data  (so, fui(1.0f));
+		so_data  (so, fui(1.0f));
+		so_data  (so, fui(1.0f));
+		so_data  (so, fui(1.0f));
+		so_method(so, nvfx->screen->eng3d, 0x1d78, 1);
+		so_data  (so, nvfx->is_nv4x ? 0x110 : 1);
+	}
 
 	so_ref(so, &nvfx->state.hw[NVFX_STATE_VIEWPORT]);
 	so_ref(NULL, &so);
@@ -32,7 +45,7 @@ nvfx_state_viewport_validate(struct nvfx_context *nvfx)
 struct nvfx_state_entry nvfx_state_viewport = {
 	.validate = nvfx_state_viewport_validate,
 	.dirty = {
-		.pipe = NVFX_NEW_VIEWPORT | NVFX_NEW_RAST,
+		.pipe = NVFX_NEW_VIEWPORT,
 		.hw = NVFX_STATE_VIEWPORT
 	}
 };
