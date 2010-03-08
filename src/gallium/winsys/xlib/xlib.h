7f9a3959b0a69f6f4b520a87e3ea87918cf89f11
Keith Whitwell <keithw@vmware.com>
2010-03-08 15:35:51 +0000

    ws/xlib: remove redundant declarations

--- src/gallium/winsys/xlib/xlib.h
+++ src/gallium/winsys/xlib/xlib.h
@@ -9,12 +9,5 @@ extern struct xm_driver xlib_softpipe_driver;
 extern struct xm_driver xlib_llvmpipe_driver;
 extern struct xm_driver xlib_cell_driver;
 
-/* Internal:
- */
-struct sw_winsys;
-struct sw_displaytarget;
-struct sw_winsys *xlib_create_sw_winsys( void );
-void xlib_sw_display(struct xmesa_buffer *xm_buffer,
-                     struct sw_displaytarget *dt);
 
 #endif
