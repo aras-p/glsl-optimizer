ddb081e3edaadfefe8fdbefe6f572e1632e55942
José Fonseca <jfonseca@vmware.com>
2010-03-10 18:04:28 +0000

    Revert "scons: Refuse to use LLVM 2.7 for now."
    This reverts commit 44703217f73095c4946e06057e399e447b252576.
    
    http://www.llvm.org/bugs/show_bug.cgi?id=6429 has been fixed now. Things
    appear to be working reasonably so far.

--- scons/llvm.py
+++ scons/llvm.py
@@ -82,11 +82,6 @@ def generate(env):
             print 'scons: could not determine the LLVM version from %s' % llvm_config
             return
 
-        if llvm_version >= distutils.version.LooseVersion('2.7'):
-            print 'scons: Ignoring unsupported LLVM version %s' % llvm_version
-            print 'scons: See http://www.llvm.org/bugs/show_bug.cgi?id=6429'
-            return
-
         env.Prepend(CPPPATH = [os.path.join(llvm_dir, 'include')])
         env.AppendUnique(CPPDEFINES = [
             '__STDC_LIMIT_MACROS', 
@@ -137,11 +132,6 @@ def generate(env):
     elif env.Detect('llvm-config'):
         llvm_version = env.backtick('llvm-config --version').rstrip()
         llvm_version = distutils.version.LooseVersion(llvm_version)
-
-        if llvm_version >= distutils.version.LooseVersion('2.7'):
-            print 'scons: Ignoring unsupported LLVM version %s' % llvm_version
-            print 'scons: See http://www.llvm.org/bugs/show_bug.cgi?id=6429'
-            return
 
         try:
             env.ParseConfig('llvm-config --cppflags')
