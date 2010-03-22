1e7d65bb5b50efbd5812e7996910b7688eb27192
Vinson Lee <vlee@vmware.com>
2010-03-21 19:09:54 -0700

    progs/tests: Include stddef.h for ptrdiff_t on all platforms.
    stddef.h is the standard C header that defines ptrdiff_t.
    
    Fixes build of cva_huge on Mac OS X.

--- progs/tests/cva_huge.c
+++ progs/tests/cva_huge.c
@@ -32,11 +32,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#ifdef __VMS
-# include <stddef.h>	/* for ptrdiff_t, referenced by GL.h when GL_GLEXT_LEGACY defined */
-#else
-# include <malloc.h>	/* for ptrdiff_t, referenced by GL.h when GL_GLEXT_LEGACY defined */
-#endif
+#include <stddef.h>	/* for ptrdiff_t, referenced by GL.h when GL_GLEXT_LEGACY defined */
 #ifdef _WIN32
 #include <windows.h>
 #endif
