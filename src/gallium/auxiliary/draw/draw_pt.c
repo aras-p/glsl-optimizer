45c89091487a9cd3999362cdb486299c4a0f24c6
Brian Paul <brianp@vmware.com>
2010-04-06 20:20:13 -0600

    gallium/draw: add additional DRAW_LLVM check
    Fixes build breakage.

--- src/gallium/auxiliary/draw/draw_pt.c
+++ src/gallium/auxiliary/draw/draw_pt.c
@@ -140,7 +140,10 @@ boolean draw_pt_init( struct draw_context *draw )
    if (!draw->pt.middle.fetch_shade_emit)
       return FALSE;
 
+#if DRAW_LLVM
    draw->pt.middle.general = draw_pt_fetch_pipeline_or_emit_llvm( draw );
+#endif
+
    if (!draw->pt.middle.general)
       draw->pt.middle.general = draw_pt_fetch_pipeline_or_emit( draw );
    if (!draw->pt.middle.general)
