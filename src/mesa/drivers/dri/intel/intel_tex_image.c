06c72da97a1bce87bc4b3fb8e7e18bf966926acb
Kristian Høgsberg <krh@bitplanet.net>
2010-05-04 13:35:23 -0400

    intel: Update renderbuffers unconditionally in intelSetTexBuffer2()

--- src/mesa/drivers/dri/intel/intel_tex_image.c
+++ src/mesa/drivers/dri/intel/intel_tex_image.c
@@ -728,8 +728,7 @@ intelSetTexBuffer2(__DRIcontext *pDRICtx, GLint target,
    if (!intelObj)
       return;
 
-   if (dPriv->lastStamp != dPriv->dri2.stamp)
-      intel_update_renderbuffers(pDRICtx, dPriv);
+   intel_update_renderbuffers(pDRICtx, dPriv);
 
    rb = intel_get_renderbuffer(fb, BUFFER_FRONT_LEFT);
    /* If the region isn't set, then intel_update_renderbuffers was unable
