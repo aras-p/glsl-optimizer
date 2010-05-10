7b937cf47f3d8ce0c2bdc8aa22dacbfc999718b8
Brian Paul <brianp@vmware.com>
2010-05-10 17:03:10 -0600

    llvmpipe: enable PIPE_CAP_INDEP_BLEND_ENABLE
    This enables the GL_EXT_draw_buffers2 extension.
    Tested with progs/tests/drawbuffers2.

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -124,7 +124,7 @@ llvmpipe_get_param(struct pipe_screen *screen, int param)
    case PIPE_CAP_BLEND_EQUATION_SEPARATE:
       return 1;
    case PIPE_CAP_INDEP_BLEND_ENABLE:
-      return 0;
+      return 1;
    case PIPE_CAP_INDEP_BLEND_FUNC:
       return 0;
    case PIPE_CAP_TGSI_FS_COORD_ORIGIN_UPPER_LEFT:
