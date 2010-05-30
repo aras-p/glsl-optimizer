6f67a71aada11a097c7c11db8f11cd56557354ab
José Fonseca <jfonseca@vmware.com>
2010-05-30 16:36:08 +0100

    util: Generic pointer to func conversion helper.
    That observes strict-aliasing rules.

--- src/gallium/auxiliary/util/u_pointer.h
+++ src/gallium/auxiliary/util/u_pointer.h
@@ -98,6 +98,18 @@ align16( void *unaligned )
    return align_pointer( unaligned, 16 );
 }
 
+typedef void (*func_pointer)(void);
+
+static INLINE func_pointer
+pointer_to_func( void *p )
+{
+   union {
+      void *p;
+      func_pointer f;
+   } pf;
+   pf.p = p;
+   return pf.f;
+}
 
 
 #ifdef __cplusplus
