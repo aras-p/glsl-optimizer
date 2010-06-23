f9574c5f890f3205efa4ab4ff509223e2a7c6b74
Chia-I Wu <olv@lunarg.com>
2010-06-23 17:45:06 +0800

    st/egl: Make api_OpenGL support OpenGL ES.
    This allows api_OpenGL.so to support OpenGL ES.

--- src/gallium/targets/egl-apis/api_GLESv2.c
+++ src/gallium/targets/egl-apis/api_GLESv2.c
@@ -1,8 +1,7 @@
 #include "state_tracker/st_gl_api.h"
 
 PUBLIC struct st_api *
-st_api_create_OpenGL_ES2()
+st_api_create_OpenGL_ES2(void)
 {
-   /* linker magic creates different versions */
-   return st_gl_api_create();
+   return st_gl_api_create_es2();
 }
--- src/gallium/targets/egl-apis/api_OpenVG.c
+++ src/gallium/targets/egl-apis/api_OpenVG.c
@@ -2,7 +2,7 @@
 #include "vg_api.h"
 
 PUBLIC struct st_api *
-st_api_create_OpenVG()
+st_api_create_OpenVG(void)
 {
    return (struct st_api *) vg_api_get();
 }
