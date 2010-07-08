e72b87736d8453e79bb6da48ba4cfcc2e97c8e14
Eric Anholt <eric@anholt.net>
2010-07-08 12:12:54 -0700

    intel: Update intel_decode.c from intel-gpu-tools.
    This came from commit cf255e382d147fe3ca450f0dcec3525190e7dcbc

--- src/mesa/drivers/dri/intel/intel_decode.h
+++ src/mesa/drivers/dri/intel/intel_decode.h
@@ -25,5 +25,7 @@
  *
  */
 
-int intel_decode(uint32_t *data, int count, uint32_t hw_offset, uint32_t devid);
+int intel_decode(uint32_t *data, int count, uint32_t hw_offset, uint32_t devid,
+		 uint32_t ignore_end_of_batchbuffer);
+void intel_decode_context_set_head_tail(uint32_t head, uint32_t tail);
 void intel_decode_context_reset(void);
