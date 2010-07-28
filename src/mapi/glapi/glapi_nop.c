9b858186988083918483b00ca8a14a5d36787c82
Vinson Lee <vlee@vmware.com>
2010-07-28 01:11:34 -0700

    glapi: Fix VMware spelling.

--- src/mapi/glapi/glapi_nop.c
+++ src/mapi/glapi/glapi_nop.c
@@ -3,7 +3,7 @@
  * Version:  7.8
  *
  * Copyright (C) 1999-2008  Brian Paul   All Rights Reserved.
- * Copyright (C) 2010  VMWare, Inc.  All Rights Reserved.
+ * Copyright (C) 2010  VMware, Inc.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
