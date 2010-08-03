121368e7382fc9d17194b7120bf252de08791833
Aras Pranckevicius <aras@unity3d.com>
2010-08-03 14:47:19 +0300

    tests: use OpenGL to check input/raw/output shaders

--- src/glsl/glsl_optimizer_main.cpp
+++ src/glsl/glsl_optimizer_main.cpp
@@ -4,9 +4,117 @@
 
 #ifdef _MSC_VER
 #include <windows.h>
+#include <gl/GL.h>
+extern "C" {
+typedef char GLcharARB;		/* native character */
+typedef unsigned int GLhandleARB;	/* shader object handle */
+#define GL_VERTEX_SHADER_ARB              0x8B31
+#define GL_FRAGMENT_SHADER_ARB            0x8B30
+#define GL_OBJECT_COMPILE_STATUS_ARB      0x8B81
+typedef void (WINAPI * PFNGLDELETEOBJECTARBPROC) (GLhandleARB obj);
+typedef GLhandleARB (WINAPI * PFNGLCREATESHADEROBJECTARBPROC) (GLenum shaderType);
+typedef void (WINAPI * PFNGLSHADERSOURCEARBPROC) (GLhandleARB shaderObj, GLsizei count, const GLcharARB* *string, const GLint *length);
+typedef void (WINAPI * PFNGLCOMPILESHADERARBPROC) (GLhandleARB shaderObj);
+typedef void (WINAPI * PFNGLGETINFOLOGARBPROC) (GLhandleARB obj, GLsizei maxLength, GLsizei *length, GLcharARB *infoLog);
+typedef void (WINAPI * PFNGLGETOBJECTPARAMETERIVARBPROC) (GLhandleARB obj, GLenum pname, GLint *params);
+static PFNGLDELETEOBJECTARBPROC glDeleteObjectARB;
+static PFNGLCREATESHADEROBJECTARBPROC glCreateShaderObjectARB;
+static PFNGLSHADERSOURCEARBPROC glShaderSourceARB;
+static PFNGLCOMPILESHADERARBPROC glCompileShaderARB;
+static PFNGLGETINFOLOGARBPROC glGetInfoLogARB;
+static PFNGLGETOBJECTPARAMETERIVARBPROC glGetObjectParameterivARB;
+}
 #else
+#include <OpenGL/OpenGL.h>
+#include <AGL/agl.h>
 #include <dirent.h>
 #endif
+
+
+static bool InitializeOpenGL ()
+{
+	bool hasGLSL = false;
+
+#ifdef _MSC_VER
+	// setup minimal required GL
+	HWND wnd = CreateWindowA(
+		"STATIC",
+		"GL",
+		WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS |	WS_CLIPCHILDREN,
+		0, 0, 16, 16,
+		NULL, NULL,
+		GetModuleHandle(NULL), NULL );
+	HDC dc = GetDC( wnd );
+
+	PIXELFORMATDESCRIPTOR pfd = {
+		sizeof(PIXELFORMATDESCRIPTOR), 1,
+		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL,
+		PFD_TYPE_RGBA, 32,
+		0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0,
+		16, 0,
+		0, PFD_MAIN_PLANE, 0, 0, 0, 0
+	};
+
+	int fmt = ChoosePixelFormat( dc, &pfd );
+	SetPixelFormat( dc, fmt, &pfd );
+
+	HGLRC rc = wglCreateContext( dc );
+	wglMakeCurrent( dc, rc );
+
+#else
+	GLint attributes[16];
+	int i = 0;
+	attributes[i++]=AGL_RGBA;
+	attributes[i++]=AGL_PIXEL_SIZE;
+	attributes[i++]=32;
+	attributes[i++]=AGL_NO_RECOVERY;
+	attributes[i++]=AGL_NONE;
+	
+	AGLPixelFormat pixelFormat = aglChoosePixelFormat(NULL,0,attributes);
+	AGLContext agl = aglCreateContext(pixelFormat, NULL);
+	aglSetCurrentContext (agl);
+
+#endif
+	
+	// check if we have GLSL
+	const char* extensions = (const char*)glGetString(GL_EXTENSIONS);
+	hasGLSL = strstr(extensions, "GL_ARB_shader_objects") && strstr(extensions, "GL_ARB_vertex_shader") && strstr(extensions, "GL_ARB_fragment_shader");
+	
+#ifdef _MSC_VER
+	if (hasGLSL)
+	{
+		glDeleteObjectARB = (PFNGLDELETEOBJECTARBPROC)wglGetProcAddress("glDeleteObjectARB");
+		glCreateShaderObjectARB = (PFNGLCREATESHADEROBJECTARBPROC)wglGetProcAddress("glCreateShaderObjectARB");
+		glShaderSourceARB = (PFNGLSHADERSOURCEARBPROC)wglGetProcAddress("glShaderSourceARB");
+		glCompileShaderARB = (PFNGLCOMPILESHADERARBPROC)wglGetProcAddress("glCompileShaderARB");
+		glGetInfoLogARB = (PFNGLGETINFOLOGARBPROC)wglGetProcAddress("glGetInfoLogARB");
+		glGetObjectParameterivARB = (PFNGLGETOBJECTPARAMETERIVARBPROC)wglGetProcAddress("glGetObjectParameterivARB");
+	}
+#endif
+	
+	return hasGLSL;
+}
+
+static bool CheckGLSL (bool vertex, const char* prefix, const char* source)
+{
+	GLhandleARB shader = glCreateShaderObjectARB (vertex ? GL_VERTEX_SHADER_ARB : GL_FRAGMENT_SHADER_ARB);
+	glShaderSourceARB (shader, 1, &source, NULL);
+	glCompileShaderARB (shader);
+	GLint status;
+	glGetObjectParameterivARB (shader, GL_OBJECT_COMPILE_STATUS_ARB, &status);
+	bool res = true;
+	if (status == 0)
+	{
+		char log[4096];
+		GLsizei logLength;
+		glGetInfoLogARB (shader, sizeof(log), &logLength, log);
+		printf ("  glsl compile error on %s:\n%s\n", prefix, log);
+		res = false;
+	}
+	glDeleteObjectARB (shader);
+	return res;
+}
 
 
 static bool ReadStringFromFile (const char* pathName, std::string& output)
@@ -93,6 +201,10 @@ static bool TestFile (glslopt_ctx* ctx, bool vertex, const std::string& inputPat
 		printf ("  failed to read input file\n");
 		return false;
 	}
+	if (doCheckGLSL && !CheckGLSL (vertex, "input", input.c_str()))
+	{
+		return false;
+	}
 
 	bool res = true;
 
@@ -128,8 +240,10 @@ static bool TestFile (glslopt_ctx* ctx, bool vertex, const std::string& inputPat
 			printf ("  does not match optimized output\n");
 			res = false;
 		}
-		//if (doCheckGLSL && !CheckGLSL (vertex, text.c_str()))
-		//	res = false;
+		if (res && doCheckGLSL && !CheckGLSL (vertex, "raw", textHir.c_str()))
+			res = false;
+		if (res && doCheckGLSL && !CheckGLSL (vertex, "optimized", textOpt.c_str()))
+			res = false;
 	}
 	else
 	{
@@ -151,8 +265,7 @@ int main (int argc, const char** argv)
 		return 1;
 	}
 
-	//bool hasOpenGL = InitializeOpenGL ();
-	bool hasOpenGL = false;
+	bool hasOpenGL = InitializeOpenGL ();
 	glslopt_ctx* ctx = glslopt_initialize();
 
 	std::string baseFolder = argv[1];
