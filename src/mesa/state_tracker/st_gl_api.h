5e56c8907b9f31005a0655aac322b761e1b87c53
Vinson Lee <vlee@vmware.com>
2010-08-05 01:38:46 -0700

    st/mesa: Remove unnecessary header from st_gl_api.h.

--- src/mesa/state_tracker/st_gl_api.h
+++ src/mesa/state_tracker/st_gl_api.h
@@ -2,8 +2,6 @@
 #ifndef ST_GL_API_H
 #define ST_GL_API_H
 
-#include "state_tracker/st_api.h"
-
 struct st_api *st_gl_api_create(void);
 struct st_api *st_gl_api_create_es1(void);
 struct st_api *st_gl_api_create_es2(void);
