f717fd25cc44d7dda5a49dc05337c7ee7c8d2d2f
Vinson Lee <vlee@vmware.com>
2010-08-05 18:21:09 -0700

    st/mesa: Clean up header file inclusion in st_program.h.
    st_program.h
    Remove p_shader_tokens.h
    Include st_context.h for st_context symbol.
    Include p_state.h for PIPE_MAX_SHADER_INPUTS symbol.
    Remove unnecessary forward declarations.
    
    st_cb_bitmap.c
    st_cb_clear.c
    Include p_shader_tokens.h now that st_program.h doesn't include it.

--- src/mesa/state_tracker/st_cb_bitmap.c
+++ src/mesa/state_tracker/st_cb_bitmap.c
@@ -46,6 +46,7 @@
 
 #include "pipe/p_context.h"
 #include "pipe/p_defines.h"
+#include "pipe/p_shader_tokens.h"
 #include "util/u_inlines.h"
 #include "util/u_draw_quad.h"
 #include "util/u_simple_shaders.h"
--- src/mesa/state_tracker/st_program.h
+++ src/mesa/state_tracker/st_program.h
@@ -36,11 +36,8 @@
 
 #include "main/mtypes.h"
 #include "program/program.h"
-#include "pipe/p_shader_tokens.h"
-
-
-struct cso_fragment_shader;
-struct cso_vertex_shader;
+#include "pipe/p_state.h"
+#include "st_context.h"
 
 
 /**
