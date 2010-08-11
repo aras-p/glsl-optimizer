0578acbe18cf940850c9a94f2fd96f4602dd03bf
Vinson Lee <vlee@vmware.com>
2010-08-11 01:10:54 -0700

    mesa: Include missing header in prog_noise.h.
    Include glheader.h for GLfloat symbol.

--- src/mesa/program/prog_noise.h
+++ src/mesa/program/prog_noise.h
@@ -25,6 +25,8 @@
 #ifndef PROG_NOISE
 #define PROG_NOISE
 
+#include "main/glheader.h"
+
 extern GLfloat _mesa_noise1(GLfloat);
 extern GLfloat _mesa_noise2(GLfloat, GLfloat);
 extern GLfloat _mesa_noise3(GLfloat, GLfloat, GLfloat);
