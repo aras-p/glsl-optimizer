1d18739290fa667dc81d6780d695a09f8c1cde99
Dave Airlie <airlied@redhat.com>
2010-08-18 09:56:21 +1000

    r600g: emit texture level offset in CB/DB setup.
    8 more piglit tests pass,
    fbo-clearmipmap, fbo-copyteximage, fbo-generatemipmap,
    fbo-generatemipmap-nonsquare, fbo-generatemipmap-scissor,
    fbo-generatemipmap-viewport, gen-teximage, gen-texsubimage

--- src/gallium/drivers/r600/r600_state.c
+++ src/gallium/drivers/r600/r600_state.c
@@ -741,7 +741,7 @@ static struct radeon_state *r600_cb(struct r600_context *rctx, int cb)
 		S_0280A0_SOURCE_FORMAT(1) |
 		S_0280A0_NUMBER_TYPE(ntype);
 
-	rstate->states[R600_CB0__CB_COLOR0_BASE] = 0x00000000;
+	rstate->states[R600_CB0__CB_COLOR0_BASE] = rtex->offset[level] >> 8;
 	rstate->states[R600_CB0__CB_COLOR0_INFO] = color_info;
 	rstate->states[R600_CB0__CB_COLOR0_SIZE] = S_028060_PITCH_TILE_MAX(pitch) |
 						S_028060_SLICE_TILE_MAX(slice);
@@ -782,7 +782,7 @@ static struct radeon_state *r600_db(struct r600_context *rctx)
 	pitch = (rtex->pitch[level] / rtex->bpt) / 8 - 1;
 	slice = (rtex->pitch[level] / rtex->bpt) * state->zsbuf->height / 64 - 1;
 	format = r600_translate_dbformat(state->zsbuf->texture->format);
-	rstate->states[R600_DB__DB_DEPTH_BASE] = 0x00000000;
+	rstate->states[R600_DB__DB_DEPTH_BASE] = rtex->offset[level] >> 8;
 	rstate->states[R600_DB__DB_DEPTH_INFO] = 0x00010000 |
 					S_028010_FORMAT(format);
 	rstate->states[R600_DB__DB_DEPTH_VIEW] = 0x00000000;
