d3fe699b0b2d46480b699b3fa4c77e41eea8d30d
Luca Barbieri <luca@luca-barbieri.com>
2010-08-20 00:04:30 +0200

    translate_sse: enable on Win64
    According to Vinson, enabling it causes no regressions

--- src/gallium/auxiliary/translate/translate.c
+++ src/gallium/auxiliary/translate/translate.c
@@ -38,8 +38,7 @@ struct translate *translate_create( const struct translate_key *key )
 {
    struct translate *translate = NULL;
 
-/* TODO: enable Win64 once it has actually been tested */
-#if defined(PIPE_ARCH_X86) || (defined(PIPE_ARCH_X86_64) && !defined(_WIN64))
+#if defined(PIPE_ARCH_X86) || defined(PIPE_ARCH_X86_64)
    translate = translate_sse2_create( key );
    if (translate)
       return translate;
