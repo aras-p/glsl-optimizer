67a9a90794ae2a3e8019dbcb0d3d0b97111b862b
Luca Barbieri <luca@luca-barbieri.com>
2010-09-03 20:57:44 +0200

    nvfx: fix division by zero in vp-ignore-input

--- src/gallium/drivers/nvfx/nvfx_vbo.c
+++ src/gallium/drivers/nvfx/nvfx_vbo.c
@@ -527,7 +527,7 @@ nvfx_vtxelts_state_create(struct pipe_context *pipe,
 
 	cso->translate = translate_create(&transkey);
 	cso->vertex_length = transkey.output_stride >> 2;
-	cso->max_vertices_per_packet = 2047 / cso->vertex_length;
+	cso->max_vertices_per_packet = 2047 / MAX2(cso->vertex_length, 1);
 
 	return (void *)cso;
 }
