7fc882643ce1c57fb8f60d8c5edd761eff404749
Kenneth Graunke <kenneth@whitecape.org>
2010-09-08 16:03:33 -0700

    glsl/builtins: Set the API in the fake context.
    Otherwise it gets used uninitialized.

--- src/glsl/builtin_function.cpp
+++ src/glsl/builtin_function.cpp
@@ -36,6 +36,7 @@ gl_shader *
 read_builtins(GLenum target, const char *protos, const char **functions, unsigned count)
 {
    GLcontext fakeCtx;
+   fakeCtx.API = API_OPENGL;
    gl_shader *sh = _mesa_new_shader(NULL, 0, target);
    struct _mesa_glsl_parse_state *st =
       new(sh) _mesa_glsl_parse_state(&fakeCtx, target, sh);
