9aee4be5e010725f3f06a5638532752c400c2f0f
Vinson Lee <vlee@vmware.com>
2010-09-15 05:34:29 -0700

    r600g: Silence uninitialized variable warning.

--- src/gallium/drivers/r600/eg_hw_states.c
+++ src/gallium/drivers/r600/eg_hw_states.c
@@ -358,7 +358,7 @@ static void eg_dsa(struct r600_context *rctx, struct radeon_state *rstate)
 	unsigned db_depth_control, alpha_test_control, alpha_ref, db_shader_control;
 	unsigned stencil_ref_mask, stencil_ref_mask_bf, db_render_override, db_render_control;
 	struct r600_shader *rshader;
-	struct r600_query *rquery;
+	struct r600_query *rquery = NULL;
 	boolean query_running;
 	int i;
 
