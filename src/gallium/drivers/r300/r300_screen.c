469907d59738ad32c7a3b95072897432f1e81a0f
Marek Olšák <maraeo@gmail.com>
2010-10-22 20:33:25 +0200

    r300g: say no to PIPE_CAP_STREAM_OUTPUT and PIPE_CAP_PRIMITIVE_RESTART

--- src/gallium/drivers/r300/r300_screen.c
+++ src/gallium/drivers/r300/r300_screen.c
@@ -125,6 +125,8 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_DEPTH_CLAMP: /* XXX implemented, but breaks Regnum Online */
         case PIPE_CAP_DEPTHSTENCIL_CLEAR_SEPARATE:
         case PIPE_CAP_SHADER_STENCIL_EXPORT:
+        case PIPE_CAP_STREAM_OUTPUT:
+        case PIPE_CAP_PRIMITIVE_RESTART:
             return 0;
 
         /* Texturing. */
