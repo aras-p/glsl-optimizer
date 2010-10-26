519835de04f39ef8aee61f6ef38e347958d7c52c
Eric Anholt <eric@anholt.net>
2010-10-26 13:15:01 -0700

    i965: Set up the constant buffer on gen6 when it's needed.
    This was slightly confused because gen6_wm_constants does the push
    constant buffer, while brw_wm_constants does pull constants.

--- src/mesa/drivers/dri/i965/brw_state_upload.c
+++ src/mesa/drivers/dri/i965/brw_state_upload.c
@@ -128,6 +128,7 @@ const struct brw_tracked_state *gen6_atoms[] =
    &gen6_cc_state_pointers,
 
    &brw_vs_constants, /* Before vs_surfaces and constant_buffer */
+   &brw_wm_constants, /* Before wm_surfaces and constant_buffer */
    &gen6_wm_constants, /* Before wm_surfaces and constant_buffer */
 
    &brw_vs_surfaces,		/* must do before unit */
