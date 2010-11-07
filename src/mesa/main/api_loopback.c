d421149cc8b9abe2b9061e166dce953a984b169a
Vinson Lee <vlee@vmware.com>
2010-11-06 21:05:16 -0700

    mesa: Include mfeatures.h in api_loopback for FEATURE_beginend.

--- src/mesa/main/api_loopback.c
+++ src/mesa/main/api_loopback.c
@@ -36,6 +36,7 @@
 #include "glapi/glapi.h"
 #include "glapi/glthread.h"
 #include "main/dispatch.h"
+#include "mfeatures.h"
 
 /* KW: A set of functions to convert unusual Color/Normal/Vertex/etc
  * calls to a smaller set of driver-provided formats.  Currently just
