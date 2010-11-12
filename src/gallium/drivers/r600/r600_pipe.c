5c7127c07c3933662bc5447ce4a83b4e8af3b473
Marek Olšák <maraeo@gmail.com>
2010-11-12 03:07:05 +0100

    r600g: fill out CAPs for indirect addressing

--- src/gallium/drivers/r600/r600_pipe.c
+++ src/gallium/drivers/r600/r600_pipe.c
@@ -370,6 +370,11 @@ static int r600_get_shader_param(struct pipe_screen* pscreen, unsigned shader, e
 		return 0; /* FIXME */
 	case PIPE_SHADER_CAP_TGSI_CONT_SUPPORTED:
 		return 1;
+	case PIPE_SHADER_CAP_INDIRECT_INPUT_ADDR:
+	case PIPE_SHADER_CAP_INDIRECT_OUTPUT_ADDR:
+	case PIPE_SHADER_CAP_INDIRECT_TEMP_ADDR:
+	case PIPE_SHADER_CAP_INDIRECT_CONST_ADDR:
+		return 1;
 	default:
 		return 0;
 	}
