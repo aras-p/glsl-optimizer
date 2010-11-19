d54d67499ced853d54a739131541208a416193e2
Daniel Vetter <daniel.vetter@ffwll.ch>
2010-11-19 23:38:21 +0100

    i915g: kill RGBA/X formats
    It's intel, so always little endian!
    
    Reviewed-by: Jakob Bornecrantz <wallbraker@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/gallium/drivers/i915/i915_screen.c
+++ src/gallium/drivers/i915/i915_screen.c
@@ -231,10 +231,6 @@ i915_is_format_supported(struct pipe_screen *screen,
    static const enum pipe_format tex_supported[] = {
       PIPE_FORMAT_B8G8R8A8_UNORM,
       PIPE_FORMAT_B8G8R8X8_UNORM,
-      PIPE_FORMAT_R8G8B8A8_UNORM,
-#if 0
-      PIPE_FORMAT_R8G8B8X8_UNORM,
-#endif
       PIPE_FORMAT_B5G6R5_UNORM,
       PIPE_FORMAT_L8_UNORM,
       PIPE_FORMAT_A8_UNORM,
