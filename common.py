792caebced5f8f6dbd7bf23aa8d94230aa786d00
José Fonseca <jfonseca@vmware.com>
2010-12-01 12:20:58 +0000

    scons: Move MSVS_VERSION option to common module.

--- common.py
+++ common.py
@@ -91,3 +91,4 @@ def AddOptions(opts):
 	opts.Add(BoolOption('llvm', 'use LLVM', default_llvm))
 	opts.Add(BoolOption('debug', 'DEPRECATED: debug build', 'yes'))
 	opts.Add(BoolOption('profile', 'DEPRECATED: profile build', 'no'))
+	opts.Add(EnumOption('MSVS_VERSION', 'MS Visual C++ version', None, allowed_values=('7.1', '8.0', '9.0')))
