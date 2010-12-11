bf8242684a893c2b3abb4a515c31d24a0a6b7dc4
Vinson Lee <vlee@vmware.com>
2010-12-11 14:37:18 -0800

    mesa: Clean up header file inclusion in nvfragparse.h.

--- src/mesa/program/nvfragparse.h
+++ src/mesa/program/nvfragparse.h
@@ -30,7 +30,10 @@
 #ifndef NVFRAGPARSE_H
 #define NVFRAGPARSE_H
 
-#include "main/mtypes.h"
+#include "main/glheader.h"
+
+struct gl_context;
+struct gl_fragment_program;
 
 extern void
 _mesa_parse_nv_fragment_program(struct gl_context *ctx, GLenum target,
