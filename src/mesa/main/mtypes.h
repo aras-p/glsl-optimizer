b4452c3baad6e0379eeb7f22f2e51d13999e1323
Eric Anholt <eric@anholt.net>
2011-01-12 12:27:12 -0800

    mesa: Track a computed _CurrentFragmentProgram for current gl_shader_program
    This is like how we track FragmentProgram._Current for the computed
    ARB fragment program for fixed function texenv, but this gives direct
    access to the gl_shader_program for drivers to codegen from, skipping
    ARB_fp.

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -2191,6 +2191,7 @@ struct gl_shader_state
    struct gl_shader_program *CurrentVertexProgram;
    struct gl_shader_program *CurrentGeometryProgram;
    struct gl_shader_program *CurrentFragmentProgram;
+   struct gl_shader_program *_CurrentFragmentProgram;
 
    /**
     * Program used by glUniform calls.
