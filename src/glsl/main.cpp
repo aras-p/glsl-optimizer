d42acef139740a54fe571f99b62cba7720734d8c
Brian Paul <brianp@vmware.com>
2011-01-14 17:45:23 -0700

    glsl: fix implicit int to bool warning
    Maybe preprocess() should return a bool.

--- src/glsl/main.cpp
+++ src/glsl/main.cpp
@@ -185,7 +185,7 @@ compile_shader(struct gl_context *ctx, struct gl_shader *shader)
 
    const char *source = shader->Source;
    state->error = preprocess(state, &source, &state->info_log,
-			     state->extensions, ctx->API);
+			     state->extensions, ctx->API) != 0;
 
    if (!state->error) {
       _mesa_glsl_lexer_ctor(state, source);
