ef3b8042e00fa4ac371f61b0832d1d5e73cff52f
Christian König <deathsimple@vodafone.de>
2011-01-16 23:52:53 +0100

    r600g: fix PIPE_CAP_INSTANCED_DRAWING warning

--- src/gallium/drivers/r600/r600_pipe.c
+++ src/gallium/drivers/r600/r600_pipe.c
@@ -287,6 +287,7 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_STREAM_OUTPUT:
 	case PIPE_CAP_PRIMITIVE_RESTART:
 	case PIPE_CAP_INDEP_BLEND_FUNC: /* FIXME allow this */
+	case PIPE_CAP_INSTANCED_DRAWING:
 		return 0;
 
 	/* Texturing. */
