2a18d1950c84c96d263cc8f15434502e03aeb1dc
Carl Worth <cworth@cworth.org>
2011-01-29 08:19:39 +1000

    Revert "glcpp: Demote "macro redefined" from an error to a warning"
    This reverts commit d3df641f0aba99b0b65ecd4d9b06798bca090a29.
    
    The original commit had sat unpushed on my machine for months. By the
    time I found it again, I had forgotten that we had decided not to use
    this change after all, (the relevant test was removed long ago).

--- src/glsl/glcpp/glcpp-parse.c
+++ src/glsl/glcpp/glcpp-parse.c
@@ -4012,8 +4012,8 @@ _define_object_macro (glcpp_parser_t *parser,
 			talloc_free (macro);
 			return;
 		}
-		glcpp_warning (loc, parser, "Redefinition of macro %s\n",
-			       identifier);
+		glcpp_error (loc, parser, "Redefinition of macro %s\n",
+			     identifier);
 	}
 
 	hash_table_insert (parser->defines, macro, identifier);
@@ -4043,8 +4043,8 @@ _define_function_macro (glcpp_parser_t *parser,
 			talloc_free (macro);
 			return;
 		}
-		glcpp_warning (loc, parser, "Redefinition of macro %s\n",
-			       identifier);
+		glcpp_error (loc, parser, "Redefinition of macro %s\n",
+			     identifier);
 	}
 
 	hash_table_insert (parser->defines, macro, identifier);
