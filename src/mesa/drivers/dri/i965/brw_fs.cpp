4a3b28113c3d23ba21bb8b8f5ebab7c567083a6d
Kenneth Graunke <kenneth@whitecape.org>
2011-02-19 16:48:24 -0800

    i965/fs: Correctly set up gl_FragCoord.w on Sandybridge.
    pixel_w is the final result; wpos_w is used on gen4 to compute it.
    
    NOTE: This is a candidate for the 7.10 branch.
    
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -494,7 +494,7 @@ fs_visitor::emit_fragcoord_interpolation(ir_variable *ir)
    wpos.reg_offset++;
 
    /* gl_FragCoord.w: Already set up in emit_interpolation */
-   emit(fs_inst(BRW_OPCODE_MOV, wpos, this->wpos_w));
+   emit(fs_inst(BRW_OPCODE_MOV, wpos, this->pixel_w));
 
    return reg;
 }
