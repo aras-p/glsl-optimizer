02448f2241d90fdbf97b0197e857a8f5acf5d4c7
Dave Airlie <airlied@redhat.com>
2011-03-01 18:29:24 +1000

    st/dri: fix missing array size init.
    Init array size to 1,
    
    reported by bnf on irc.

--- src/gallium/state_trackers/dri/drm/dri2.c
+++ src/gallium/state_trackers/dri/drm/dri2.c
@@ -317,6 +317,7 @@ dri2_allocate_buffer(__DRIscreen *sPriv,
    templ.width0 = width;
    templ.height0 = height;
    templ.depth0 = 1;
+   templ.array_size = 1;
 
    buffer->resource =
       screen->base.screen->resource_create(screen->base.screen, &templ);
