b79b05e17e197a7cdc523deff8f7ea456a91e8d0
José Fonseca <jfonseca@vmware.com>
2011-03-12 20:52:13 +0000

    draw: Fix draw_variant_output::format's type.

--- src/gallium/auxiliary/draw/draw_vs.h
+++ src/gallium/auxiliary/draw/draw_vs.h
@@ -33,6 +33,7 @@
 
 #include "draw_context.h"
 #include "draw_private.h"
+#include "draw_vertex.h"
 
 
 struct draw_context;
@@ -48,7 +49,7 @@ struct draw_variant_input
 
 struct draw_variant_output
 {
-   enum pipe_format format;     /* output format */
+   enum attrib_emit format;     /* output format */
    unsigned vs_output:8;        /* which vertex shader output is this? */
    unsigned offset:24;          /* offset into output vertex */
 };
