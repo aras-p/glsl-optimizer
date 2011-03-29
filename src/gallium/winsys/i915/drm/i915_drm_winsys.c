aras-p/glsl-optimizer

commit 0fbb64a52931ba5871aa2c5eb01b81d391f678d1

i915g: use drm ioctl to get pci devid
Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

src/gallium/winsys/i915/drm/i915_drm_winsys.c
@@ -15,24 +15,14 @@
 static void
 i915_drm_get_device_id(unsigned int *device_id)
 {
-   char path[512];
-   FILE *file;
-   void *shutup_gcc;
+   int ret;
+   struct drm_i915_getparam gp;
 
-   /*
-    * FIXME: Fix this up to use a drm ioctl or whatever.
-    */
+   gp.param = I915_PARAM_CHIPSET_ID;
+   gp.value = (int *)devid;
 
-   snprintf(path, sizeof(path), "/sys/class/drm/card0/device/device");
-   file = fopen(path, "r");
-   if (!file) {
-      return;
-   }
-
-   shutup_gcc = fgets(path, sizeof(path), file);
-   (void) shutup_gcc;
-   sscanf(path, "%x", device_id);
-   fclose(file);
+   ret = ioctl(fd, DRM_IOCTL_I915_GETPARAM, &gp, sizeof(gp));
+   assert(ret == 0);
 }
 
 static void

commit 2737350157cf048b436d7b64d420881ff6df9223

i915g: Fix build.
Fix build failures introduced with commit 0fbb64a52931ba5871aa2c5eb01b81d391f678d1.

Reviewed-by: Jakob Bornecrantz <wallbraker@gmail.com>

src/gallium/winsys/i915/drm/i915_drm_winsys.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+
+#include "i915_drm.h"
 
 #include "state_tracker/drm_driver.h"
 
@@ -13,13 +16,13 @@
 
 
 static void
-i915_drm_get_device_id(unsigned int *device_id)
+i915_drm_get_device_id(int fd, unsigned int *device_id)
 {
    int ret;
    struct drm_i915_getparam gp;
 
    gp.param = I915_PARAM_CHIPSET_ID;
-   gp.value = (int *)devid;
+   gp.value = (int *)device_id;
 
    ret = ioctl(fd, DRM_IOCTL_I915_GETPARAM, &gp, sizeof(gp));
    assert(ret == 0);
@@ -45,7 +48,7 @@ i915_drm_winsys_create(int drmFD)
    if (!idws)
       return NULL;
 
-   i915_drm_get_device_id(&deviceID);
+   i915_drm_get_device_id(drmFD, &deviceID);
 
    i915_drm_winsys_init_batchbuffer_functions(idws);
    i915_drm_winsys_init_buffer_functions(idws);