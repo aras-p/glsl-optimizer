14eedf3028422e98ac05713a3e3c37e6c9b4cf37
Eric Anholt <eric@anholt.net>
2011-04-13 10:50:56 -0700

    mesa: Fix _mesa_unpack_dudv_span_byte assertion.
    It was accepting only GL_DUDV_ATI and not the specific sized format
    GL_DU8DV8_ATI.  Fixes assertion failure at startup in Shadowgrounds.
    
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/mesa/main/pack.c
+++ src/mesa/main/pack.c
@@ -3973,7 +3973,8 @@ _mesa_unpack_dudv_span_byte( struct gl_context *ctx,
                              GLbitfield transferOps )
 {
    ASSERT(dstFormat == GL_DUDV_ATI);
-   ASSERT(srcFormat == GL_DUDV_ATI);
+   ASSERT(srcFormat == GL_DUDV_ATI ||
+	  srcFormat == GL_DU8DV8_ATI);
 
    ASSERT(srcType == GL_UNSIGNED_BYTE ||
           srcType == GL_BYTE ||
