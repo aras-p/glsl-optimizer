ab13ebf7c138cc54b405b990159c6b8ed0531d58
Marek Olšák <maraeo@gmail.com>
2011-04-17 23:41:33 +0200

    r200: enable some extensions
    Such as:
    - GL_ARB_half_float_pixel
    - GL_ARB_vertex_array_object
    - GL_APPLE_vertex_array_object
    - GL_EXT_gpu_program_parameters

--- src/mesa/drivers/dri/r200/r200_context.c
+++ src/mesa/drivers/dri/r200/r200_context.c
@@ -62,13 +62,16 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "radeon_span.h"
 
 #define need_GL_ARB_occlusion_query
+#define need_GL_ARB_vertex_array_object
 #define need_GL_ARB_vertex_program
+#define need_GL_APPLE_vertex_array_object
 #define need_GL_ATI_fragment_shader
 #define need_GL_EXT_blend_minmax
 #define need_GL_EXT_fog_coord
 #define need_GL_EXT_secondary_color
 #define need_GL_EXT_blend_equation_separate
 #define need_GL_EXT_blend_func_separate
+#define need_GL_EXT_gpu_program_parameters
 #define need_GL_NV_vertex_program
 #define need_GL_ARB_point_parameters
 #define need_GL_EXT_framebuffer_object
@@ -112,6 +115,7 @@ static const GLubyte *r200GetString( struct gl_context *ctx, GLenum name )
  */
 static const struct dri_extension card_extensions[] =
 {
+    { "GL_ARB_half_float_pixel",           NULL },
     { "GL_ARB_multitexture",               NULL },
     { "GL_ARB_occlusion_query",		   GL_ARB_occlusion_query_functions},
     { "GL_ARB_texture_border_clamp",       NULL },
@@ -120,6 +124,7 @@ static const struct dri_extension card_extensions[] =
     { "GL_ARB_texture_env_dot3",           NULL },
     { "GL_ARB_texture_env_crossbar",       NULL },
     { "GL_ARB_texture_mirrored_repeat",    NULL },
+    { "GL_ARB_vertex_array_object",        GL_ARB_vertex_array_object_functions},
     { "GL_EXT_blend_minmax",               GL_EXT_blend_minmax_functions },
     { "GL_EXT_blend_subtract",             NULL },
     { "GL_EXT_fog_coord",                  GL_EXT_fog_coord_functions },
@@ -133,6 +138,7 @@ static const struct dri_extension card_extensions[] =
     { "GL_EXT_texture_lod_bias",           NULL },
     { "GL_EXT_texture_mirror_clamp",       NULL },
     { "GL_EXT_texture_rectangle",          NULL },
+    { "GL_APPLE_vertex_array_object",      GL_APPLE_vertex_array_object_functions },
     { "GL_ATI_texture_env_combine3",       NULL },
     { "GL_ATI_texture_mirror_once",        NULL },
     { "GL_MESA_pack_invert",               NULL },
@@ -150,7 +156,9 @@ static const struct dri_extension blend_extensions[] = {
 };
 
 static const struct dri_extension ARB_vp_extension[] = {
-    { "GL_ARB_vertex_program",             GL_ARB_vertex_program_functions }
+    { "GL_ARB_vertex_program",             GL_ARB_vertex_program_functions },
+    { "GL_EXT_gpu_program_parameters",     GL_EXT_gpu_program_parameters_functions},
+    { NULL,                                NULL }
 };
 
 static const struct dri_extension NV_vp_extension[] = {
@@ -455,7 +463,7 @@ GLboolean r200CreateContext( gl_api api,
        driInitExtensions( ctx, blend_extensions, GL_FALSE );
    }
    if(rmesa->radeon.radeonScreen->drmSupportsVertexProgram)
-      driInitSingleExtension( ctx, ARB_vp_extension );
+      driInitExtensions( ctx, ARB_vp_extension, GL_FALSE );
    if(driQueryOptionb(&rmesa->radeon.optionCache, "nv_vertex_program"))
       driInitSingleExtension( ctx, NV_vp_extension );
 
