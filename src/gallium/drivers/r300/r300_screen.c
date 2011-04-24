aras-p/glsl-optimizer

commit 72239d16cd08113e994ea3508f91193c682b0930

r300g: do not advertise color_buffer_float on r300 and r400
It seems they can't do unclamped vertex colors. Tested on RV350.

src/gallium/drivers/r300/r300_screen.c
@@ -114,7 +114,7 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_BLEND_EQUATION_SEPARATE:
         case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
         case PIPE_CAP_FRAGMENT_COLOR_CLAMP_CONTROL:
-            return 1;
+            return is_r500 ? 1 : 0;
         case PIPE_CAP_TEXTURE_SWIZZLE:
             return util_format_s3tc_enabled ? r300screen->caps.dxtc_swizzle : 1;
         case PIPE_CAP_MIXED_COLORBUFFER_FORMATS:

commit 0c71da18ee29e8c29538104a0a8d07c9e9aae9a7

r300g: fix exposing caps on r300-r400
Broken with 72239d16cd08113e994ea3508f91193c682b0930.

src/gallium/drivers/r300/r300_screen.c
@@ -113,10 +113,10 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_TEXTURE_MIRROR_REPEAT:
         case PIPE_CAP_BLEND_EQUATION_SEPARATE:
         case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
-        case PIPE_CAP_FRAGMENT_COLOR_CLAMP_CONTROL:
-            return is_r500 ? 1 : 0;
+            return 1;
         case PIPE_CAP_TEXTURE_SWIZZLE:
             return util_format_s3tc_enabled ? r300screen->caps.dxtc_swizzle : 1;
+        case PIPE_CAP_FRAGMENT_COLOR_CLAMP_CONTROL:
         case PIPE_CAP_MIXED_COLORBUFFER_FORMATS:
             return is_r500 ? 1 : 0;
 