97e5099c5a7542ffa9f9def7302f668f4d6c0ca4
Aras Pranckevicius <aras@unity3d.com>
2011-05-19 09:14:05 +0300

    Do constant propagation across built-in function calls. Improves things after unrolling loops, and works around driver crash in iPad 2 ;)

--- src/glsl/opt_constant_propagation.cpp
+++ src/glsl/opt_constant_propagation.cpp
@@ -263,10 +263,13 @@ ir_constant_propagation_visitor::visit_enter(ir_call *ir)
    }
 
    /* Since we're unlinked, we don't (necssarily) know the side effects of
-    * this call.  So kill all copies.
+    * this call.  So kill all copies. Except if it's a built-in; we know
+	* they are side effect free.
     */
-   acp->make_empty();
-   this->killed_all = true;
+   if (!ir->get_callee()->is_builtin) {
+      acp->make_empty();
+      this->killed_all = true;
+   }
 
    return visit_continue_with_parent;
 }
