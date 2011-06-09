23ed3b90c7f9056182307f9a69a56f748da331a3
Chad Versace <chad@chad-versace.us>
2011-06-08 19:08:14 -0700

    intel: Unconditionally enable support for S8_Z24 texture format
    Commit b5c847c7ca06823af3b72324056a2e478caca70b erroneously disabled
    support for S8_Z24 texture format when the context required separate
    stencil (intel_context.must_use_separate_stencil).
    
    But the GL spec requires implementations to support GL_DEPTH24_STENCIL8.
    So we better find a way to fake it...
    
    From page 180 (196 of pdf) of the OpenGL 3.0 spec:
        In addition, implementations are required to support the following
        sized internal [texture] formats.
    
        [...]
    
        - Combined depth+stencil formats: DEPTH32F_STENCIL8 and and
          DEPTH24_STENCIL8.
    
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Chad Versace <chad@chad-versace.us>

--- src/mesa/drivers/dri/intel/intel_context.c
+++ src/mesa/drivers/dri/intel/intel_context.c
@@ -663,7 +663,7 @@ intelInitContext(struct intel_context *intel,
       ctx->TextureFormatSupported[MESA_FORMAT_AL1616] = GL_TRUE;
 
    /* Depth and stencil */
-   ctx->TextureFormatSupported[MESA_FORMAT_S8_Z24] = !intel->must_use_separate_stencil;
+   ctx->TextureFormatSupported[MESA_FORMAT_S8_Z24] = GL_TRUE;
    ctx->TextureFormatSupported[MESA_FORMAT_X8_Z24] = intel->has_separate_stencil;
    ctx->TextureFormatSupported[MESA_FORMAT_S8] = intel->has_separate_stencil;
 
