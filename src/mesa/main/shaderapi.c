47441956284b3e22df2ee8500667617d2880bfc8
Henri Verbeet <hverbeet@gmail.com>
2011-07-04 00:57:42 +0200

    mesa: Use the Elements macro for the sampler index assert in validate_samplers().
    This is probably nicer if the array size ever changes.
    
    NOTE: This is a candidate for the 7.11 branch.
    
    Signed-off-by: Henri Verbeet <hverbeet@gmail.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/mesa/main/shaderapi.c
+++ src/mesa/main/shaderapi.c
@@ -1050,7 +1050,7 @@ validate_samplers(const struct gl_program *prog, char *errMsg)
       gl_texture_index target;
       GLint sampler = _mesa_ffs(samplersUsed) - 1;
       assert(sampler >= 0);
-      assert(sampler < MAX_TEXTURE_IMAGE_UNITS);
+      assert(sampler < Elements(prog->SamplerUnits));
       unit = prog->SamplerUnits[sampler];
       target = prog->SamplerTargets[sampler];
       if (targetUsed[unit] != -1 && targetUsed[unit] != (int) target) {
