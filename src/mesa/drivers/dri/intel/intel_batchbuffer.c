aras-p/glsl-optimizer

commit 6862b54f4d4e88ef6ebf709ea7798093ec337e2a

intel: Give an explanation why we are exiting for debugging.
This could happen in 3 different cases, and ERRNO can explain what
happened. First case would be EIO (gpu hang), second EINVAL (something is
wrong inside the batch), and we also discovered that sometimes it happens
with ENOSPACE. All of those cases are different it it could be worth to at
least know what happened.

Signed-off-by: Eugeni Dodonov <eugeni.dodonov@intel.com>

src/mesa/drivers/dri/intel/intel_batchbuffer.c
@@ -138,6 +138,7 @@ do_flush_locked(struct intel_context *intel)
    }
 
    if (ret != 0) {
+      fprintf(stderr, "intel_do_flush_locked failed: %s\n", strerr(ret));
       exit(1);
    }
    intel->vtbl.new_batch(intel);

commit 0a00a9a05b357dafae86bf8af879aa601f101eba

intel: fix build error
Fix a build error introduced by commit 6862b54f:
i965_dri.so.tmp: undefined reference to `strerr'

Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>
Reviewed-by: Paul Berry <stereotype441@gmail.com>

src/mesa/drivers/dri/intel/intel_batchbuffer.c
@@ -138,7 +138,7 @@ do_flush_locked(struct intel_context *intel)
    }
 
    if (ret != 0) {
-      fprintf(stderr, "intel_do_flush_locked failed: %s\n", strerr(ret));
+      fprintf(stderr, "intel_do_flush_locked failed: %s\n", strerror(ret));
       exit(1);
    }
    intel->vtbl.new_batch(intel);