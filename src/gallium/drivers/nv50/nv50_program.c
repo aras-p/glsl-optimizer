d2d19ea51fa3575a8d014a69a9b835c335728817
Christoph Bumiller <e0425955@student.tuwien.ac.at>
2011-09-14 16:47:44 +0200

    nv50/ir: add missing license headers

--- src/gallium/drivers/nv50/nv50_program.c
+++ src/gallium/drivers/nv50/nv50_program.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Chrsitoph Bumiller
+ * Copyright 2010 Christoph Bumiller
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
