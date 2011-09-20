64cebb6c8339f515ff8acb7c34fd0615b0e34a19
Nicholas Miell <nmiell@gmail.com>
2011-09-19 23:17:01 -0700

    st/mesa: Enable NV_fog_distance for Gallium drivers
    The fixed-function generated vertex program is all that's needed for
    Gallium drivers.

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -291,6 +291,7 @@ void st_init_extensions(struct st_context *st)
    ctx->Extensions.MESA_pack_invert = GL_TRUE;
 
    ctx->Extensions.NV_blend_square = GL_TRUE;
+   ctx->Extensions.NV_fog_distance = GL_TRUE;
    ctx->Extensions.NV_texgen_reflection = GL_TRUE;
    ctx->Extensions.NV_texture_env_combine4 = GL_TRUE;
    ctx->Extensions.NV_texture_rectangle = GL_TRUE;
