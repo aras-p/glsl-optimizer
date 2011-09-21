aras-p/glsl-optimizer

commit a8cf4b6acf9ee996090cc0bb95fa3558b481108c

svga: Ignore redefine_user_buffer calls
We currently always treat contents of user-buffers as volatile so
we don't need to take any particular action when the state tracker
announces that the contents has changed.

Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>

src/gallium/drivers/svga/svga_resource_buffer_upload.c
@@ -651,8 +651,6 @@ svga_redefine_user_buffer(struct pipe_context *pipe,
                           unsigned offset,
                           unsigned size)
 {
-   struct svga_screen *ss = svga_screen(pipe->screen);
-   struct svga_context *svga = svga_context(pipe);
    struct svga_buffer *sbuf = svga_buffer(resource);
 
    assert(sbuf->user);
@@ -661,19 +659,8 @@ svga_redefine_user_buffer(struct pipe_context *pipe,
    assert(!sbuf->hwbuf);
 
    /*
-    * Release any uploaded user buffer.
-    *
-    * TODO: As an optimization, we could try to update the uploaded buffer
-    * instead.
+    * We always treat the contents of user-buffers as volatile,
+    * so no particular action needed here.
     */
 
-   pipe_resource_reference(&sbuf->uploaded.buffer, NULL);
-
-   pipe_mutex_lock(ss->swc_mutex);
-
-   sbuf->key.size.width = sbuf->b.b.width0 = offset + size;
-
-   pipe_mutex_unlock(ss->swc_mutex);
-
-   svga->dirty |= SVGA_NEW_VBUFFER | SVGA_NEW_VELEMENT;
 }

commit 559a0cd1fc3e8dab27075afa0064c1aebe003d9d

svga: call u_default_redefine_user_buffer() to fix failed assertion
This fixes a regression from a8cf4b6acf9ee996090cc0bb95fa3558b481108c

The problem occured when two successive glDrawArrays calls accessed
subsequent elements in user-space arrays.  The user-space array
from the first call wasn't being grown to accomodate the second
draw call's elements.

Reviewed-by: Thomas Hellstrom <thellstrom@vmware.com>

src/gallium/drivers/svga/svga_resource_buffer_upload.c
@@ -658,9 +658,6 @@ svga_redefine_user_buffer(struct pipe_context *pipe,
    assert(!sbuf->handle);
    assert(!sbuf->hwbuf);
 
-   /*
-    * We always treat the contents of user-buffers as volatile,
-    * so no particular action needed here.
-    */
-
+   /* use the default action of simply resizing the user buffer's size */
+   u_default_redefine_user_buffer(pipe, resource, offset, size);
 }