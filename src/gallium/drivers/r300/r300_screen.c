5baa33a738fcecea8d8ff95e6972b85af2e99ee5
Marek Olšák <maraeo@gmail.com>
2011-10-06 18:29:02 +0200

    r300g: disable stream output on SWTCL chipsets
    Unimplemented and not so useful for this driver.

--- src/gallium/drivers/r300/r300_screen.c
+++ src/gallium/drivers/r300/r300_screen.c
@@ -145,10 +145,10 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_SCALED_RESOLVE:
         case PIPE_CAP_MIN_TEXEL_OFFSET:
         case PIPE_CAP_MAX_TEXEL_OFFSET:
+        case PIPE_CAP_STREAM_OUTPUT:
             return 0;
 
         /* SWTCL-only features. */
-        case PIPE_CAP_STREAM_OUTPUT:
         case PIPE_CAP_PRIMITIVE_RESTART:
             return !r300screen->caps.has_tcl;
 
