6b27f1468071ab68f38da0472b4bc20f4a0b0e87
Christoph Bumiller <e0425955@student.tuwien.ac.at>
2011-10-14 19:54:34 +0200

    nv50/ir: initialize default prog_info values for GP,TP

--- src/gallium/drivers/nv50/codegen/nv50_ir.cpp
+++ src/gallium/drivers/nv50/codegen/nv50_ir.cpp
@@ -926,6 +926,16 @@ extern "C" {
 static void
 nv50_ir_init_prog_info(struct nv50_ir_prog_info *info)
 {
+#if defined(PIPE_SHADER_HULL) && defined(PIPE_SHADER_DOMAIN)
+   if (info->type == PIPE_SHADER_HULL || info->type == PIPE_SHADER_DOMAIN) {
+      info->prop.tp.domain = PIPE_PRIM_MAX;
+      info->prop.tp.outputPrim = PIPE_PRIM_MAX;
+   }
+#endif
+   if (info->type == PIPE_SHADER_GEOMETRY) {
+      info->prop.gp.instanceCount = 1;
+      info->prop.gp.maxVertices = 1;
+   }
    info->io.clipDistance = 0xff;
    info->io.pointSize = 0xff;
    info->io.edgeFlagIn = 0xff;
