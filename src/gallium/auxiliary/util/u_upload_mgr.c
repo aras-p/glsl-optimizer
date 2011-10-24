19961da4cb115ca930186bef41fb92740fdd09d1
Brian Paul <brianp@vmware.com>
2011-10-24 09:37:26 -0600

    util: handle failed mapping in u_upload_alloc_buffer()
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/gallium/auxiliary/util/u_upload_mgr.c
+++ src/gallium/auxiliary/util/u_upload_mgr.c
@@ -141,7 +141,12 @@ u_upload_alloc_buffer( struct u_upload_mgr *upload,
                                        PIPE_TRANSFER_WRITE |
                                        PIPE_TRANSFER_FLUSH_EXPLICIT,
                                        &upload->transfer);
-   
+   if (upload->map == NULL) {
+      upload->size = 0;
+      pipe_resource_reference(&upload->buffer, NULL);
+      return PIPE_ERROR_OUT_OF_MEMORY;
+   }
+
    upload->size = size;
 
    upload->offset = 0;
