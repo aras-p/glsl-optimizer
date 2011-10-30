fb0e4a23fab6992e827f0b62d5326745065074a2
Chia-I Wu <olvaffe@gmail.com>
2011-10-30 19:32:41 +0800

    android: include src/glsl/Makefile.sources
    This also fixes the build error due to missing link_uniforms.cpp in the source
    lists.
    
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Chad Versace <chad@chad-versace.us>
    
    [olv: the missing link_uniforms.cpp was added before this patch is committed]

--- src/glsl/Android.mk
+++ src/glsl/Android.mk
@@ -25,82 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# from Makefile
-LIBGLCPP_SOURCES = \
-	glcpp/glcpp-lex.c \
-	glcpp/glcpp-parse.c \
-	glcpp/pp.c
-
-C_SOURCES = \
-	strtod.c \
-	ralloc.c \
-	$(LIBGLCPP_SOURCES)
-
-CXX_SOURCES = \
-	ast_expr.cpp \
-	ast_function.cpp \
-	ast_to_hir.cpp \
-	ast_type.cpp \
-	glsl_lexer.cpp \
-	glsl_parser.cpp \
-	glsl_parser_extras.cpp \
-	glsl_types.cpp \
-	glsl_symbol_table.cpp \
-	hir_field_selection.cpp \
-	ir_basic_block.cpp \
-	ir_clone.cpp \
-	ir_constant_expression.cpp \
-	ir.cpp \
-	ir_expression_flattening.cpp \
-	ir_function_can_inline.cpp \
-	ir_function_detect_recursion.cpp \
-	ir_function.cpp \
-	ir_hierarchical_visitor.cpp \
-	ir_hv_accept.cpp \
-	ir_import_prototypes.cpp \
-	ir_print_visitor.cpp \
-	ir_reader.cpp \
-	ir_rvalue_visitor.cpp \
-	ir_set_program_inouts.cpp \
-	ir_validate.cpp \
-	ir_variable.cpp \
-	ir_variable_refcount.cpp \
-	linker.cpp \
-	link_functions.cpp \
-	link_uniforms.cpp \
-	loop_analysis.cpp \
-	loop_controls.cpp \
-	loop_unroll.cpp \
-	lower_clip_distance.cpp \
-	lower_discard.cpp \
-	lower_if_to_cond_assign.cpp \
-	lower_instructions.cpp \
-	lower_jumps.cpp \
-	lower_mat_op_to_vec.cpp \
-	lower_noise.cpp \
-	lower_texture_projection.cpp \
-	lower_variable_index_to_cond_assign.cpp \
-	lower_vec_index_to_cond_assign.cpp \
-	lower_vec_index_to_swizzle.cpp \
-	lower_vector.cpp \
-	opt_algebraic.cpp \
-	opt_constant_folding.cpp \
-	opt_constant_propagation.cpp \
-	opt_constant_variable.cpp \
-	opt_copy_propagation.cpp \
-	opt_copy_propagation_elements.cpp \
-	opt_dead_code.cpp \
-	opt_dead_code_local.cpp \
-	opt_dead_functions.cpp \
-	opt_discard_simplification.cpp \
-	opt_function_inlining.cpp \
-	opt_if_simplification.cpp \
-	opt_noop_swizzle.cpp \
-	opt_redundant_jumps.cpp \
-	opt_structure_splitting.cpp \
-	opt_swizzle_swizzle.cpp \
-	opt_tree_grafting.cpp \
-	s_expression.cpp
+include $(LOCAL_PATH)/Makefile.sources
 
 # ---------------------------------------
 # Build libmesa_glsl
@@ -109,9 +34,9 @@ CXX_SOURCES = \
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	$(C_SOURCES) \
-	$(CXX_SOURCES) \
-	builtin_function.cpp
+	$(LIBGLCPP_SOURCES) \
+	$(LIBGLSL_SOURCES) \
+	$(LIBGLSL_CXX_SOURCES)
 
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/mapi \
@@ -130,11 +55,11 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	$(C_SOURCES) \
-	$(CXX_SOURCES) \
-	builtin_stubs.cpp \
-	main.cpp \
-	standalone_scaffolding.cpp
+	$(LIBGLCPP_SOURCES) \
+	$(LIBGLSL_SOURCES) \
+	$(LIBGLSL_CXX_SOURCES) \
+	$(BUILTIN_COMPILER_CXX_SOURCES) \
+	$(GLSL_COMPILER_CXX_SOURCES)
 
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/mapi \
@@ -157,8 +82,7 @@ include $(BUILD_HOST_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	main.cpp \
-	standalone_scaffolding.cpp
+	$(GLSL_COMPILER_CXX_SOURCES)
 
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/mapi \
