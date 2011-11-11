da1544b39ebdf9b24414dcee214a5ccf76e74a77
Christoph Bumiller <e0425955@student.tuwien.ac.at>
2011-11-11 23:18:26 +0100

    st/mesa: fix system value to semantic mapping
    Broken by addition of SYSTEM_VALUE_VERTEX_ID in
    919c53e87a1f6f5322bc1f1486bb3e6b954b00d5.

--- src/mesa/state_tracker/st_mesa_to_tgsi.c
+++ src/mesa/state_tracker/st_mesa_to_tgsi.c
@@ -108,6 +108,7 @@ struct st_translate {
 /** Map Mesa's SYSTEM_VALUE_x to TGSI_SEMANTIC_x */
 static unsigned mesa_sysval_to_semantic[SYSTEM_VALUE_MAX] = {
    TGSI_SEMANTIC_FACE,
+   TGSI_SEMANTIC_VERTEXID,
    TGSI_SEMANTIC_INSTANCEID
 };
 
