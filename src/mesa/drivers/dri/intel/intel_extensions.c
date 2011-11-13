6ca61441b5cea8a2996c1adb4cc300a3c2be3ec9
Paul Berry <stereotype441@gmail.com>
2011-11-12 16:25:22 -0800

    i965 gen6: Turn on transform feedback extension.
    This patch advertises support for EXT_transform_feedback on Intel
    Gen6.
    
    Since transform feedback support is not completely finished yet, for
    now we only advertise support for it when MESA_GL_VERSION_OVERRIDE is
    3.0 or greater (since transform feedback is required by GL version
    3.0).
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/intel/intel_extensions.c
+++ src/mesa/drivers/dri/intel/intel_extensions.c
@@ -104,6 +104,9 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Const.GLSLVersion = 120;
    _mesa_override_glsl_version(ctx);
 
+   if (intel->gen == 6 && override_version >= 30)
+      ctx->Extensions.EXT_transform_feedback = true;
+
    if (intel->gen >= 5)
       ctx->Extensions.EXT_timer_query = true;
 
