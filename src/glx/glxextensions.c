8e5efbe62bfd4a1f5c960fa9b061c9dffac2bbce
Ian Romanick <ian.d.romanick@intel.com>
2011-11-30 10:53:59 -0800

    glx: Remove some extensions that are not, and never will be, supported
    There are a few unsupported extensions (e.g., the ATI and NV float
    extensions) that are still in the list.  There is some small chance
    that these may be supported some day.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/glx/glxextensions.c
+++ src/glx/glxextensions.c
@@ -73,7 +73,6 @@ struct extension_info
 static const struct extension_info known_glx_extensions[] = {
    { GLX(ARB_get_proc_address),        VER(1,4), Y, N, Y, N },
    { GLX(ARB_multisample),             VER(1,4), Y, Y, N, N },
-   { GLX(ARB_render_texture),          VER(0,0), N, N, N, N },
    { GLX(ATI_pixel_format_float),      VER(0,0), N, N, N, N },
 #ifdef GLX_USE_APPLEGL
    { GLX(EXT_import_context),          VER(0,0), N, N, N, N },
@@ -85,40 +84,30 @@ static const struct extension_info known_glx_extensions[] = {
    { GLX(EXT_visual_rating),           VER(0,0), Y, Y, N, N },
    { GLX(EXT_framebuffer_sRGB),        VER(0,0), Y, Y, N, N },
 #ifdef GLX_USE_APPLEGL
-   { GLX(MESA_agp_offset),             VER(0,0), N, N, N, N }, /* Deprecated */
    { GLX(MESA_copy_sub_buffer),        VER(0,0), N, N, N, N },
 #else
-   { GLX(MESA_agp_offset),             VER(0,0), N, N, N, Y }, /* Deprecated */
    { GLX(MESA_copy_sub_buffer),        VER(0,0), Y, N, N, N },
 #endif
    { GLX(MESA_multithread_makecurrent),VER(0,0), Y, N, Y, N },
-   { GLX(MESA_pixmap_colormap),        VER(0,0), N, N, N, N }, /* Deprecated */
-   { GLX(MESA_release_buffers),        VER(0,0), N, N, N, N }, /* Deprecated */
 #ifdef GLX_USE_APPLEGL
    { GLX(MESA_swap_control),           VER(0,0), N, N, N, N },
 #else
    { GLX(MESA_swap_control),           VER(0,0), Y, N, N, Y },
 #endif
    { GLX(NV_float_buffer),             VER(0,0), N, N, N, N },
-   { GLX(NV_render_depth_texture),     VER(0,0), N, N, N, N },
-   { GLX(NV_render_texture_rectangle), VER(0,0), N, N, N, N },
 #ifdef GLX_USE_APPLEGL
-   { GLX(NV_vertex_array_range),       VER(0,0), N, N, N, N }, /* Deprecated */
    { GLX(OML_swap_method),             VER(0,0), N, N, N, N },
    { GLX(OML_sync_control),            VER(0,0), N, N, N, N },
    { GLX(SGI_make_current_read),       VER(1,3), N, N, N, N },
    { GLX(SGI_swap_control),            VER(0,0), N, N, N, N },
    { GLX(SGI_video_sync),              VER(0,0), N, N, N, N },
 #else
-   { GLX(NV_vertex_array_range),       VER(0,0), N, N, N, Y }, /* Deprecated */
    { GLX(OML_swap_method),             VER(0,0), Y, Y, N, N },
    { GLX(OML_sync_control),            VER(0,0), Y, N, N, Y },
    { GLX(SGI_make_current_read),       VER(1,3), Y, N, N, N },
    { GLX(SGI_swap_control),            VER(0,0), Y, N, N, N },
    { GLX(SGI_video_sync),              VER(0,0), Y, N, N, Y },
 #endif
-   { GLX(SGIS_blended_overlay),        VER(0,0), N, N, N, N },
-   { GLX(SGIS_color_range),            VER(0,0), N, N, N, N },
 #ifdef GLX_USE_APPLEGL
    { GLX(SGIS_multisample),            VER(0,0), N, N, N, N },
 #else
