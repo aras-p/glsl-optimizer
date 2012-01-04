207cbc68dc43049c56c3af7a460ce07bd4284aa9
Kenneth Graunke <kenneth@whitecape.org>
2012-01-04 03:14:01 -0800

    i965: Add missing _NEW_TEXTURE dirty bit to brw_vs_prog state atom.
    Commit d45814c925dd6c479cfd383b9b59458fc4359cf7 totally added a data
    dependency on _NEW_TEXTURE, even including the comment, but didn't
    actually add the dirty bit.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/mesa/drivers/dri/i965/brw_vs.c
+++ src/mesa/drivers/dri/i965/brw_vs.c
@@ -349,6 +349,7 @@ static void brw_upload_vs_prog(struct brw_context *brw)
 const struct brw_tracked_state brw_vs_prog = {
    .dirty = {
       .mesa  = (_NEW_TRANSFORM | _NEW_POLYGON | _NEW_POINT | _NEW_LIGHT |
+		_NEW_TEXTURE |
 		_NEW_BUFFERS),
       .brw   = (BRW_NEW_VERTEX_PROGRAM |
 		BRW_NEW_VERTICES),
