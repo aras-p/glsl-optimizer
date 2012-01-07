78028dc2bdf3a10182467356a70f8fb1cbcc7e39
Brian Paul <brianp@vmware.com>
2012-01-07 14:16:27 -0700

    svga: add switch cases for PIPE_SHADER_CAP_OUTPUT_READ
    Silences unhandled switch case warning.
    Return 0 since we don't want to read from output regs.

--- src/gallium/drivers/svga/svga_screen.c
+++ src/gallium/drivers/svga/svga_screen.c
@@ -259,6 +259,8 @@ static int svga_get_shader_param(struct pipe_screen *screen, unsigned shader, en
          return 0;
       case PIPE_SHADER_CAP_MAX_TEXTURE_SAMPLERS:
          return 16;
+      case PIPE_SHADER_CAP_OUTPUT_READ:
+         return 0;
       }
       break;
    case PIPE_SHADER_VERTEX:
@@ -301,6 +303,8 @@ static int svga_get_shader_param(struct pipe_screen *screen, unsigned shader, en
       case PIPE_SHADER_CAP_SUBROUTINES:
          return 0;
       case PIPE_SHADER_CAP_INTEGERS:
+         return 0;
+      case PIPE_SHADER_CAP_OUTPUT_READ:
          return 0;
       default:
          break;
