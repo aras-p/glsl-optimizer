f892debdc21d12c40d2b69b4e9efdf527672d3ec
Brian Paul <brianp@vmware.com>
2012-01-16 11:29:53 -0700

    nouveau: remove obsolete GetRow/PutRow code

--- src/mesa/drivers/dri/nouveau/nouveau_span.c
+++ src/mesa/drivers/dri/nouveau/nouveau_span.c
@@ -31,72 +31,7 @@
 
 #include "swrast/swrast.h"
 
-#define LOCAL_VARS							\
-	struct nouveau_surface *s = &to_nouveau_renderbuffer(rb)->surface; \
-	GLuint p;							\
-	(void)p;
 
-#define LOCAL_DEPTH_VARS LOCAL_VARS
-
-#define HW_LOCK()
-#define HW_UNLOCK()
-
-#define HW_CLIPLOOP() {							\
-	int minx = 0;							\
-	int miny = 0;							\
-	int maxx = rb->Width;						\
-	int maxy = rb->Height;
-
-#define HW_ENDCLIPLOOP() }
-
-#define Y_FLIP(y) (rb->Name ? (y) : rb->Height - 1 - (y))
-
-/* RGB565 span functions */
-#define SPANTMP_PIXEL_FMT GL_RGB
-#define SPANTMP_PIXEL_TYPE GL_UNSIGNED_SHORT_5_6_5
-#define TAG(x) nouveau_##x##_rgb565
-#define TAG2(x, y) nouveau_##x##_rgb565##y
-#define GET_PTR(x, y) (s->bo->map + (y)*s->pitch + (x)*s->cpp)
-
-#include "spantmp2.h"
-
-/* RGB888 span functions */
-#define SPANTMP_PIXEL_FMT GL_BGR
-#define SPANTMP_PIXEL_TYPE GL_UNSIGNED_INT_8_8_8_8_REV
-#define TAG(x) nouveau_##x##_rgb888
-#define TAG2(x, y) nouveau_##x##_rgb888##y
-#define GET_PTR(x, y) (s->bo->map + (y)*s->pitch + (x)*s->cpp)
-
-#include "spantmp2.h"
-
-/* ARGB8888 span functions */
-#define SPANTMP_PIXEL_FMT GL_BGRA
-#define SPANTMP_PIXEL_TYPE GL_UNSIGNED_INT_8_8_8_8_REV
-#define TAG(x) nouveau_##x##_argb8888
-#define TAG2(x, y) nouveau_##x##_argb8888##y
-#define GET_PTR(x, y) (s->bo->map + (y)*s->pitch + (x)*s->cpp)
-
-#include "spantmp2.h"
-
-/* Z16 span functions */
-#define VALUE_TYPE uint16_t
-#define READ_DEPTH(v, x, y)						\
-	v = *(uint16_t *)(s->bo->map + (y)*s->pitch + (x)*s->cpp);
-#define WRITE_DEPTH(x, y, v)						\
-	*(uint16_t *)(s->bo->map + (y)*s->pitch + (x)*s->cpp) = v
-#define TAG(x) nouveau_##x##_z16
-
-#include "depthtmp.h"
-
-/* Z24S8 span functions */
-#define VALUE_TYPE uint32_t
-#define READ_DEPTH(v, x, y)						\
-	v = *(uint32_t *)(s->bo->map + (y)*s->pitch + (x)*s->cpp);
-#define WRITE_DEPTH(x, y, v)						\
-	*(uint32_t *)(s->bo->map + (y)*s->pitch + (x)*s->cpp) = v
-#define TAG(x) nouveau_##x##_z24s8
-
-#include "depthtmp.h"
 
 static void
 renderbuffer_map_unmap(struct gl_renderbuffer *rb, GLboolean map)
@@ -104,26 +39,6 @@ renderbuffer_map_unmap(struct gl_renderbuffer *rb, GLboolean map)
 	struct nouveau_surface *s = &to_nouveau_renderbuffer(rb)->surface;
 
 	if (map) {
-		switch (rb->Format) {
-		case MESA_FORMAT_RGB565:
-			nouveau_InitPointers_rgb565(rb);
-			break;
-		case MESA_FORMAT_XRGB8888:
-			nouveau_InitPointers_rgb888(rb);
-			break;
-		case MESA_FORMAT_ARGB8888:
-			nouveau_InitPointers_argb8888(rb);
-			break;
-		case MESA_FORMAT_Z16:
-			nouveau_InitDepthPointers_z16(rb);
-			break;
-		case MESA_FORMAT_Z24_S8:
-			nouveau_InitDepthPointers_z24s8(rb);
-			break;
-		default:
-			assert(0);
-		}
-
 		nouveau_bo_map(s->bo, NOUVEAU_BO_RDWR);
 	} else {
 		nouveau_bo_unmap(s->bo);
