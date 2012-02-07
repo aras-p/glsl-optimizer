699e3b98214b52579e186594c21b972ea4cb4037
Dave Airlie <airlied@redhat.com>
2012-02-07 14:26:16 +0000

    scons: fix typo in package version
    pointed out on irc by GArik_
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- scons/gallium.py
+++ scons/gallium.py
@@ -508,7 +508,7 @@ def generate(env):
     env.PkgCheckModules('XCB', ['x11-xcb', 'xcb-glx'])
     env.PkgCheckModules('XF86VIDMODE', ['xxf86vm'])
     env.PkgCheckModules('DRM', ['libdrm >= 2.4.24'])
-    env.PkgCheckModules('DRM_INTEL', ['libdrm_intel >= 2.4.38'])
+    env.PkgCheckModules('DRM_INTEL', ['libdrm_intel >= 2.4.30'])
     env.PkgCheckModules('DRM_RADEON', ['libdrm_radeon >= 2.4.31'])
     env.PkgCheckModules('XORG', ['xorg-server >= 1.6.0'])
     env.PkgCheckModules('KMS', ['libkms >= 2.4.24'])
