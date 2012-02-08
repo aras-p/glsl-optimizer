6beed86ab8970b57aad8442a3e6bf6410fff13ec
José Fonseca <jfonseca@vmware.com>
2012-02-08 15:46:28 +0000

    st/wgl: Fix argument of stw_pixelformat_get_info().
    stw_pixelformat_get_info takes zero based index, not a 1 based pixel
    format number.

--- src/gallium/state_trackers/wgl/stw_ext_pbuffer.c
+++ src/gallium/state_trackers/wgl/stw_ext_pbuffer.c
@@ -82,7 +82,7 @@ wglCreatePbufferARB(HDC _hDC,
    HWND hWnd;
    HDC hDC;
 
-   info = stw_pixelformat_get_info(iPixelFormat);
+   info = stw_pixelformat_get_info(iPixelFormat - 1);
    if (!info) {
       SetLastError(ERROR_INVALID_PIXEL_FORMAT);
       return 0;
