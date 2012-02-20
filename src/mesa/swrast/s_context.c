cb49def3d6f946def1f53c736d1179ed9b576ab1
Brian Paul <brianp@vmware.com>
2012-02-20 11:07:00 -0700

    swrast: check max renderbuffer size against SWRAST_MAX_WIDTH

--- src/mesa/swrast/s_context.c
+++ src/mesa/swrast/s_context.c
@@ -729,6 +729,8 @@ _swrast_CreateContext( struct gl_context *ctx )
    assert(ctx->Const.MaxViewportWidth <= SWRAST_MAX_WIDTH);
    assert(ctx->Const.MaxViewportHeight <= SWRAST_MAX_WIDTH);
 
+   assert(ctx->Const.MaxRenderbufferSize <= SWRAST_MAX_WIDTH);
+
    /* make sure largest texture image is <= SWRAST_MAX_WIDTH in size */
    assert((1 << (ctx->Const.MaxTextureLevels - 1)) <= SWRAST_MAX_WIDTH);
    assert((1 << (ctx->Const.MaxCubeTextureLevels - 1)) <= SWRAST_MAX_WIDTH);
