4aa1ac5fe94b5696095229ee3568bf4fa7cfed95
nobled <nobled@dreamwidth.org>
2012-03-04 15:20:31 -0500

    dri/nouveau: don't use nested functions
    It's a GNU extension that isn't supported by clang right now:
    http://gcc.gnu.org/onlinedocs/gcc-4.6.3/gcc/Nested-Functions.html
    http://clang.llvm.org/docs/UsersManual.html#c_unimpl_gcc
    
    With this, clang now compiles the nouveau classic driver.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=44061
    
    (Types changed from e.g. 'unsigned char' to 'GLubyte' so that the types can
    be concatenated to form a unique function name without any whitespace
    interfering.)
    
    [ Francisco Jerez: give meaningful names to the dispatch functions. ]

--- src/mesa/drivers/dri/nouveau/nouveau_array.c
+++ src/mesa/drivers/dri/nouveau/nouveau_array.c
@@ -29,54 +29,71 @@
 #include "nouveau_array.h"
 #include "nouveau_bufferobj.h"
 
+#define EXTRACT(in_t, out_t) extract_func_##in_t##_to_##out_t
+
+#define EXTRACT_FUNC(in_t, out_t, k)			\
+static out_t EXTRACT(in_t, out_t)			\
+(struct nouveau_array *a, int i, int j) {		\
+	in_t x = ((in_t *)(a->buf + i * a->stride))[j];	\
+							\
+	return (out_t)x / (k);				\
+}
+
+EXTRACT_FUNC(GLchar, unsigned, 1);
+EXTRACT_FUNC(GLchar, float, SCHAR_MAX);
+EXTRACT_FUNC(GLubyte, unsigned, 1);
+EXTRACT_FUNC(GLubyte, float, UCHAR_MAX);
+EXTRACT_FUNC(GLshort, unsigned, 1);
+EXTRACT_FUNC(GLshort, float, SHRT_MAX);
+EXTRACT_FUNC(GLushort, unsigned, 1);
+EXTRACT_FUNC(GLushort, float, USHRT_MAX);
+EXTRACT_FUNC(GLint, unsigned, 1);
+EXTRACT_FUNC(GLint, float, INT_MAX);
+EXTRACT_FUNC(GLuint, unsigned, 1);
+EXTRACT_FUNC(GLuint, float, UINT_MAX);
+EXTRACT_FUNC(GLfloat, unsigned, 1.0 / UINT_MAX);
+EXTRACT_FUNC(GLfloat, float, 1);
+
+#undef EXTRACT_FUNC
+
 static void
 get_array_extract(struct nouveau_array *a, extract_u_t *extract_u,
 		  extract_f_t *extract_f)
 {
-#define EXTRACT(in_t, out_t, k)						\
-	({								\
-		auto out_t f(struct nouveau_array *, int, int);		\
-		out_t f(struct nouveau_array *a, int i, int j) {	\
-			in_t x = ((in_t *)(a->buf + i * a->stride))[j];	\
-									\
-			return (out_t)x / (k);				\
-		};							\
-		f;							\
-	});
-
 	switch (a->type) {
 	case GL_BYTE:
-		*extract_u = EXTRACT(char, unsigned, 1);
-		*extract_f = EXTRACT(char, float, SCHAR_MAX);
+		*extract_u = EXTRACT(GLchar, unsigned);
+		*extract_f = EXTRACT(GLchar, float);
 		break;
 	case GL_UNSIGNED_BYTE:
-		*extract_u = EXTRACT(unsigned char, unsigned, 1);
-		*extract_f = EXTRACT(unsigned char, float, UCHAR_MAX);
+		*extract_u = EXTRACT(GLubyte, unsigned);
+		*extract_f = EXTRACT(GLubyte, float);
 		break;
 	case GL_SHORT:
-		*extract_u = EXTRACT(short, unsigned, 1);
-		*extract_f = EXTRACT(short, float, SHRT_MAX);
+		*extract_u = EXTRACT(GLshort, unsigned);
+		*extract_f = EXTRACT(GLshort, float);
 		break;
 	case GL_UNSIGNED_SHORT:
-		*extract_u = EXTRACT(unsigned short, unsigned, 1);
-		*extract_f = EXTRACT(unsigned short, float, USHRT_MAX);
+		*extract_u = EXTRACT(GLushort, unsigned);
+		*extract_f = EXTRACT(GLushort, float);
 		break;
 	case GL_INT:
-		*extract_u = EXTRACT(int, unsigned, 1);
-		*extract_f = EXTRACT(int, float, INT_MAX);
+		*extract_u = EXTRACT(GLint, unsigned);
+		*extract_f = EXTRACT(GLint, float);
 		break;
 	case GL_UNSIGNED_INT:
-		*extract_u = EXTRACT(unsigned int, unsigned, 1);
-		*extract_f = EXTRACT(unsigned int, float, UINT_MAX);
+		*extract_u = EXTRACT(GLuint, unsigned);
+		*extract_f = EXTRACT(GLuint, float);
 		break;
 	case GL_FLOAT:
-		*extract_u = EXTRACT(float, unsigned, 1.0 / UINT_MAX);
-		*extract_f = EXTRACT(float, float, 1);
+		*extract_u = EXTRACT(GLfloat, unsigned);
+		*extract_f = EXTRACT(GLfloat, float);
 		break;
 	default:
 		assert(0);
 	}
 }
+#undef EXTRACT
 
 void
 nouveau_init_array(struct nouveau_array *a, int attr, int stride,
--- src/mesa/drivers/dri/nouveau/nouveau_render_t.c
+++ src/mesa/drivers/dri/nouveau/nouveau_render_t.c
@@ -97,52 +97,49 @@
 		}							\
 	} while (0)
 
+static void
+dispatch_l(struct gl_context *ctx, unsigned int start, int delta,
+	   unsigned int n)
+{
+	struct nouveau_channel *chan = context_chan(ctx);
+	RENDER_LOCALS(ctx);
+
+	EMIT_VBO(L, ctx, start, delta, n);
+}
+
+static void
+dispatch_i32(struct gl_context *ctx, unsigned int start, int delta,
+	     unsigned int n)
+{
+	struct nouveau_channel *chan = context_chan(ctx);
+	RENDER_LOCALS(ctx);
+
+	EMIT_VBO(I32, ctx, start, delta, n);
+}
+
+static void
+dispatch_i16(struct gl_context *ctx, unsigned int start, int delta,
+	     unsigned int n)
+{
+	struct nouveau_channel *chan = context_chan(ctx);
+	RENDER_LOCALS(ctx);
+
+	EMIT_VBO(I32, ctx, start, delta, n & 1);
+	EMIT_VBO(I16, ctx, start, delta, n & ~1);
+}
+
 /*
  * Select an appropriate dispatch function for the given index buffer.
  */
 static dispatch_t
 get_array_dispatch(struct nouveau_array *a)
 {
-	if (!a->fields) {
-		auto void f(struct gl_context *, unsigned int, int, unsigned int);
-
-		void f(struct gl_context *ctx, unsigned int start, int delta,
-		       unsigned int n) {
-			struct nouveau_channel *chan = context_chan(ctx);
-			RENDER_LOCALS(ctx);
-
-			EMIT_VBO(L, ctx, start, delta, n);
-		};
-
-		return f;
-
-	} else if (a->type == GL_UNSIGNED_INT) {
-		auto void f(struct gl_context *, unsigned int, int, unsigned int);
-
-		void f(struct gl_context *ctx, unsigned int start, int delta,
-		       unsigned int n) {
-			struct nouveau_channel *chan = context_chan(ctx);
-			RENDER_LOCALS(ctx);
-
-			EMIT_VBO(I32, ctx, start, delta, n);
-		};
-
-		return f;
-
-	} else {
-		auto void f(struct gl_context *, unsigned int, int, unsigned int);
-
-		void f(struct gl_context *ctx, unsigned int start, int delta,
-		       unsigned int n) {
-			struct nouveau_channel *chan = context_chan(ctx);
-			RENDER_LOCALS(ctx);
-
-			EMIT_VBO(I32, ctx, start, delta, n & 1);
-			EMIT_VBO(I16, ctx, start, delta, n & ~1);
-		};
-
-		return f;
-	}
+	if (!a->fields)
+		return dispatch_l;
+	else if (a->type == GL_UNSIGNED_INT)
+		return dispatch_i32;
+	else
+		return dispatch_i16;
 }
 
 /*
