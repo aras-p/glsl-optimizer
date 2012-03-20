20836c81851e0df29a8ee9c86e5e5388738c840b
Brian Paul <brianp@vmware.com>
2012-03-20 17:43:52 -0600

    st/mesa: set MaxUnrollIterations = 255
    The default was 32 for the EmitNoLoops=0 case.  This allows the oZone3D
    soft shadows test to work properly with the vmware driver.  Jose reported
    that SM3 supports up to 255 loop iterations.
    
    NOTE: This is a candidate for the 8.0 branch.
    
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -216,6 +216,8 @@ void st_init_limits(struct st_context *st)
 
       if (options->EmitNoLoops)
          options->MaxUnrollIterations = MIN2(screen->get_shader_param(screen, sh, PIPE_SHADER_CAP_MAX_INSTRUCTIONS), 65536);
+      else
+         options->MaxUnrollIterations = 255; /* SM3 limit */
    }
 
    /* PIPE_SHADER_CAP_MAX_INPUTS for the FS specifies the maximum number
