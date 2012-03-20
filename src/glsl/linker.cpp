7feabfe23dc54960abba34755f484f786575ac49
Brian Paul <brianp@vmware.com>
2012-03-20 17:43:12 -0600

    glsl: propagate MaxUnrollIterations to the optimizer's loop unroller
    Instead of the hard-coded value of 32.  Note that MaxUnrollIterations
    defaults to 32 so there's no net change.  But the gallium state tracker
    can override this.
    
    NOTE: This is a candidate for the 8.0 branch.
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/glsl/linker.cpp
+++ src/glsl/linker.cpp
@@ -2294,7 +2294,9 @@ link_shaders(struct gl_context *ctx, struct gl_shader_program *prog)
       if (ctx->ShaderCompilerOptions[i].LowerClipDistance)
          lower_clip_distance(prog->_LinkedShaders[i]->ir);
 
-      while (do_common_optimization(prog->_LinkedShaders[i]->ir, true, false, 32))
+      unsigned max_unroll = ctx->ShaderCompilerOptions[i].MaxUnrollIterations;
+
+      while (do_common_optimization(prog->_LinkedShaders[i]->ir, true, false, max_unroll))
 	 ;
    }
 
