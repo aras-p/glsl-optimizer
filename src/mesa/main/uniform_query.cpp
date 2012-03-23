0f3aa9f4bd7daab354deb9268307c5628b460f6b
Vinson Lee <vlee@freedesktop.org>
2012-03-22 23:59:52 -0700

    mesa: Fix memory leak in _mesa_get_uniform_location.
    Fixes Coverity resource leak defect.
    
    NOTE: This is a candidate for the 8.0 branch.
    
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Reviewed-by: José Fonseca <jfonseca@vmware.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/main/uniform_query.cpp
+++ src/mesa/main/uniform_query.cpp
@@ -905,8 +905,10 @@ _mesa_get_uniform_location(struct gl_context *ctx,
       name_copy[i-1] = '\0';
 
       offset = strtol(&name[i], NULL, 10);
-      if (offset < 0)
+      if (offset < 0) {
+	 free(name_copy);
 	 return -1;
+      }
 
       array_lookup = true;
    } else {
