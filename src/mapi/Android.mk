aras-p/glsl-optimizer

commit ca760181b4420696c7e86aa2951d7203522ad1e8

shared-glapi: Convert to automake
This fixes a build problem where EGL links to libgbm.la, which encodes
a relative path to it's libglapi.so dependency.  The relative path
breaks when the linker tries to resolve it from src/egl/main instead
of src/gbm.  Typically we silently fall back to the system
libglapi.so, which is wrong and breaks when there isn't one.

Morale of the story: don't mix mklib and libtool.

src/mapi/Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# get MAPI_GLAPI_SOURCES
+# get MAPI_GLAPI_FILES
 include $(LOCAL_PATH)/mapi/sources.mak
 
 mapi_abi_headers :=
@@ -38,7 +38,7 @@ include $(CLEAR_VARS)
 
 abi_header := shared-glapi/glapi_mapi_tmp.h
 
-LOCAL_SRC_FILES := $(addprefix mapi/, $(MAPI_GLAPI_SOURCES))
+LOCAL_SRC_FILES := $(MAPI_GLAPI_FILES)
 
 LOCAL_CFLAGS := \
 	-DMAPI_MODE_GLAPI \

configure.ac
@@ -702,6 +702,7 @@ if test "x$enable_shared_glapi" = xyes; then
     # libGL will use libglapi for function lookups (IN_DRI_DRIVER means to use
     # the remap table)
     DEFINES="$DEFINES -DIN_DRI_DRIVER"
+    SRC_DIRS="$SRC_DIRS mapi/shared-glapi"
 fi
 AC_SUBST([SHARED_GLAPI])
 AM_CONDITIONAL(HAVE_SHARED_GLAPI, test $SHARED_GLAPI = 1)
@@ -1985,6 +1986,7 @@ AC_CONFIG_FILES([configs/autoconf
 		src/egl/wayland/wayland-egl/wayland-egl.pc
 		src/egl/wayland/wayland-drm/Makefile
 		src/glx/Makefile
+		src/mapi/shared-glapi/Makefile
 		src/mesa/drivers/dri/dri.pc
 		src/mesa/drivers/dri/Makefile
 		src/mesa/drivers/dri/common/Makefile

src/egl/main/Makefile.am
@@ -93,7 +93,6 @@ endif
 if HAVE_EGL_PLATFORM_DRM
 AM_CFLAGS += -DHAVE_DRM_PLATFORM
 libEGL_la_LIBADD += ../../gbm/libgbm.la
-libEGL_la_LIBADD += ../../gbm/libgbm.la
 endif
 
 if HAVE_EGL_PLATFORM_FBDEV

src/gbm/Makefile.am
@@ -31,8 +31,8 @@ libgbm_dri_la_CFLAGS = \
 	-DDEFAULT_DRIVER_DIR='"$(DRI_DRIVER_SEARCH_DIR)"' \
 	$(LIBDRM_CFLAGS)
 
-libgbm_la_LDFLAGS += -L$(top_builddir)/$(LIB_DIR)
-libgbm_la_LIBADD += libgbm_dri.la -lglapi
+libgbm_la_LIBADD += \
+	libgbm_dri.la $(top_builddir)/src/mapi/shared-glapi/libglapi.la
 endif
 
 all-local: libgbm.la

src/glx/Makefile.am
@@ -21,7 +21,7 @@
 
 if HAVE_SHARED_GLAPI
 SHARED_GLAPI_CFLAGS = -DGLX_SHARED_GLAPI
-SHARED_GLAPI_LIBS = -L$(top_builddir)/$(LIB_DIR) -lglapi
+SHARED_GLAPI_LIBS = $(top_builddir)/src/mapi/shared-glapi/libglapi.la
 endif
 
 GLAPI_LIB = ../mapi/glapi/libglapi.a

src/mapi/es1api/Makefile
@@ -41,8 +41,8 @@ esapi_CPPFLAGS := \
 	-DMAPI_ABI_HEADER=\"$(ESAPI)/glapi_mapi_tmp.h\"
 
 include $(MAPI)/sources.mak
-esapi_SOURCES := $(addprefix $(MAPI)/, $(MAPI_BRIDGE_SOURCES))
-esapi_OBJECTS := $(MAPI_BRIDGE_SOURCES:.c=.o)
+esapi_SOURCES := $(MAPI_BRIDGE_FILES)
+esapi_OBJECTS := $(notdir $(MAPI_BRIDGE_FILES:.c=.o))
 esapi_CPPFLAGS += -DMAPI_MODE_BRIDGE
 
 esapi_LIB_DEPS := -L$(TOP)/$(LIB_DIR) -l$(GLAPI_LIB) $(esapi_LIB_DEPS)

src/mapi/glapi/Makefile
@@ -19,18 +19,18 @@ ifeq ($(SHARED_GLAPI),1)
 glapi_CPPFLAGS += \
 	-DMAPI_MODE_BRIDGE \
 	-DMAPI_ABI_HEADER=\"glapi/glapi_mapi_tmp.h\"
-glapi_SOURCES := $(addprefix $(MAPI)/, $(MAPI_BRIDGE_SOURCES))
+glapi_SOURCES := $(MAPI_BRIDGE_FILES)
 
 glapi_GLAPI_OBJECTS :=
 glapi_ASM_OBJECTS :=
-glapi_MAPI_OBJECTS := $(MAPI_BRIDGE_SOURCES:.c=.o)
+glapi_MAPI_OBJECTS := $(notdir $(MAPI_BRIDGE_FILES:.c=.o))
 else
 glapi_CPPFLAGS += -DMAPI_MODE_UTIL
-glapi_SOURCES := $(GLAPI_SOURCES) $(addprefix $(MAPI)/, $(MAPI_UTIL_SOURCES))
+glapi_SOURCES := $(GLAPI_SOURCES) $(MAPI_UTIL_FILES)
 
 glapi_GLAPI_OBJECTS := $(GLAPI_SOURCES:.c=.o)
 glapi_ASM_OBJECTS := $(GLAPI_ASM_SOURCES:.S=.o)
-glapi_MAPI_OBJECTS := $(MAPI_UTIL_SOURCES:.c=.o)
+glapi_MAPI_OBJECTS := $(notdir $(MAPI_UTIL_FILES:.c=.o))
 endif # SHARED_GLAPI
 
 glapi_OBJECTS := \

src/mapi/mapi/sources.mak
@@ -3,34 +3,34 @@
 # mapi may be used in several ways
 #
 #  - In default mode, mapi implements the interface defined by mapi.h.  To use
-#    this mode, compile MAPI_SOURCES.
+#    this mode, compile MAPI_FILES.
 #
 #  - In util mode, mapi provides utility functions for use with glapi.  To use
-#    this mode, compile MAPI_UTIL_SOURCES with MAPI_MODE_UTIL defined.
+#    this mode, compile MAPI_UTIL_FILES with MAPI_MODE_UTIL defined.
 #
 #  - In glapi mode, mapi implements the interface defined by glapi.h.  To use
-#    this mode, compile MAPI_GLAPI_SOURCES with MAPI_MODE_GLAPI defined.
+#    this mode, compile MAPI_GLAPI_FILES with MAPI_MODE_GLAPI defined.
 #
 #  - In bridge mode, mapi provides entry points calling into glapi.  To use
-#    this mode, compile MAPI_BRIDGE_SOURCES with MAPI_MODE_BRIDGE defined.
+#    this mode, compile MAPI_BRIDGE_FILES with MAPI_MODE_BRIDGE defined.
 
-MAPI_UTIL_SOURCES = \
-	u_current.c \
-	u_execmem.c
+MAPI_UTIL_FILES = \
+	$(TOP)/src/mapi/mapi/u_current.c \
+	$(TOP)/src/mapi/mapi/u_execmem.c
 
-MAPI_SOURCES = \
-	entry.c \
-	mapi.c \
-	stub.c \
-	table.c \
-	$(MAPI_UTIL_SOURCES)
+MAPI_FILES = \
+	$(TOP)/src/mapi/mapi/entry.c \
+	$(TOP)/src/mapi/mapi/mapi.c \
+	$(TOP)/src/mapi/mapi/stub.c \
+	$(TOP)/src/mapi/mapi/table.c \
+	$(MAPI_UTIL_FILES)
 
-MAPI_GLAPI_SOURCES = \
-	entry.c \
-	mapi_glapi.c \
-	stub.c \
-	table.c \
-	$(MAPI_UTIL_SOURCES)
+MAPI_GLAPI_FILES = \
+	$(TOP)/src/mapi/mapi/entry.c \
+	$(TOP)/src/mapi/mapi/mapi_glapi.c \
+	$(TOP)/src/mapi/mapi/stub.c \
+	$(TOP)/src/mapi/mapi/table.c \
+	$(MAPI_UTIL_FILES)
 
-MAPI_BRIDGE_SOURCES = \
-	entry.c
+MAPI_BRIDGE_FILES = \
+	$(TOP)/src/mapi/mapi/entry.c

commit 5154b45217695e5daf24110bcff043fa1959d0a5

mapi: Fix Android build
The Android build was broken by
    commit ca760181b4420696c7e86aa2951d7203522ad1e8
    Author: Kristian Høgsberg <krh@bitplanet.net>
    Date:   Fri Mar 16 12:55:40 2012 -0400

    shared-glapi: Convert to automake

The offending change was that it redefined the filepaths in sources.mak
like this:
    - FOO_FILES := bar.c
    + FOO_FILES := $(TOP)/src/mapi/mapi/bar.c
This broke the build because source filepaths in Android makefiles must be
relative to the makefile.

Ideally, this could be fixed by reverting the change in sources.mak and
making shared-glapi's Makefile.am use $(addprefix $(TOP)/src/mapi/mapi,
$(FOO_FILES)). However, automake doesn't understand builtin GNU make
functions, such as addprefix. So, it seems that automake and Android can
no longer share sources.mak.

Fix the build by duplicating the source lists from sources.mak into
Android.mk.

Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

src/mapi/Android.mk
@@ -25,9 +25,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# get MAPI_GLAPI_FILES
-include $(LOCAL_PATH)/mapi/sources.mak
-
 mapi_abi_headers :=
 
 # ---------------------------------------
@@ -38,7 +35,13 @@ include $(CLEAR_VARS)
 
 abi_header := shared-glapi/glapi_mapi_tmp.h
 
-LOCAL_SRC_FILES := $(MAPI_GLAPI_FILES)
+LOCAL_SRC_FILES := \
+	mapi/entry.c \
+	mapi/mapi_glapi.c \
+	mapi/stub.c \
+	mapi/table.c \
+	mapi/u_current.c \
+	mapi/u_execmem.c
 
 LOCAL_CFLAGS := \
 	-DMAPI_MODE_GLAPI \