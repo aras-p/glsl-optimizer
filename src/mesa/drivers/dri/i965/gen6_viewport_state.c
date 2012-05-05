85cd30406ff8414bc6ac6de1064d059a4c564160
Kenneth Graunke <kenneth@whitecape.org>
2012-05-04 17:06:38 -0700

    i965: Implement guardband clipping on Sandybridge.
    Improves performance in Citybench:
    - 320x240:  19.8008% +/- 0.937818%
    - 1280x480: 6.53856% +/- 0.859083%
    
    No apparent difference in OpenArena nor Xonotic.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/mesa/drivers/dri/i965/gen6_viewport_state.c
+++ src/mesa/drivers/dri/i965/gen6_viewport_state.c
@@ -33,31 +33,35 @@
 
 /* The clip VP defines the guardband region where expensive clipping is skipped
  * and fragments are allowed to be generated and clipped out cheaply by the SF.
- *
- * By setting it to NDC bounds of [-1,1], we don't do GB clipping.  It's
- * supposed to cause seams to become visible in apps due to shared edges taking
- * different clip/no clip paths depending on whether the rest of the prim ends
- * up in the guardband or not.
  */
 static void
 gen6_upload_clip_vp(struct brw_context *brw)
 {
+   struct gl_context *ctx = &brw->intel.ctx;
    struct brw_clipper_viewport *vp;
 
    vp = brw_state_batch(brw, AUB_TRACE_CLIP_VP_STATE,
 			sizeof(*vp), 32, &brw->clip.vp_offset);
 
-   vp->xmin = -1.0;
-   vp->xmax = 1.0;
-   vp->ymin = -1.0;
-   vp->ymax = 1.0;
+   /* According to the Sandybridge PRM, Volume 2, Part 1, Section 6.3.8
+    * "Vertex X,Y Clamping and Quantization", the screen-aligned 2D
+    * bounding-box of an object must not exceed 16K pixels in either X or Y.
+    */
+   const float maximum_post_clamp_delta = 16384;
+   float gbx = maximum_post_clamp_delta / (float) ctx->Viewport.Width;
+   float gby = maximum_post_clamp_delta / (float) ctx->Viewport.Height;
+
+   vp->xmin = -gbx;
+   vp->xmax = gbx;
+   vp->ymin = -gby;
+   vp->ymax = gby;
 
    brw->state.dirty.cache |= CACHE_NEW_CLIP_VP;
 }
 
 const struct brw_tracked_state gen6_clip_vp = {
    .dirty = {
-      .mesa = 0,
+      .mesa = _NEW_VIEWPORT,
       .brw = BRW_NEW_BATCH,
       .cache = 0,
    },
