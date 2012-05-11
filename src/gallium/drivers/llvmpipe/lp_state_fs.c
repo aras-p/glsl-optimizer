550de24c17fd4873b0200c0d4a61656e184631ac
Brian Paul <brianp@vmware.com>
2012-05-11 16:16:11 -0600

    llvmpipe: add cast to silence warning

--- src/gallium/drivers/llvmpipe/lp_state_fs.c
+++ src/gallium/drivers/llvmpipe/lp_state_fs.c
@@ -1174,7 +1174,8 @@ llvmpipe_set_constant_buffer(struct pipe_context *pipe,
    const void *data;
 
    if (cb && cb->user_buffer) {
-      constants = llvmpipe_user_buffer_create(pipe->screen, cb->user_buffer,
+      constants = llvmpipe_user_buffer_create(pipe->screen,
+                                              (void *) cb->user_buffer,
                                               cb->buffer_size,
                                               PIPE_BIND_CONSTANT_BUFFER);
    }
