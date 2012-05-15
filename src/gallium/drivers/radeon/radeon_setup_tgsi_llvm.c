63a85952711415ab151a39d21c4a67da97f2734e
Vadim Girlin <vadimgirlin@gmail.com>
2012-05-15 18:48:06 +0400

    radeon/llvm: use correct intrinsic for CEIL
    Should be round_posinf instead of round_neginf.
    
    Signed-off-by: Vadim Girlin <vadimgirlin@gmail.com>
    Reviewed-by: Tom Stellard <thomas.stellard@amd.com>

--- src/gallium/drivers/radeon/radeon_setup_tgsi_llvm.c
+++ src/gallium/drivers/radeon/radeon_setup_tgsi_llvm.c
@@ -968,7 +968,7 @@ void radeon_llvm_context_init(struct radeon_llvm_context * ctx)
 	bld_base->op_actions[TGSI_OPCODE_TXQ].fetch_args = tex_fetch_args;
 	bld_base->op_actions[TGSI_OPCODE_TXQ].intr_name = "llvm.AMDGPU.txq";
 	bld_base->op_actions[TGSI_OPCODE_CEIL].emit = lp_build_tgsi_intrinsic;
-	bld_base->op_actions[TGSI_OPCODE_CEIL].intr_name = "llvm.AMDIL.round.neginf.";
+	bld_base->op_actions[TGSI_OPCODE_CEIL].intr_name = "llvm.AMDIL.round.posinf.";
 
 
 
