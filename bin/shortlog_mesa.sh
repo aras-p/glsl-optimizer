c1dcf9665c770d25ba8cbe827f4e3e69349d2665
Andreas Boll <andreas.boll.dev@gmail.com>
2012-06-14 08:25:42 -0600

    mesa: fix html in shortlog_mesa.sh script
    Signed-off-by: Brian Paul <brianp@vmware.com>

--- bin/shortlog_mesa.sh
+++ bin/shortlog_mesa.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is used to generate the list of changes that
 # appears in the release notes files, with HTML formatting.
@@ -9,15 +9,15 @@ typeset -i in_log=0
 git shortlog $* | while read l
 do
     if [ $in_log -eq 0 ]; then
-	echo '<p>'$l
+	echo '<p>'$l'</p>'
 	echo '<ul>'
 	in_log=1
     elif echo "$l" | egrep -q '^$' ; then
-	echo '</ul></p>'
+	echo '</ul>'
 	echo
 	in_log=0
     else
-        mesg=$(echo $l | sed 's/ (cherry picked from commit [0-9a-f]\+)//;s/\&/&amp;/g;s/</\&lt/g;s/>/\&gt;/g')
+        mesg=$(echo $l | sed 's/ (cherry picked from commit [0-9a-f]\+)//;s/\&/&amp;/g;s/</\&lt;/g;s/>/\&gt;/g')
 	echo '  <li>'${mesg}'</li>'
     fi
 done
