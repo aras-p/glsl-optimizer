b5c037f6b1cd54f72c3575c33adb2bcdaae678d7
Eric Anholt <eric@anholt.net>
2012-07-04 13:12:09 -0700

    Add emacs setup for the docs/devinfo.html comment wrapping recommendation.
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
   (tab-width . 8)
   (c-basic-offset . 3)
   (c-file-style . "stroustrup")
+  (fill-column . 78)
   (eval . (progn
 	    (c-set-offset 'innamespace '0)
 	    (c-set-offset 'inline-open '0)))
