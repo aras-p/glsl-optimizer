b656df990f8c1d7468e11afa2079511d8b1febf4
Kenneth Graunke <kenneth@whitecape.org>
2012-07-09 12:36:49 -0700

    glsl: Initialize coordinate to NULL in ir_texture constructor.
    I ran into this while trying to create a TXS query, which doesn't have a
    coordinate.  Since it didn't get initialized to NULL, a bunch of
    visitors tried to access it and crashed.
    
    Most of the time, this won't be a problem, but it's just a good idea.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/glsl/ir.h
+++ src/glsl/ir.h
@@ -1341,7 +1341,8 @@ enum ir_texture_opcode {
 class ir_texture : public ir_rvalue {
 public:
    ir_texture(enum ir_texture_opcode op)
-      : op(op), projector(NULL), shadow_comparitor(NULL), offset(NULL)
+      : op(op), coordinate(NULL), projector(NULL), shadow_comparitor(NULL),
+        offset(NULL)
    {
       this->ir_type = ir_type_texture;
    }
