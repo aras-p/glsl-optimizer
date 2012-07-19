73fef0178ad9fb0fc47679fcc485144b4ef08fb6
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2012-07-19 12:30:11 +1000

    build/mapi: More killing of TOP in favour of top_srcdir
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>

--- src/mapi/glapi/gen/glapi_gen.mk
+++ src/mapi/glapi/gen/glapi_gen.mk
@@ -1,14 +1,14 @@
 # Helpers for glapi header generation
 
-ifndef TOP
-$(error TOP must be defined.)
+ifndef top_srcdir
+$(error top_srcdir must be defined.)
 endif
 
 glapi_gen_common_deps := \
-	$(wildcard $(TOP)/src/mapi/glapi/gen/*.xml) \
-	$(wildcard $(TOP)/src/mapi/glapi/gen/*.py)
+	$(wildcard $(top_srcdir)/src/mapi/glapi/gen/*.xml) \
+	$(wildcard $(top_srcdir)/src/mapi/glapi/gen/*.py)
 
-glapi_gen_mapi_script := $(TOP)/src/mapi/mapi/mapi_abi.py
+glapi_gen_mapi_script := $(top_srcdir)/src/mapi/mapi/mapi_abi.py
 glapi_gen_mapi_deps := \
 	$(glapi_gen_mapi_script) \
 	$(glapi_gen_common_deps)
@@ -21,7 +21,7 @@ $(AM_V_GEN)$(PYTHON2) $(PYTHON_FLAGS) $(glapi_gen_mapi_script) \
 	--mode lib --printer $(2) $(1) > $@
 endef
 
-glapi_gen_dispatch_script := $(TOP)/src/mapi/glapi/gen/gl_table.py
+glapi_gen_dispatch_script := $(top_srcdir)/src/mapi/glapi/gen/gl_table.py
 glapi_gen_dispatch_deps := $(glapi_gen_common_deps)
 
 # $(1): path to an XML file
@@ -32,7 +32,7 @@ $(AM_V_GEN)$(PYTHON2) $(PYTHON_FLAGS) $(glapi_gen_dispatch_script) \
 	-f $(1) -m remap_table $(if $(2),-c $(2),) > $@
 endef
 
-glapi_gen_remap_script := $(TOP)/src/mapi/glapi/gen/remap_helper.py
+glapi_gen_remap_script := $(top_srcdir)/src/mapi/glapi/gen/remap_helper.py
 glapi_gen_remap_deps := $(glapi_gen_common_deps)
 
 # $(1): path to an XML file
