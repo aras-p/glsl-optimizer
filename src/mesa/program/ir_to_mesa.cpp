af3fc6bb2836ce545c624bce2b47c3fd8cf4f9fa
Eric Anholt <eric@anholt.net>
2012-07-20 16:49:15 -0700

    ir_to_mesa: Don't whack the ->location field of uniform block variables.
    Fixes some failures in GL_ARB_uniform_buffer_object/maxblocks.
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/program/ir_to_mesa.cpp
+++ src/mesa/program/ir_to_mesa.cpp
@@ -2459,7 +2459,7 @@ _mesa_generate_parameters_list_for_uniforms(struct gl_shader_program
       ir_variable *var = ((ir_instruction *) node)->as_variable();
 
       if ((var == NULL) || (var->mode != ir_var_uniform)
-	  || (strncmp(var->name, "gl_", 3) == 0))
+	  || var->uniform_block != -1 || (strncmp(var->name, "gl_", 3) == 0))
 	 continue;
 
       add.process(var);
