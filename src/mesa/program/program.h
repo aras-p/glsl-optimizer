59c67f81165fadd46c99cda5623397c798f015eb
Brian Paul <brianp@vmware.com>
2012-07-27 15:43:53 -0600

    mesa: whitespace, etc. fixes in program.h

--- src/mesa/program/program.h
+++ src/mesa/program/program.h
@@ -183,13 +183,11 @@ _mesa_valid_register_index(const struct gl_context *ctx,
 extern void
 _mesa_postprocess_program(struct gl_context *ctx, struct gl_program *prog);
 
-/* keep these in the same order as TGSI_PROCESSOR_* */
 
 static inline GLuint
 _mesa_program_target_to_index(GLenum v)
 {
-   switch(v)
-   {
+   switch (v) {
    case GL_VERTEX_PROGRAM_ARB:
       return MESA_SHADER_VERTEX;
    case GL_FRAGMENT_PROGRAM_ARB:
@@ -205,10 +203,10 @@ _mesa_program_target_to_index(GLenum v)
 static inline GLenum
 _mesa_program_index_to_target(GLuint i)
 {
-   GLenum enums[MESA_SHADER_TYPES] = {
-         GL_VERTEX_PROGRAM_ARB,
-         GL_FRAGMENT_PROGRAM_ARB,
-         GL_GEOMETRY_PROGRAM_NV,
+   static const GLenum enums[MESA_SHADER_TYPES] = {
+      GL_VERTEX_PROGRAM_ARB,
+      GL_FRAGMENT_PROGRAM_ARB,
+      GL_GEOMETRY_PROGRAM_NV,
    };
    if(i >= MESA_SHADER_TYPES)
       return 0;
