87cdefed405da1a57a006737297d20ba0c25fa1b
Kenneth Graunke <kenneth@whitecape.org>
2012-08-26 00:07:56 -0700

    i965: Double the size of the state cache.
    We probably want to do something more sophisticated here, but this at
    least makes it through L4D2 without dumping the program cache.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/mesa/drivers/dri/i965/brw_state_cache.c
+++ src/mesa/drivers/dri/i965/brw_state_cache.c
@@ -372,10 +372,10 @@ brw_clear_cache(struct brw_context *brw, struct brw_cache *cache)
 void
 brw_state_cache_check_size(struct brw_context *brw)
 {
-   /* un-tuned guess.  Each object is generally a page, so 1000 of them is 4 MB of
+   /* un-tuned guess.  Each object is generally a page, so 2000 of them is 8 MB of
     * state cache.
     */
-   if (brw->cache.n_items > 1000) {
+   if (brw->cache.n_items > 2000) {
       perf_debug("Exceeded state cache size limit.  Clearing the set "
                  "of compiled programs, which will trigger recompiles\n");
       brw_clear_cache(brw, &brw->cache);
