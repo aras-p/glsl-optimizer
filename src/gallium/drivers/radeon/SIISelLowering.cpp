f9fede884b7ace711ccf63152afdbdaf209edced
Tom Stellard <thomas.stellard@amd.com>
2012-08-31 16:11:38 -0400

    radeon/llvm: Fix encoding of V_CNDMASK_B32
    The CodeEmitter was not setting the VGPR bit for src0, because the
    instruction definition had the VCC register in the src0 slot, instead of
    the actual src0 register.  This has been fixed by moving the VCC
    register to the end of the operand list.

--- src/gallium/drivers/radeon/SIISelLowering.cpp
+++ src/gallium/drivers/radeon/SIISelLowering.cpp
@@ -242,9 +242,9 @@ void SITargetLowering::LowerSI_V_CNDLT(MachineInstr *MI, MachineBasicBlock &BB,
 
   BuildMI(BB, I, BB.findDebugLoc(I), TII->get(AMDGPU::V_CNDMASK_B32))
           .addOperand(MI->getOperand(0))
-	  .addReg(AMDGPU::VCC)
           .addOperand(MI->getOperand(2))
-          .addOperand(MI->getOperand(3));
+          .addOperand(MI->getOperand(3))
+	  .addReg(AMDGPU::VCC);
 
   MI->eraseFromParent();
 }
