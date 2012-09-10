97a693d1fa8b5ad85309cf5576be08a5a77f11ff
Imre Deak <imre.deak@intel.com>
2012-09-10 09:41:38 +0300

    mesa: glGet: fix indentation of find_value
    No functional change.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Brian Paul <brianp@vmware.com>

--- src/mesa/main/get.c
+++ src/mesa/main/get.c
@@ -1992,13 +1992,13 @@ find_value(const char *func, GLenum pname, void **p, union value *v)
       /* If the enum isn't valid, the hash walk ends with index 0,
        * which is the API mask entry at the beginning of values[]. */
       if (unlikely(d->type == TYPE_API_MASK)) {
-	 _mesa_error(ctx, GL_INVALID_ENUM, "%s(pname=%s)", func,
-                     _mesa_lookup_enum_by_nr(pname));
-	 return &error_value;
+         _mesa_error(ctx, GL_INVALID_ENUM, "%s(pname=%s)", func,
+               _mesa_lookup_enum_by_nr(pname));
+         return &error_value;
       }
 
       if (likely(d->pname == pname))
-	 break;
+         break;
 
       hash += prime_step;
    }
