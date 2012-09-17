7b6b447fa3f11c96c417cbf857757591e7bfb235
Brian Paul <brianp@vmware.com>
2012-09-16 18:59:51 -0600

    softpipe: update SP_MAX_TEXTURE_3D_LEVELS comment
    9 levels = max size of 256 texels.

--- src/gallium/drivers/softpipe/sp_limits.h
+++ src/gallium/drivers/softpipe/sp_limits.h
@@ -31,7 +31,7 @@
 
 
 #define SP_MAX_TEXTURE_2D_LEVELS 15  /* 16K x 16K */
-#define SP_MAX_TEXTURE_3D_LEVELS 9   /* 512 x 512 x 512 */
+#define SP_MAX_TEXTURE_3D_LEVELS 9   /* 256 x 256 x 256 */
 #define SP_MAX_TEXTURE_CUBE_LEVELS 13  /* 4K x 4K */
 
 
