7b4aefd3c9c1b04832306b8114b3f2007318f087
Alex Deucher <alexander.deucher@amd.com>
2012-09-20 11:16:36 -0400

    r600g/llvm: rs780/rs880 are r600 asics
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- src/gallium/drivers/r600/r600_llvm.c
+++ src/gallium/drivers/r600/r600_llvm.c
@@ -259,10 +259,10 @@ const char * r600_llvm_gpu_string(enum radeon_family family)
 	case CHIP_RV630:
 	case CHIP_RV620:
 	case CHIP_RV635:
-		gpu_family = "r600";
-		break;
 	case CHIP_RS780:
 	case CHIP_RS880:
+		gpu_family = "r600";
+		break;
 	case CHIP_RV710:
 		gpu_family = "rv710";
 		break;
