b2991526ed6faa713e3cf640fcdbbad68d991f3e
Andreas Boll <andreas.boll.dev@gmail.com>
2012-10-19 23:00:17 +0200

    mesa: optimize get-pick-list.sh script
    cuts down the while loop iterations from 4600 to 380 commits at the
    moment
    
    NOTE: This is a candidate for the stable branches.
    
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- bin/get-pick-list.sh
+++ bin/get-pick-list.sh
@@ -2,11 +2,11 @@
 
 # Script for generating a list of candidates for cherry-picking to a stable branch
 
-git log --reverse --pretty=%H HEAD..origin/master |\
+# Grep for commits that were marked as a candidate for the stable tree.
+git log --reverse --pretty=%H -i --grep='^[[:space:]]*NOTE: This is a candidate' HEAD..origin/master |\
 while read sha
 do
-    # Check to see whether the patch was marked as a candidate for the stable tree.
-    if git log -n1 $sha | grep -iq '^[[:space:]]*NOTE: This is a candidate' ; then
+    # Check to see whether the patch is on the ignore list.
 	if [ -f .git/cherry-ignore ] ; then
 	    if grep -q ^$sha .git/cherry-ignore ; then
 		continue
@@ -19,5 +19,4 @@ do
 	fi
 
 	git log -n1 --pretty=oneline $sha | cat
-    fi
 done
