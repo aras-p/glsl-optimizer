135ec3a1db0de50e2bb3c57d4bf76f87ac80320f
Andreas Boll <andreas.boll.dev@gmail.com>
2012-10-19 23:13:12 +0200

    mesa: grep for commits with cherry picked in commit message only once
    and save them temporary in already_picked
    
    NOTE: This is a candidate for the stable branches.
    
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- bin/get-pick-list.sh
+++ bin/get-pick-list.sh
@@ -2,6 +2,11 @@
 
 # Script for generating a list of candidates for cherry-picking to a stable branch
 
+# Grep for commits with "cherry picked from commit" in the commit message.
+git log --reverse --grep="cherry picked from commit" origin/master..HEAD |\
+	grep "cherry picked from commit" |\
+	sed -e 's/^[[:space:]]*(cherry picked from commit[[:space:]]*//' -e 's/)//' > already_picked
+
 # Grep for commits that were marked as a candidate for the stable tree.
 git log --reverse --pretty=%H -i --grep='^[[:space:]]*NOTE: This is a candidate' HEAD..origin/master |\
 while read sha
@@ -14,9 +19,11 @@ do
 	fi
 
 	# Check to see if it has already been picked over.
-	if git log origin/master..HEAD | grep -q "cherry picked from commit $sha"; then
-	    continue
+	if grep -q ^$sha already_picked ; then
+		continue
 	fi
 
 	git log -n1 --pretty=oneline $sha | cat
 done
+
+rm -f already_picked
