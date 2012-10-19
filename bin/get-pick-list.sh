2d95db660e20787bcfe6af55569299515b434971
Ian Romanick <ian.d.romanick@intel.com>
2012-10-19 22:30:53 +0200

    mesa: add get-pick-list.sh script into bin/
    NOTE: This is a candidate for the stable branches.

---
+++ bin/get-pick-list.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git log --reverse --pretty=oneline HEAD..origin/master | cut -d' ' -f1 |\
+while read sha
+do
+    # Check to see whether the patch was marked as a candidate for the stable tree.
+    if git log -n1 $sha | grep -iq '^[[:space:]]*NOTE: This is a candidate' ; then
+	if [ -f .git/cherry-ignore ] ; then
+	    if grep -q ^$sha .git/cherry-ignore ; then
+		continue
+	    fi
+	fi
+
+	# Check to see if it has already been picked over.
+	if git log origin/master..HEAD | grep -q "cherry picked from commit $sha"; then
+	    continue
+	fi
+
+	git log -n1 --pretty=oneline $sha | cat
+    fi
+done
