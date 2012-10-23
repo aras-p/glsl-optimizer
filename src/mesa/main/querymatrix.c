5a1b40acf50955d727b15dc70703a19115bb3552
Paul Berry <stereotype441@gmail.com>
2012-10-23 14:31:27 -0700

    dispatch: Make a header to go along with querymatrix.c.
    This patch creates a header querymatrix.h, to allow functions defined
    in querymatrix.c to be used from other .c files.  It also switches
    from the nonstandard GL_APIENTRY to GLAPIENTRY.
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    
    v2: Don't declare _mesa_Get{Integer,Float}v in querymatrix.c.
    Instead, just include main/get.h.
    
    Reviewed-by: Chad Versace <chad.versace@linux.intel.com>

--- src/mesa/main/querymatrix.c
+++ src/mesa/main/querymatrix.c
@@ -14,8 +14,9 @@
 
 #include <stdlib.h>
 #include <math.h>
-#include "GLES/gl.h"
-#include "GLES/glext.h"
+#include "glheader.h"
+#include "querymatrix.h"
+#include "main/get.h"
 
 
 /**
@@ -88,13 +89,7 @@ fpclassify(double x)
 
 #endif
 
-extern GLbitfield GL_APIENTRY _es_QueryMatrixxOES(GLfixed mantissa[16], GLint exponent[16]);
-
-/* The Mesa functions we'll need */
-extern void GL_APIENTRY _mesa_GetIntegerv(GLenum pname, GLint *params);
-extern void GL_APIENTRY _mesa_GetFloatv(GLenum pname, GLfloat *params);
-
-GLbitfield GL_APIENTRY _es_QueryMatrixxOES(GLfixed mantissa[16], GLint exponent[16])
+GLbitfield GLAPIENTRY _es_QueryMatrixxOES(GLfixed mantissa[16], GLint exponent[16])
 {
     GLfloat matrix[16];
     GLint tmp;
