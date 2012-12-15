32f322925592e9eeda6a5624c7320232fc170c03
Ian Romanick <ian.d.romanick@intel.com>
2012-12-15 14:49:53 -0800

    glsl: Allow elimination of uniform block members
    glGetActiveUniform is not supposed to report block members that are not
    active even if they are included in the layout of the block.  The block
    layout is determined from the GLSL_TYPE_INTERFACE that defines the
    block, so eliminating the ir_variables that correspond to the individual
    fields is safe.
    
    Fixes gles3conform test
    uniform_buffer_object_getuniformindices_for_for_nonexistent_or_not_active_uniform_names.
    
    This also fixes the assertion failures (added in the previous commit) in
    gles3conform uniform_buffer_object_index_of_not_active_block,
    uniform_buffer_object_inherit_and_override_layouts, and
    uniform_buffer_object_repeat_global_scope_layouts.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/glsl/opt_dead_code.cpp
+++ src/glsl/opt_dead_code.cpp
@@ -98,15 +98,10 @@ do_dead_code(exec_list *instructions, bool uniform_locations_assigned)
 	 /* uniform initializers are precious, and could get used by another
 	  * stage.  Also, once uniform locations have been assigned, the
 	  * declaration cannot be deleted.
-	  *
-	  * Also, GL_ARB_uniform_buffer_object says that std140
-	  * uniforms will not be eliminated.  Since we always do
-	  * std140, just don't eliminate uniforms in UBOs.
 	  */
 	 if (entry->var->mode == ir_var_uniform &&
 	     (uniform_locations_assigned ||
-	      entry->var->constant_value ||
-	      entry->var->is_in_uniform_block()))
+	      entry->var->constant_value))
 	    continue;
 
 	 entry->var->remove();
