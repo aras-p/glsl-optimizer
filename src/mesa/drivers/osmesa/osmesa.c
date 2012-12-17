aras-p/glsl-optimizer

commit 4bea4cb9fd55cdb267003a6e6e16f7e903e00940

drivers: compute version and then initialize exec table
This change forces the context version to be computed before
initilizing the exec dispatch tables.

Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/mesa/drivers/osmesa/osmesa.c
@@ -35,6 +35,7 @@
 
 #include "main/glheader.h"
 #include "GL/osmesa.h"
+#include "main/api_exec.h"
 #include "main/context.h"
 #include "main/extensions.h"
 #include "main/formats.h"
@@ -43,6 +44,8 @@
 #include "main/macros.h"
 #include "main/mtypes.h"
 #include "main/renderbuffer.h"
+#include "main/version.h"
+#include "main/vtxfmt.h"
 #include "swrast/swrast.h"
 #include "swrast_setup/swrast_setup.h"
 #include "swrast/s_context.h"
@@ -787,6 +790,12 @@ OSMesaCreateContextExt( GLenum format, GLint depthBits, GLint stencilBits,
          swrast->choose_line = osmesa_choose_line;
          swrast->choose_triangle = osmesa_choose_triangle;
       }
+
+      _mesa_compute_version(ctx);
+
+      /* Exec table initialization requires the version to be computed */
+      _mesa_initialize_exec_table(ctx);
+      _mesa_initialize_vbo_vtxfmt(ctx);
    }
    return osmesa;
 }

src/mesa/drivers/x11/xm_api.c
@@ -71,6 +71,7 @@
 #include "main/macros.h"
 #include "main/renderbuffer.h"
 #include "main/teximage.h"
+#include "main/vtxfmt.h"
 #include "glapi/glthread.h"
 #include "swrast/swrast.h"
 #include "swrast/s_renderbuffer.h"
@@ -957,6 +958,12 @@ XMesaContext XMesaCreateContext( XMesaVisual v, XMesaContext share_list )
    _swsetup_Wakeup(mesaCtx);
 
    _mesa_meta_init(mesaCtx);
+
+   _mesa_compute_version(ctx);
+
+    /* Exec table initialization requires the version to be computed */
+   _mesa_initialize_exec_table(ctx);
+   _mesa_initialize_vbo_vtxfmt(ctx);
 
    return c;
 }

src/mesa/drivers/dri/i915/i830_context.c
@@ -26,7 +26,10 @@
  **************************************************************************/
 
 #include "i830_context.h"
+#include "main/api_exec.h"
 #include "main/imports.h"
+#include "main/version.h"
+#include "main/vtxfmt.h"
 #include "tnl/tnl.h"
 #include "tnl/t_vertex.h"
 #include "tnl/t_context.h"
@@ -110,6 +113,11 @@ i830CreateContext(const struct gl_config * mesaVis,
 
    _tnl_allow_vertex_fog(ctx, 1);
    _tnl_allow_pixel_fog(ctx, 0);
+
+   _mesa_compute_version(ctx);
+
+   _mesa_initialize_exec_table(ctx);
+   _mesa_initialize_vbo_vtxfmt(ctx);
 
    return true;
 }

src/mesa/drivers/dri/i915/i915_context.c
@@ -26,8 +26,11 @@
  **************************************************************************/
 
 #include "i915_context.h"
+#include "main/api_exec.h"
 #include "main/imports.h"
 #include "main/macros.h"
+#include "main/version.h"
+#include "main/vtxfmt.h"
 #include "intel_tris.h"
 #include "tnl/t_context.h"
 #include "tnl/t_pipeline.h"
@@ -294,6 +297,11 @@ i915CreateContext(int api,
     */
    _tnl_allow_vertex_fog(ctx, 0);
    _tnl_allow_pixel_fog(ctx, 1);
+
+   _mesa_compute_version(ctx);
+
+   _mesa_initialize_exec_table(ctx);
+   _mesa_initialize_vbo_vtxfmt(ctx);
 
    return true;
 }

src/mesa/drivers/dri/i965/brw_context.c
@@ -30,9 +30,12 @@
   */
 
 
+#include "main/api_exec.h"
 #include "main/imports.h"
 #include "main/macros.h"
 #include "main/simple_list.h"
+#include "main/version.h"
+#include "main/vtxfmt.h"
 
 #include "vbo/vbo_context.h"
 
@@ -385,6 +388,11 @@ brwCreateContext(int api,
 
    if (INTEL_DEBUG & DEBUG_SHADER_TIME)
       brw_init_shader_time(brw);
+
+   _mesa_compute_version(ctx);
+
+   _mesa_initialize_exec_table(ctx);
+   _mesa_initialize_vbo_vtxfmt(ctx);
 
    return true;
 }

src/mesa/drivers/dri/nouveau/nouveau_context.c
@@ -31,12 +31,14 @@
 #include "nouveau_fbo.h"
 #include "nv_object.xml.h"
 
+#include "main/api_exec.h"
 #include "main/dd.h"
 #include "main/framebuffer.h"
 #include "main/fbobject.h"
 #include "main/light.h"
 #include "main/state.h"
 #include "main/version.h"
+#include "main/vtxfmt.h"
 #include "drivers/common/meta.h"
 #include "drivers/common/driverfuncs.h"
 #include "swrast/swrast.h"
@@ -102,6 +104,10 @@ nouveau_context_create(gl_api api,
 	   *error = __DRI_CTX_ERROR_BAD_VERSION;
 	   return GL_FALSE;
 	}
+
+	/* Exec table initialization requires the version to be computed */
+	_mesa_initialize_exec_table(ctx);
+	_mesa_initialize_vbo_vtxfmt(ctx);
 
 	if (nouveau_bo_new(context_dev(ctx), NOUVEAU_BO_VRAM, 0, 4096,
 			   NULL, &nctx->fence)) {

src/mesa/drivers/dri/r200/r200_context.c
@@ -35,12 +35,14 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdbool.h>
 #include "main/glheader.h"
 #include "main/api_arrayelt.h"
+#include "main/api_exec.h"
 #include "main/context.h"
 #include "main/simple_list.h"
 #include "main/imports.h"
 #include "main/extensions.h"
 #include "main/mfeatures.h"
 #include "main/version.h"
+#include "main/vtxfmt.h"
 
 #include "swrast/swrast.h"
 #include "swrast_setup/swrast_setup.h"
@@ -458,6 +460,10 @@ GLboolean r200CreateContext( gl_api api,
    }
 
    _mesa_compute_version(ctx);
+
+   /* Exec table initialization requires the version to be computed */
+   _mesa_initialize_exec_table(ctx);
+   _mesa_initialize_vbo_vtxfmt(ctx);
 
    *error = __DRI_CTX_ERROR_SUCCESS;
    return GL_TRUE;

src/mesa/drivers/dri/radeon/radeon_context.c
@@ -37,12 +37,14 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdbool.h>
 #include "main/glheader.h"
 #include "main/api_arrayelt.h"
+#include "main/api_exec.h"
 #include "main/context.h"
 #include "main/simple_list.h"
 #include "main/imports.h"
 #include "main/extensions.h"
 #include "main/mfeatures.h"
 #include "main/version.h"
+#include "main/vtxfmt.h"
 
 #include "swrast/swrast.h"
 #include "swrast_setup/swrast_setup.h"
@@ -411,6 +413,10 @@ r100CreateContext( gl_api api,
    }
 
    _mesa_compute_version(ctx);
+
+   /* Exec table initialization requires the version to be computed */
+   _mesa_initialize_exec_table(ctx);
+   _mesa_initialize_vbo_vtxfmt(ctx);
 
    *error = __DRI_CTX_ERROR_SUCCESS;
    return GL_TRUE;

commit 6cf3034ba72bff55841b1e3338fbc541cd04efdc

mesa osmesa/x11: fix build error introduced in 4bea4cb9
Fixes https://bugs.freedesktop.org/show_bug.cgi?id=58380

Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Brian Paul <brianp@vmware.com>

src/mesa/drivers/osmesa/osmesa.c
@@ -789,13 +789,13 @@ OSMesaCreateContextExt( GLenum format, GLint depthBits, GLint stencilBits,
          swrast = SWRAST_CONTEXT( ctx );
          swrast->choose_line = osmesa_choose_line;
          swrast->choose_triangle = osmesa_choose_triangle;
+
+         _mesa_compute_version(ctx);
+
+         /* Exec table initialization requires the version to be computed */
+         _mesa_initialize_exec_table(ctx);
+         _mesa_initialize_vbo_vtxfmt(ctx);
       }
-
-      _mesa_compute_version(ctx);
-
-      /* Exec table initialization requires the version to be computed */
-      _mesa_initialize_exec_table(ctx);
-      _mesa_initialize_vbo_vtxfmt(ctx);
    }
    return osmesa;
 }

src/mesa/drivers/x11/xm_api.c
@@ -959,11 +959,11 @@ XMesaContext XMesaCreateContext( XMesaVisual v, XMesaContext share_list )
 
    _mesa_meta_init(mesaCtx);
 
-   _mesa_compute_version(ctx);
+   _mesa_compute_version(mesaCtx);
 
     /* Exec table initialization requires the version to be computed */
-   _mesa_initialize_exec_table(ctx);
-   _mesa_initialize_vbo_vtxfmt(ctx);
+   _mesa_initialize_exec_table(mesaCtx);
+   _mesa_initialize_vbo_vtxfmt(mesaCtx);
 
    return c;
 }