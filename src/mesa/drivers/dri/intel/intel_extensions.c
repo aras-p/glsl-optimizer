1d4dbeeaec8490a14dafefa9c647ae48b56ab952
Chris Forbes <chrisf@ijw.co.nz>
2012-12-29 21:28:57 +1300

    i965: enable ARB_texture_multisample on Gen6+
    V2: Works on Ivy Bridge now too, so this can be 6+.
    
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>
    Reviewed-by: Paul Berry <stereotype441@gmail.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/mesa/drivers/dri/intel/intel_extensions.c
+++ src/mesa/drivers/dri/intel/intel_extensions.c
@@ -107,6 +107,7 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.ARB_texture_cube_map_array = true;
       ctx->Extensions.OES_depth_texture_cube_map = true;
       ctx->Extensions.ARB_shading_language_packing = true;
+      ctx->Extensions.ARB_texture_multisample = true;
    }
 
    if (intel->gen >= 5)
