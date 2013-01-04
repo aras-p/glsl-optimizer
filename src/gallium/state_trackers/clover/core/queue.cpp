aras-p/glsl-optimizer

commit 598cc1f74d7ae924e84dee801b456ab7b0b22f84

gallium: extend pipe_context::flush for it to accept an END_OF_FRAME flag
Usage with pipe_context:
  pipe->flush(pipe, NULL, PIPE_FLUSH_END_OF_FRAME);

Usage with st_context_iface:
  st->flush(st, ST_FLUSH_END_OF_FRAME, NULL);

The flag is only a hint for drivers. Radeon will use it for buffer eviction
heuristics in the kernel (e.g. for queries like how many frames have passed
since a buffer was used).

The flag is currently only generated by st/dri on SwapBuffers.

Reviewed-by: Brian Paul <brianp@vmware.com>
Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

src/gallium/state_trackers/clover/core/queue.cpp
@@ -53,7 +53,7 @@ _cl_command_queue::flush() {
                                [](event_ptr &ev) { return !ev->signalled(); });
 
       // Flush and fence them.
-      pipe->flush(pipe, &fence);
+      pipe->flush(pipe, &fence, 0);
       std::for_each(first, last, [&](event_ptr &ev) { ev->fence(fence); });
       screen->fence_reference(screen, &fence, NULL);
       queued_events.erase(first, last);

commit 54f3a3e88d35c855c43c31ae7c915cfa55d1f5d7

clover: Fix build after the addition of enum pipe_flush_flags
Broken since commit 598cc1f74d7ae924e84dee801b456ab7b0b22f84

src/gallium/state_trackers/clover/core/queue.cpp
@@ -53,7 +53,7 @@ _cl_command_queue::flush() {
                                [](event_ptr &ev) { return !ev->signalled(); });
 
       // Flush and fence them.
-      pipe->flush(pipe, &fence, 0);
+      pipe->flush(pipe, &fence, (enum pipe_flush_flags)0);
       std::for_each(first, last, [&](event_ptr &ev) { ev->fence(fence); });
       screen->fence_reference(screen, &fence, NULL);
       queued_events.erase(first, last);