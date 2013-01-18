285fe32bd9232009279c4df87d2e084f4059c67e
Ian Romanick <ian.d.romanick@intel.com>
2013-01-18 15:44:38 -0800

    intel: Enable GL_OES_depth_texture_cube_map
    For now I'm just enabling this on the same subset of hardware that has
    OpenGL 3.0 enabled.  This same functionality is part of OpenGL 3.0, and
    there is no matching desktop extension.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/intel/intel_extensions.c
+++ src/mesa/drivers/dri/intel/intel_extensions.c
@@ -106,6 +106,7 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.ARB_texture_buffer_object = true;
       ctx->Extensions.ARB_texture_buffer_object_rgb32 = true;
       ctx->Extensions.ARB_texture_cube_map_array = true;
+      ctx->Extensions.OES_depth_texture_cube_map = true;
    }
 
    if (intel->gen >= 5)
