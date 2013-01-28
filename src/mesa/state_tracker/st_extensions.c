c80bacba2ed88bccc92ab7a7151e14ad32729249
Brian Paul <brianp@vmware.com>
2013-01-28 11:32:13 -0700

    st/mesa: only enable GL_EXT_framebuffer_multisample if GL_MAX_SAMPLES >= 2
    We never really have multisampling with one sample per pixel.
    See also http://bugs.freedesktop.org/show_bug.cgi?id=59873
    
    Note: This is a candidate for the 9.0 branch.
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -534,7 +534,6 @@ void st_init_extensions(struct st_context *st)
    ctx->Extensions.EXT_blend_minmax = GL_TRUE;
    ctx->Extensions.EXT_framebuffer_blit = GL_TRUE;
    ctx->Extensions.EXT_framebuffer_object = GL_TRUE;
-   ctx->Extensions.EXT_framebuffer_multisample = GL_TRUE;
    ctx->Extensions.EXT_fog_coord = GL_TRUE;
    ctx->Extensions.EXT_gpu_program_parameters = GL_TRUE;
    ctx->Extensions.EXT_pixel_buffer_object = GL_TRUE;
@@ -656,6 +655,9 @@ void st_init_extensions(struct st_context *st)
    if (ctx->Const.MaxSamples == 1) {
       /* one sample doesn't really make sense */
       ctx->Const.MaxSamples = 0;
+   }
+   else if (ctx->Const.MaxSamples >= 2) {
+      ctx->Extensions.EXT_framebuffer_multisample = GL_TRUE;
    }
 
    if (ctx->Const.MaxDualSourceDrawBuffers > 0)
