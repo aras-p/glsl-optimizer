f29ab4ece5b6a0321f5e4a6fafb7ecacf7214044
Ian Romanick <ian.d.romanick@intel.com>
2013-02-07 19:21:36 -0800

    i965: Set UniformBufferOffsetAlignment to sizeof(vec4)
    This matches the behavior of the Windows driver, but a bspec reference
    should would be nice.
    
    NOTE: This is a candidate for the 9.0 and 9.1 branches.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -376,6 +376,7 @@ brwCreateContext(int api,
 
    ctx->Const.NativeIntegers = true;
    ctx->Const.UniformBooleanTrue = 1;
+   ctx->Const.UniformBufferOffsetAlignment = 16;
 
    ctx->Const.ForceGLSLExtensionsWarn = driQueryOptionb(&intel->optionCache, "force_glsl_extensions_warn");
 
