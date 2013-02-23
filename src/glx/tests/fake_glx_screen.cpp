8c5330226f391a7a29b6538851090b0ef730a239
Ian Romanick <ian.d.romanick@intel.com>
2013-02-22 16:45:27 -0800

    glx/tests: Add unit tests for the DRI2 part of GLX_MESA_query_renderer
    After adding $(DEFINES) to AM_CPPFLAGS, the __glXGetCurrentContext
    wrapper function is no longer needed and causes compile errors.  Using
    the correct defines causes it to be a macro!
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/glx/tests/fake_glx_screen.cpp
+++ src/glx/tests/fake_glx_screen.cpp
@@ -76,9 +76,3 @@ indirect_create_context_attribs(struct glx_screen *base,
 }
 
 __thread void *__glX_tls_Context = NULL;
-
-extern "C" struct glx_context *
-__glXGetCurrentContext()
-{
-   return (struct glx_context *) __glX_tls_Context;
-}
