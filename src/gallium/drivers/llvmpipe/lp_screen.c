4c12276607e6a95a9a0c5997f018605e86aa5843
Roland Scheidegger <sroland@vmware.com>
2013-03-01 14:50:32 +0100

    llvmpipe: bump glsl version to 140
    texel offsets should have been the last missing feature for 130, and in
    fact 140 as well (last there were texture buffers). In any case we still
    don't do OpenGL 3.0 (missing MSAA which will be difficult,
    plus EXT_packed_float, ARB_depth_buffer_float and EXT_framebuffer_sRGB).
    
    v2: bump to 140 instead - we have everything except we crash when not writing
    to gl_Position (but softpipe crashes as well) so let's just say this is a bug
    instead. Also (by Dave Airlie's suggestion) update llvm-todo.txt.
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -195,7 +195,7 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_VERTEX_COLOR_CLAMPED:
       return 1;
    case PIPE_CAP_GLSL_FEATURE_LEVEL:
-      return 120;
+      return 140;
    case PIPE_CAP_QUADS_FOLLOW_PROVOKING_VERTEX_CONVENTION:
       return 0;
    case PIPE_CAP_COMPUTE:
