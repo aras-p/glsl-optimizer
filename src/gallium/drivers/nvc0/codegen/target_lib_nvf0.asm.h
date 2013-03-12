tizbac/Mesa-3D

nvc0/ir: add formatted surface load lib code, move to extra header
OpenGL is nice and makes the user specify a format with an image unit.
OpenCL is evil and doesn't, and what's better than adding a huge load
of functions that we call indirectly to handle the conversion ?

src/gallium/drivers/nvc0/codegen/target_lib_nvf0.asm.h
@@ -1,0 +1,13 @@
+
+static const uint32_t nvf0_builtin_code[] =
+{
+   0x19000000001c003cULL,
+};
+
+static const uint16_t nvf0_builtin_offsets[NVC0_BUILTIN_COUNT] =
+{
+   0,
+   0,
+   0,
+   0
+};

aras-p/glsl-optimizer

src/gallium/drivers/nvc0/codegen/target_lib_nvf0.asm.h
@@ -1,0 +1,13 @@
+
+static const uint64_t nvf0_builtin_code[] =
+{
+   0x19000000001c003cULL,
+};
+
+static const uint16_t nvf0_builtin_offsets[NVC0_BUILTIN_COUNT] =
+{
+   0,
+   0,
+   0,
+   0
+};