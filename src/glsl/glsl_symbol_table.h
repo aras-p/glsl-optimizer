3919c19468e71fd9590b53e6160db79afb55bfc8
Jordan Justen <jordan.l.justen@intel.com>
2013-03-21 09:57:20 -0700

    glsl_symbol_table: add interface block namespaces
    For interface blocks, there are three separate namespaces for
    uniform, input and output blocks.
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/glsl/glsl_symbol_table.h
+++ src/glsl/glsl_symbol_table.h
@@ -99,6 +99,8 @@ public:
    bool add_variable(ir_variable *v);
    bool add_type(const char *name, const glsl_type *t);
    bool add_function(ir_function *f);
+   bool add_interface(const char *name, const glsl_type *i,
+                      enum ir_variable_mode mode);
    /*@}*/
 
    /**
@@ -113,6 +115,8 @@ public:
    ir_variable *get_variable(const char *name);
    const glsl_type *get_type(const char *name);
    ir_function *get_function(const char *name);
+   const glsl_type *get_interface(const char *name,
+                                  enum ir_variable_mode mode);
    /*@}*/
 
 private:
