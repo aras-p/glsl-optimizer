980f1cf8a165ad25fbb6c0424e6166a4210c3196
Rob Clark <robdclark@gmail.com>
2013-03-25 11:55:18 -0400

    freedreno: prefer sw upload for textures
    Since we are UMA, in most cases the GPU blit doesn't make much sense for
    texture upload.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- src/gallium/drivers/freedreno/freedreno_screen.c
+++ src/gallium/drivers/freedreno/freedreno_screen.c
@@ -165,7 +165,9 @@ fd_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
 	case PIPE_CAP_TEXTURE_MULTISAMPLE:
 	case PIPE_CAP_USER_CONSTANT_BUFFERS:
 		return 1;
+
 	case PIPE_CAP_TGSI_TEXCOORD:
+	case PIPE_CAP_PREFER_BLIT_BASED_TEXTURE_TRANSFER:
 		return 0;
 
 	case PIPE_CAP_CONSTANT_BUFFER_OFFSET_ALIGNMENT:
