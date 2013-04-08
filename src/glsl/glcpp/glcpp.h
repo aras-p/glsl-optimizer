aras-p/glsl-optimizer

commit e3ba06e22f65043dc7824360b8502cce4c5a169d

Fixed strtoll conversion warning.

src/glsl/glcpp/glcpp.h
@@ -50,7 +50,7 @@ typedef struct token_list token_list_t;
 
 typedef union YYSTYPE
 {
-	int64_t ival;
+	int ival;
 	char *str;
 	string_list_t *string_list;
 	token_t *token;

src/glsl/glcpp/glcpp-parse.c
@@ -2146,11 +2146,11 @@ yyreduce:
 #line 361 "src/glsl/glcpp/glcpp-parse.y"
     {
 		if (strlen ((yyvsp[(1) - (1)].str)) >= 3 && strncmp ((yyvsp[(1) - (1)].str), "0x", 2) == 0) {
-			(yyval.ival) = strtoll ((yyvsp[(1) - (1)].str) + 2, NULL, 16);
+			(yyval.ival) = (int)strtoll ((yyvsp[(1) - (1)].str) + 2, NULL, 16);
 		} else if ((yyvsp[(1) - (1)].str)[0] == '0') {
-			(yyval.ival) = strtoll ((yyvsp[(1) - (1)].str), NULL, 8);
+			(yyval.ival) = (int)strtoll ((yyvsp[(1) - (1)].str), NULL, 8);
 		} else {
-			(yyval.ival) = strtoll ((yyvsp[(1) - (1)].str), NULL, 10);
+			(yyval.ival) = (int)strtoll ((yyvsp[(1) - (1)].str), NULL, 10);
 		}
 	}
     break;

src/glsl/glcpp/glcpp-parse.y
@@ -360,11 +360,11 @@ control_line:
 integer_constant:
 	INTEGER_STRING {
 		if (strlen ($1) >= 3 && strncmp ($1, "0x", 2) == 0) {
-			$$ = strtoll ($1 + 2, NULL, 16);
+			$$ = (int)strtoll ($1 + 2, NULL, 16);
 		} else if ($1[0] == '0') {
-			$$ = strtoll ($1, NULL, 8);
+			$$ = (int)strtoll ($1, NULL, 8);
 		} else {
-			$$ = strtoll ($1, NULL, 10);
+			$$ = (int)strtoll ($1, NULL, 10);
 		}
 	}
 |	INTEGER {

commit 92ba5e9dd037363296a953f100ce9e6199938144

fixed #version parse regression on Mac since e3ba06e

src/glsl/glcpp/glcpp.h
@@ -50,7 +50,9 @@ typedef struct token_list token_list_t;
 
 typedef union YYSTYPE
 {
-	int ival;
+	// Could be int, but results in some bugs with parsing of #version directives
+	// in Apple LLVM Compiler 4.2 when building for 32 bit.
+	intmax_t ival;
 	char *str;
 	string_list_t *string_list;
 	token_t *token;