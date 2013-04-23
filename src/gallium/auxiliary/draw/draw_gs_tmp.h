1a87473998b4af2dbe37aa3ce0c004e62d3fe2c0
Zack Rusin <zackr@vmware.com>
2013-04-22 20:44:21 -0400

    draw/gs: preserve leading vertex info for gs
    We need to handle the leading vertex information when
    assembling primitives for the geometry shader otherwise
    the resulting triangles will have vertices at incorrect
    input locations.
    
    Signed-off-by: Zack Rusin <zackr@vmware.com>
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/gallium/auxiliary/draw/draw_gs_tmp.h
+++ src/gallium/auxiliary/draw/draw_gs_tmp.h
@@ -10,7 +10,7 @@
    const unsigned prim_flags = input_prims->flags;                \
    const unsigned count = input_prims->count;                     \
    const boolean quads_flatshade_last = FALSE;                    \
-   const boolean last_vertex_last = TRUE;                         \
+   const boolean last_vertex_last = !gs->draw->rasterizer->flatshade_first;  \
    do {                                                           \
       debug_assert(input_prims->primitive_count == 1);            \
       switch (prim) {                                             \
