71682c1599c3fe0096cccfdbd8eb762cd909fbd8
Brian Paul <brianp@vmware.com>
2013-05-29 18:07:11 -0600

    svga: add PIPE_CAP_MAX_VIEWPORTS to switch to silence warning

--- src/gallium/drivers/svga/svga_screen.c
+++ src/gallium/drivers/svga/svga_screen.c
@@ -269,6 +269,8 @@ svga_get_param(struct pipe_screen *screen, enum pipe_cap param)
       return 0;
    case PIPE_CAP_VERTEX_ELEMENT_SRC_OFFSET_4BYTE_ALIGNED_ONLY:
       return 1;
+   case PIPE_CAP_MAX_VIEWPORTS:
+      return 1;
    }
 
    debug_printf("Unexpected PIPE_CAP_ query %u\n", param);
