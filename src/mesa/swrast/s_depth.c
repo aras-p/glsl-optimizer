fceff14450e48a90778e0d1e79c13fa7a40631e6
Eric Anholt <eric@anholt.net>
2013-06-05 23:19:27 -0700

    mesa: Add a _mesa_problem to document a piglit failure on i965.
    Having figured out what was going on with piglit fbo-depth copypixels
    GL_DEPTH_COMPONENT32F (falling all the way back to swrast on CopyPixels to
    a float depth buffer), I'm not inclined to fix the problem currently but
    it seems worth saving someone else the debug time.
    
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- src/mesa/swrast/s_depth.c
+++ src/mesa/swrast/s_depth.c
@@ -310,6 +310,12 @@ _swrast_depth_test_span(struct gl_context *ctx, SWspan *span)
       zBufferVals = zStart;
    }
    else {
+      if (_mesa_get_format_datatype(rb->Format) != GL_UNSIGNED_NORMALIZED) {
+         _mesa_problem(ctx, "Incorrectly writing swrast's integer depth "
+                       "values to %s depth buffer",
+                       _mesa_get_format_name(rb->Format));
+      }
+
       /* copy Z buffer values into temp buffer (32-bit Z values) */
       zBufferTemp = malloc(count * sizeof(GLuint));
       if (!zBufferTemp)
