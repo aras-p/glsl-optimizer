aras-p/glsl-optimizer

commit 1f82bf12edb37a91991fdae59cf0cc36c9f49278

Remove OpenVMS support
Not maintained since 2008. Doubtful that it's worked in quite a while.

Also see commit 32ac8cb05 which removed VMS stuff from Makefile in 2009.

Cc: Jouk Jansen <j.jansen@tudelft.nl>
Reviewed-by: Brian Paul <brianp@vmware.com>
Signed-off-by: Andreas Boll <andreas.boll.dev@gmail.com>

src/mesa/main/compiler.h
@@ -183,9 +183,7 @@ extern "C" {
  * Don't define it if using a newer Windows compiler.
  */
 #ifndef __FUNCTION__
-# if defined(__VMS)
-#  define __FUNCTION__ "VMS$NL:"
-# elif !defined(__GNUC__) && !defined(__xlC__) &&	\
+# if !defined(__GNUC__) && !defined(__xlC__) &&	\
       (!defined(_MSC_VER) || _MSC_VER < 1300)
 #  if (__STDC_VERSION__ >= 199901L) /* C99 */ || \
     (defined(__SUNPRO_C) && defined(__C99FEATURES__))
@@ -363,7 +361,7 @@ static INLINE GLuint CPU_TO_LE32(GLuint x)
     defined(__arm__) || \
     defined(__sh__) || defined(__m32r__) || \
     (defined(__sun) && defined(_IEEE_754)) || \
-    (defined(__alpha__) && (defined(__IEEE_FLOAT) || !defined(VMS)))
+    (defined(__alpha__) && defined(__IEEE_FLOAT))
 #define USE_IEEE
 #define IEEE_ONE 0x3f800000
 #endif

docs/README.VMS
@@ -1,38 +1,0 @@
-
-VMS support contributed by Jouk Jansen (joukj@hrem.stm.tudelft.nl)
-
-
-The latest version was tested on a VMSAlpha7.2 system using DECC6.0, but
-probably also works for other versions.
-
-At the moment only the libraries LIBMESGL.EXE/LIBMESGL.OLB,
-LIBMESAGLU.EXE/LIBMESAGLU.OLB and LIBGLUT.EXE/LIBGLUT.OLB and the demos of the
-directory [.DEMOS] can be build.
-However, feel free to create the missing "decrip.mms-files" in the other
-directories.
-
- The make files were tested
-using the DIGITAL make utility called MMS.  There is also a public domain
-clone available (MMK) and I  think, but it is not tested, that this
-utility will give (hardly) any problem.
-
-To make everything just type MMS (or MMK) in the main directory of
-mesagl.  For MMS the deafult makefile is called descrip.mms, and
-that is what I have called it.  I included alse some config files,
-all having mms somewhere in the name which all the makefiles need
-(just as your unix makefiles).
-
-On Alpha platforms at default a sharable images for the libraries are created.
-To get a static library make it by typing MMS/MACRO=(NOSHARE=1).
-On VAX platforms only static libraries can be build.
-
-23-sep-2005
-changed default compilation to use /float=ieee/ieee=denorm. The reason for 
-this is that it makes Mesa on OpenVMS better compatible with other platforms
-and other packages for VMS that I maintain.
-For more information see
-      http://nchrem.tnw.tudelft.nl/openvms
-      https://bugs.freedesktop.org/show_bug.cgi?id=4270
-You may want to compile Mesa to use VAX-floating point arithmetic, instead
-of IEEE floating point by removing the /float=IEEE/denorm flag from the
-compiler options in the descrip.mms files.

include/GL/glx.h
@@ -27,21 +27,8 @@
 #define GLX_H
 
 
-#ifdef __VMS
-#include <GL/vms_x_fix.h>
-# ifdef __cplusplus
-/* VMS Xlib.h gives problems with C++.
- * this avoids a bunch of trivial warnings */
-#pragma message disable nosimpint
-#endif
-#endif
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#ifdef __VMS
-# ifdef __cplusplus
-#pragma message enable nosimpint
-#endif
-#endif
 #include <GL/gl.h>
 
 

src/egl/main/eglcompiler.h
@@ -110,9 +110,7 @@
  * Don't define it if using a newer Windows compiler.
  */
 #ifndef __FUNCTION__
-# if defined(__VMS)
-#  define __FUNCTION__ "VMS$NL:"
-# elif (!defined __GNUC__) && (!defined __xlC__) && \
+# if (!defined __GNUC__) && (!defined __xlC__) && \
       (!defined(_MSC_VER) || _MSC_VER < 1300)
 #  if (__STDC_VERSION__ >= 199901L) /* C99 */ || \
     (defined(__SUNPRO_C) && defined(__C99FEATURES__))

src/mesa/descrip.mms
@@ -1,27 +1,0 @@
-# Makefile for Mesa for VMS
-# contributed by Jouk Jansen  joukj@hrem.nano.tudelft.nl
-# Date last revision : 7 March 2007
-
-all :
-	set default [.main]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.glapi]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.shader]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.drivers.common]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.x11]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.osmesa]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [--.math]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.tnl]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.swrast]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.swrast_setup]
-	$(MMS)$(MMSQUALIFIERS)
-	set default [-.vbo]
-	$(MMS)$(MMSQUALIFIERS)

src/mesa/drivers/common/descrip.mms
@@ -1,42 +1,0 @@
-# Makefile for core library for VMS
-# contributed by Jouk Jansen  joukj@hrem.nano.tudelft.nl
-# Last revision : 29 September 2008
-
-.first
-	define gl [----.include.gl]
-	define math [--.math]
-	define tnl [--.tnl]
-	define swrast [--.swrast]
-	define glapi [--.glapi]
-	define shader [--.shader]
-	define main [--.main]
-
-.include [----]mms-config.
-
-##### MACROS #####
-
-VPATH = RCS
-
-INCDIR = [----.include],[--.main],[--.glapi],[--.shader]
-LIBDIR = [----.lib]
-CFLAGS = /include=($(INCDIR),[])/define=(PTHREADS=1)/name=(as_is,short)\
-	/float=ieee/ieee=denorm/warn=disable=(PTRMISMATCH)
-
-SOURCES = driverfuncs.c
-
-OBJECTS =driverfuncs.obj
-
-##### RULES #####
-
-VERSION=Mesa V3.4
-
-##### TARGETS #####
-# Make the library
-$(LIBDIR)$(GL_LIB) : $(OBJECTS)
-  @ library $(LIBDIR)$(GL_LIB) $(OBJECTS)
-
-clean :
-	purge
-	delete *.obj;*
-
-driverfuncs.obj : driverfuncs.c

src/mesa/drivers/osmesa/descrip.mms
@@ -1,45 +1,0 @@
-# Makefile for core library for VMS
-# contributed by Jouk Jansen  joukj@hrem.nano.tudelft.nl
-# Last revision : 3 October 2007
-
-.first
-	define gl [----.include.gl]
-	define math [--.math]
-	define tnl [--.tnl]
-	define vbo [--.vbo]
-	define swrast [--.swrast]
-	define swrast_setup [--.swrast_setup]
-	define array_cache [--.array_cache]
-	define drivers [-]
-	define glapi [--.glapi]
-	define main [--.main]
-	define shader [--.shader]
-
-.include [----]mms-config.
-
-##### MACROS #####
-
-VPATH = RCS
-
-INCDIR = [----.include],[--.main],[--.glapi],[--.shader]
-LIBDIR = [----.lib]
-CFLAGS = /include=($(INCDIR),[])/define=(PTHREADS=1)/name=(as_is,short)/float=ieee/ieee=denorm
-
-SOURCES = osmesa.c
-
-OBJECTS = osmesa.obj
-
-##### RULES #####
-
-VERSION=Mesa V3.4
-
-##### TARGETS #####
-# Make the library
-$(LIBDIR)$(GL_LIB) : $(OBJECTS)
-  @ library $(LIBDIR)$(GL_LIB) $(OBJECTS)
-
-clean :
-	purge
-	delete *.obj;*
-
-osmesa.obj : osmesa.c

commit 0829b893a9e9c874c0ef80d343af49334305e32e

mesa: Fix ieee fp on Alpha
Commit 1f82bf12ed inadvertently broke it, checking for __IEEE_FLOAT on all
Alpha machines instead of only on VMS as before.

NOTE: This is a candidate for the 9.1 branch.

Reviewed-by: Matt Turner <mattst88@gmail.com>
Reviewed-by: Andreas Boll <andreas.boll.dev@gmail.com>
Signed-off-by: Sven Joachim <svenjoac@gmx.de>

src/mesa/main/compiler.h
@@ -316,7 +316,7 @@ static INLINE GLuint CPU_TO_LE32(GLuint x)
     defined(__arm__) || \
     defined(__sh__) || defined(__m32r__) || \
     (defined(__sun) && defined(_IEEE_754)) || \
-    (defined(__alpha__) && defined(__IEEE_FLOAT))
+    defined(__alpha__)
 #define USE_IEEE
 #define IEEE_ONE 0x3f800000
 #endif