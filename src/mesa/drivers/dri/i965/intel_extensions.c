46d9baf3e3186078ca5a74f9d3e70ad493f899cb
Kenneth Graunke <kenneth@whitecape.org>
2013-07-17 18:18:00 -0700

    i965: Enable the GL_ARB_shading_language_420pack extension on Gen6+.
    While all the work is in the shared GLSL compiler, this extension
    requires GLSL 1.30, which is currently only supported on Gen6+.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -139,6 +139,7 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.ARB_draw_buffers_blend = true;
       ctx->Extensions.ARB_ES3_compatibility = true;
       ctx->Extensions.ARB_uniform_buffer_object = true;
+      ctx->Extensions.ARB_shading_language_420pack = true;
       ctx->Extensions.ARB_texture_buffer_object = true;
       ctx->Extensions.ARB_texture_buffer_object_rgb32 = true;
       ctx->Extensions.ARB_texture_cube_map_array = true;
