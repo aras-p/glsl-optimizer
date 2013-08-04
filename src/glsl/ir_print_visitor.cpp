1ecfdba98a346c8bb05ad9403e3a6412574215f4
Matt Turner <mattst88@gmail.com>
2013-08-04 14:01:30 -0700

    glsl: Add heuristics to print floating-point numbers better.
    v2: Fix *.expected files to match.
    Reviewed-by: Paul Berry <strereotype441@gmail.com>

--- src/glsl/ir_print_visitor.cpp
+++ src/glsl/ir_print_visitor.cpp
@@ -406,7 +406,17 @@ void ir_print_visitor::visit(ir_constant *ir)
 	 switch (ir->type->base_type) {
 	 case GLSL_TYPE_UINT:  printf("%u", ir->value.u[i]); break;
 	 case GLSL_TYPE_INT:   printf("%d", ir->value.i[i]); break;
-	 case GLSL_TYPE_FLOAT: printf("%f", ir->value.f[i]); break;
+	 case GLSL_TYPE_FLOAT:
+            if (ir->value.f[i] == 0.0f)
+               /* 0.0 == -0.0, so print with %f to get the proper sign. */
+               printf("%.1f", ir->value.f[i]);
+            else if (abs(ir->value.f[i]) < 0.000001f)
+               printf("%a", ir->value.f[i]);
+            else if (abs(ir->value.f[i]) > 1000000.0f)
+               printf("%e", ir->value.f[i]);
+            else
+               printf("%f", ir->value.f[i]);
+            break;
 	 case GLSL_TYPE_BOOL:  printf("%d", ir->value.b[i]); break;
 	 default: assert(0);
 	 }
