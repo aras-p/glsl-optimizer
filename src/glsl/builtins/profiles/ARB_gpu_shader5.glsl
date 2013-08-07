ac74de37109b62ca092827ca263de1935e2d1c08
Matt Turner <mattst88@gmail.com>
2013-08-06 21:45:30 -0700

    glsl: Add built-ins from ARB_shader_bit_encoding to ARB_gpu_shader5.
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

--- src/glsl/builtins/profiles/ARB_gpu_shader5.glsl
+++ src/glsl/builtins/profiles/ARB_gpu_shader5.glsl
@@ -59,3 +59,23 @@ float fma(float a, float b, float c);
 vec2  fma(vec2  a, vec2  b, vec2  c);
 vec3  fma(vec3  a, vec3  b, vec3  c);
 vec4  fma(vec4  a, vec4  b, vec4  c);
+
+int   floatBitsToInt(float value);
+ivec2 floatBitsToInt(vec2  value);
+ivec3 floatBitsToInt(vec3  value);
+ivec4 floatBitsToInt(vec4  value);
+
+uint  floatBitsToUint(float value);
+uvec2 floatBitsToUint(vec2  value);
+uvec3 floatBitsToUint(vec3  value);
+uvec4 floatBitsToUint(vec4  value);
+
+float intBitsToFloat(int   value);
+vec2  intBitsToFloat(ivec2 value);
+vec3  intBitsToFloat(ivec3 value);
+vec4  intBitsToFloat(ivec4 value);
+
+float uintBitsToFloat(uint  value);
+vec2  uintBitsToFloat(uvec2 value);
+vec3  uintBitsToFloat(uvec3 value);
+vec4  uintBitsToFloat(uvec4 value);
