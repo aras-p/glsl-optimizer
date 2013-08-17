aras-p/glsl-optimizer

commit 21d9a1b5ef51ce449e9a82641d0d605c5448b41c

radeonsi: require LLVM 3.4 for MSAA

src/gallium/drivers/radeonsi/radeonsi_pipe.c
@@ -371,8 +371,9 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_PREFER_BLIT_BASED_TEXTURE_TRANSFER:
 	case PIPE_CAP_TGSI_INSTANCEID:
 	case PIPE_CAP_COMPUTE:
+
 	case PIPE_CAP_TEXTURE_MULTISAMPLE:
-		return 1;
+		return HAVE_LLVM >= 0x0304 && rscreen->chip_class == SI;
 
 	case PIPE_CAP_TGSI_TEXCOORD:
 		return 0;

src/gallium/drivers/radeonsi/si_state.c
@@ -1689,7 +1689,7 @@ boolean si_is_format_supported(struct pipe_screen *screen,
 		return FALSE;
 
 	if (sample_count > 1) {
-		if (rscreen->chip_class >= CIK)
+		if (HAVE_LLVM < 0x0304 || rscreen->chip_class != SI)
 			return FALSE;
 
 		switch (sample_count) {

commit aafb0f9e06a0e08ebb38a92ce2090739d380df71

radeonsi: fix feature support reporting
broken by 21d9a1b5ef51ce449e9a82641d0d605c5448b41c

src/gallium/drivers/radeonsi/radeonsi_pipe.c
@@ -371,6 +371,7 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_PREFER_BLIT_BASED_TEXTURE_TRANSFER:
 	case PIPE_CAP_TGSI_INSTANCEID:
 	case PIPE_CAP_COMPUTE:
+		return 1;
 
 	case PIPE_CAP_TEXTURE_MULTISAMPLE:
 		return HAVE_LLVM >= 0x0304 && rscreen->chip_class == SI;