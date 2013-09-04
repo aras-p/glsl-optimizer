9641f1037c285612aebbab318a7db86a8bfb1c21
José Fonseca <jfonseca@vmware.com>
2013-09-04 14:54:31 +0100

    trace: Support bigger TGSI shaders.
    Trivial.

--- src/gallium/drivers/trace/tr_dump_state.c
+++ src/gallium/drivers/trace/tr_dump_state.c
@@ -255,7 +255,6 @@ void trace_dump_clip_state(const struct pipe_clip_state *state)
 
 void trace_dump_shader_state(const struct pipe_shader_state *state)
 {
-   static char str[8192];
    unsigned i;
 
    if (!trace_dumping_enabled_locked())
@@ -271,6 +270,7 @@ void trace_dump_shader_state(const struct pipe_shader_state *state)
 
    trace_dump_member_begin("tokens");
    if (state->tokens) {
+      static char str[64 * 1024];
       tgsi_dump_str(state->tokens, 0, str, sizeof(str));
       trace_dump_string(str);
    } else {
