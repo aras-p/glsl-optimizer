1d05caf9f2b27b1af4424ebd66584ab2b25025a5
Brian Paul <brianp@vmware.com>
2013-09-30 09:47:31 -0600

    svga: fix pixel center integer
    The svga/d3d9 convention is that pixel centers are at integer coordinates.
    Fixes piglit glsl-arb-fragment-coord-conventions test.
    
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/gallium/drivers/svga/svga_screen.c
+++ src/gallium/drivers/svga/svga_screen.c
@@ -207,11 +207,13 @@ svga_get_param(struct pipe_screen *screen, enum pipe_cap param)
       return 1;
 
    case PIPE_CAP_TGSI_FS_COORD_ORIGIN_UPPER_LEFT:
-   case PIPE_CAP_TGSI_FS_COORD_PIXEL_CENTER_HALF_INTEGER:
       return 1;
-   case PIPE_CAP_TGSI_FS_COORD_ORIGIN_LOWER_LEFT:
-   case PIPE_CAP_TGSI_FS_COORD_PIXEL_CENTER_INTEGER:
+   case PIPE_CAP_TGSI_FS_COORD_PIXEL_CENTER_HALF_INTEGER:
       return 0;
+   case PIPE_CAP_TGSI_FS_COORD_ORIGIN_LOWER_LEFT:
+      return 0;
+   case PIPE_CAP_TGSI_FS_COORD_PIXEL_CENTER_INTEGER:
+      return 1;
 
    case PIPE_CAP_VERTEX_COLOR_UNCLAMPED:
       return 1; /* The color outputs of vertex shaders are not clamped */
