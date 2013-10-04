c0a9436d191d24d9aea18fcca7a79674af3a782e
Eric Anholt <eric@anholt.net>
2013-10-04 16:13:00 -0700

    i965: Fix texture buffer rendering after a whole buffer replacement.
    If glBufferData(), glBufferSubData(0, obj->Size), or similar happens, we
    get a new drm_intel_bo for the buffer object, and thus need to re-upload
    texture buffer state so we point at the new data.
    
    Fixes the new piglit GL_ARB_texture_buffer_object/data-sync
    
    Cc: "9.2" <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/mesa/drivers/dri/i965/brw_wm_surface_state.c
+++ src/mesa/drivers/dri/i965/brw_wm_surface_state.c
@@ -261,6 +261,7 @@ brw_update_texture_surface(struct gl_context *ctx,
    struct gl_sampler_object *sampler = _mesa_get_samplerobj(ctx, unit);
    uint32_t *surf;
 
+   /* BRW_NEW_UNIFORM_BUFFER */
    if (tObj->Target == GL_TEXTURE_BUFFER) {
       brw_update_buffer_texture_surface(ctx, unit, surf_offset);
       return;
@@ -787,6 +788,7 @@ const struct brw_tracked_state brw_texture_surfaces = {
    .dirty = {
       .mesa = _NEW_TEXTURE,
       .brw = BRW_NEW_BATCH |
+             BRW_NEW_UNIFORM_BUFFER |
              BRW_NEW_VERTEX_PROGRAM |
              BRW_NEW_GEOMETRY_PROGRAM |
              BRW_NEW_FRAGMENT_PROGRAM,
