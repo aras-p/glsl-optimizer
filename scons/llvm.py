94d05bf87a21bd364e84f699a0064e5fba58a6f9
Alexander von Gluck IV <kallisti5@unixzen.com>
2013-10-15 12:08:59 -0500

    scons: Fix build when rtti is disabled
    * The rtti fix actually dug up a bug in the scons build scripts.
    * Autotools took the LLVM cpp and cxx flags, while scons only took
      the cpp flags.
    * This grabs the cxx flags and applies them where needed. We may
      want to make the same change for the llvm cpp flags in scons.
    * The only linux platform I can find with LLVM no-rtti is Ubuntu.
    * Fixes bug #70471
    
    Tested-by: Vinson Lee <vlee@freedesktop.org>

--- scons/llvm.py
+++ scons/llvm.py
@@ -190,6 +190,9 @@ def generate(env):
                 pass
             env.MergeFlags(cppflags)
 
+            cxxflags = env.backtick('llvm-config --cxxflags').rstrip()
+            env.Append(LLVM_CXXFLAGS = cxxflags)
+
             components = ['engine', 'bitwriter', 'x86asmprinter']
 
             if llvm_version >= distutils.version.LooseVersion('3.1'):
