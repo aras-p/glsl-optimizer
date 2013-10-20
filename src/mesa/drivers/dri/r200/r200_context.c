5da618c20eaf8052e3ad115c1e57bcbb1ee0e0cb
Rico Schüller <kgbricola@web.de>
2013-10-20 12:39:58 +0200

    r200: Enable ARB_texture_mirror_clamp_to_edge.
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Signed-off-by: Rico Schüller <kgbricola@web.de>

--- src/mesa/drivers/dri/r200/r200_context.c
+++ src/mesa/drivers/dri/r200/r200_context.c
@@ -362,6 +362,7 @@ GLboolean r200CreateContext( gl_api api,
    ctx->Extensions.ARB_texture_env_combine = true;
    ctx->Extensions.ARB_texture_env_dot3 = true;
    ctx->Extensions.ARB_texture_env_crossbar = true;
+   ctx->Extensions.ARB_texture_mirror_clamp_to_edge = true;
    ctx->Extensions.EXT_blend_color = true;
    ctx->Extensions.EXT_blend_minmax = true;
    ctx->Extensions.EXT_packed_depth_stencil = true;
