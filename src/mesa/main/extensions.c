a59ae25d81728e572568e64fe1ab72ef834044b6
Rico Schüller <kgbricola@web.de>
2013-10-20 12:39:56 +0200

    swrast: Enable ARB_texture_mirror_clamp_to_edge.
    v2: fix commit message
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Signed-off-by: Rico Schüller <kgbricola@web.de>

--- src/mesa/main/extensions.c
+++ src/mesa/main/extensions.c
@@ -408,6 +408,7 @@ _mesa_enable_sw_extensions(struct gl_context *ctx)
 #ifdef TEXTURE_FLOAT_ENABLED
    ctx->Extensions.ARB_texture_float = GL_TRUE;
 #endif
+   ctx->Extensions.ARB_texture_mirror_clamp_to_edge = GL_TRUE;
    ctx->Extensions.ARB_texture_non_power_of_two = GL_TRUE;
    ctx->Extensions.ARB_texture_rg = GL_TRUE;
    ctx->Extensions.ARB_texture_compression_rgtc = GL_TRUE;
