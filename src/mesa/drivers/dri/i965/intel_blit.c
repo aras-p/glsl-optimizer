68f1b274b06ed6f14cc8d069bee3cabc520fb553
Vinson Lee <vlee@freedesktop.org>
2013-11-03 14:43:53 -0800

    i965: Fix logic_op check.
    Fixes "Macro compares unsigned to 0" defect reported by Coverity.
    
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/mesa/drivers/dri/i965/intel_blit.c
+++ src/mesa/drivers/dri/i965/intel_blit.c
@@ -417,8 +417,7 @@ intelEmitImmediateColorExpandBlit(struct brw_context *brw,
 	 return false;
    }
 
-   assert( logic_op - GL_CLEAR >= 0 );
-   assert( logic_op - GL_CLEAR < 0x10 );
+   assert((logic_op >= GL_CLEAR) && (logic_op <= (GL_CLEAR + 0x0f)));
    assert(dst_pitch > 0);
 
    if (w < 0 || h < 0)
