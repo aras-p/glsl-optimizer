749cb8909714fed57b0213f4ad92def3bb24f153
Vinson Lee <vlee@freedesktop.org>
2013-11-03 20:27:13 -0800

    gallivm: Remove llvm::DisablePrettyStackTrace for LLVM >= 3.4.
    LLVM 3.4 r193971 removed llvm::DisablePrettyStackTrace and made the
    pretty stack trace opt-in rather than opt-out.
    
    The default value of DisablePrettyStackTrace has changed to true in LLVM
    3.4 and newer.
    
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=60929
    Reviewed-by: Tom Stellard <thomas.stellard@amd.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/gallium/auxiliary/gallivm/lp_bld_misc.cpp
+++ src/gallium/auxiliary/gallivm/lp_bld_misc.cpp
@@ -174,12 +174,14 @@ lp_set_target_options(void)
    }
 #endif
 
+#if HAVE_LLVM < 0x0304
    /*
     * By default LLVM adds a signal handler to output a pretty stack trace.
     * This signal handler is never removed, causing problems when unloading the
     * shared object where the gallium driver resides.
     */
    llvm::DisablePrettyStackTrace = true;
+#endif
 
    // If we have a native target, initialize it to ensure it is linked in and
    // usable by the JIT.
