477f8cd08bb8467d28fdfd6c25fe358957e3da58
Eric Anholt <eric@anholt.net>
2013-11-07 12:10:25 -0800

    glsl: Apply the transformation "(a ^^ a) -> false" in opt_algebraic.
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/glsl/opt_algebraic.cpp
+++ src/glsl/opt_algebraic.cpp
@@ -377,7 +377,6 @@ ir_algebraic_visitor::handle_expression(ir_expression *ir)
       break;
 
    case ir_binop_logic_xor:
-      /* FINISHME: Also simplify (a ^^ a) to (false). */
       if (is_vec_zero(op_const[0])) {
 	 return ir->operands[1];
       } else if (is_vec_zero(op_const[1])) {
@@ -386,6 +385,9 @@ ir_algebraic_visitor::handle_expression(ir_expression *ir)
 	 return logic_not(ir->operands[1]);
       } else if (is_vec_one(op_const[1])) {
 	 return logic_not(ir->operands[0]);
+      } else if (ir->operands[0]->equals(ir->operands[1])) {
+         /* (a ^^ a) == false */
+	 return ir_constant::zero(mem_ctx, ir->type);
       }
       break;
 
