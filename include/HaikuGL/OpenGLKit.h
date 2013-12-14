56d920a5c1b64868e77a97604c01d3a63916a6ca
Alexander von Gluck IV <kallisti5@unixzen.com>
2013-12-14 11:46:05 -0600

    Haiku: Add in public GL kit headers
    * These make up the base of what C++ GL Haiku applications
      use for 3D rendering.
    * Not placed in includes/GL to prevent Haiku headers from
      getting installed on non-Haiku systems.
    
    Acked-by: Brian Paul <brianp@vmware.com>

---
+++ include/HaikuGL/OpenGLKit.h
@@ -0,0 +1,10 @@
+/*
+ * Master include file for the Haiku OpenGL Kit.
+ */
+
+#include <GL/gl.h>
+#include <GLView.h>
+
+// Projects needing GL/glu.h and GL/glut.h should now
+// include these headers independently as glu and glut
+// are no longe core parts of mesa
