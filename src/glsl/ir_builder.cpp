61c450fc81900131621e57fdd78a40e82239daf3
Maxence Le Doré <maxence.ledore@gmail.com>
2014-01-03 00:09:50 +0100

    glsl: add min() and max() functions to builder.cpp
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/glsl/ir_builder.cpp
+++ src/glsl/ir_builder.cpp
@@ -211,6 +211,16 @@ ir_expression *sub(operand a, operand b)
    return expr(ir_binop_sub, a, b);
 }
 
+ir_expression *min(operand a, operand b)
+{
+   return expr(ir_binop_min, a, b);
+}
+
+ir_expression *max(operand a, operand b)
+{
+   return expr(ir_binop_max, a, b);
+}
+
 ir_expression *mul(operand a, operand b)
 {
    return expr(ir_binop_mul, a, b);
