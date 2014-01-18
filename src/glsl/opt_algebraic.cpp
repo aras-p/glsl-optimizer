de796b0ef09f05ca3f8a568ed07293e28ae697b0
Eric Anholt <eric@anholt.net>
2014-01-18 11:06:16 -0800

    glsl: Optimize various cases of fma (aka MAD).
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- src/glsl/opt_algebraic.cpp
+++ src/glsl/opt_algebraic.cpp
@@ -547,6 +547,19 @@ ir_algebraic_visitor::handle_expression(ir_expression *ir)
 
       break;
 
+   case ir_triop_fma:
+      /* Operands are op0 * op1 + op2. */
+      if (is_vec_zero(op_const[0]) || is_vec_zero(op_const[1])) {
+         return ir->operands[2];
+      } else if (is_vec_zero(op_const[2])) {
+         return mul(ir->operands[0], ir->operands[1]);
+      } else if (is_vec_one(op_const[0])) {
+         return add(ir->operands[1], ir->operands[2]);
+      } else if (is_vec_one(op_const[1])) {
+         return add(ir->operands[0], ir->operands[2]);
+      }
+      break;
+
    case ir_triop_lrp:
       /* Operands are (x, y, a). */
       if (is_vec_zero(op_const[2])) {
