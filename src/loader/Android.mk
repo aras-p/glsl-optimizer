aras-p/glsl-optimizer

commit 8c2e7fd8460750543367053b1be9368cc38e1d6a

loader: introduce the loader util lib
All the various window system integration layers duplicate roughly the
same code for figuring out device and driver name, pci-id's, etc.  Which
is sad.  So extract it out into a loader util lib.

v2 (Emil)
* Separate the introduction of libloader from the code de-duplication.
* Strip out non-pci devices support.
* Add scons + Android build system support.
* Add VISIBILITY_CFLAGS to avoid exporting the loader funcs.

v3 (Emil)
* PIPE_OS_ANDROID is undefined at this scope, use ANDROID
* Make sure we define _EGL_NO_DRM when building only swrast

Signed-off-by: Rob Clark <robclark@freedesktop.org>
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Reviewed-by: Rob Clark <robdclark@gmail.com>
Reviewed-by: Kristian Høgsberg <krh@bitplanet.net>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

configure.ac
@@ -2059,6 +2059,7 @@ AC_CONFIG_FILES([Makefile
 		src/glx/Makefile
 		src/glx/tests/Makefile
 		src/gtest/Makefile
+		src/loader/Makefile
 		src/mapi/Makefile
 		src/mapi/es1api/Makefile
 		src/mapi/es1api/glesv1_cm.pc

src/loader/Android.mk
@@ -1,7 +1,6 @@
 # Mesa 3-D graphics library
 #
-# Copyright (C) 2010-2011 Chia-I Wu <olvaffe@gmail.com>
-# Copyright (C) 2010-2011 LunarG Inc.
+# Copyright (C) 2014 Emil Velikov <emil.l.velikov@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -21,30 +20,29 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-# Android.mk for egl_dri2
-
 LOCAL_PATH := $(call my-dir)
+
+include $(LOCAL_PATH)/Makefile.sources
+
+# ---------------------------------------
+# Build libloader
+# ---------------------------------------
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	egl_dri2.c \
-	platform_android.c
+	$(LOADER_C_FILES)
 
-LOCAL_CFLAGS := \
-	-DDEFAULT_DRIVER_DIR=\"/system/lib/dri\" \
-	-DHAVE_SHARED_GLAPI \
-	-DHAVE_ANDROID_PLATFORM
+# swrast only
+ifeq ($(MESA_GPU_DRIVERS),swrast)
+	LOCAL_CFLAGS += -D_EGL_NO_DRM
+else
+LOCAL_C_INCLUDES += \
+	$(DRM_TOP)/include/drm \
+	$(DRM_TOP)
+endif
 
-LOCAL_C_INCLUDES := \
-	$(MESA_TOP)/src/mapi \
-	$(MESA_TOP)/src/egl/main \
-	$(MESA_TOP)/src/loader \
-	$(DRM_GRALLOC_TOP) \
-	$(DRM_TOP) \
-	$(DRM_TOP)/include/drm
-
-LOCAL_MODULE := libmesa_egl_dri2
+LOCAL_MODULE := libloader
 
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)

src/loader/Makefile.am
@@ -1,4 +1,4 @@
-# Copyright © 2013 Intel Corporation
+# Copyright © 2012 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -19,39 +19,19 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-SUBDIRS = gtest loader mapi
+include Makefile.sources
 
-if NEED_OPENGL_COMMON
-SUBDIRS += glsl mesa
-endif
+noinst_LTLIBRARIES = libloader.la
 
-if HAVE_DRI_GLX
-SUBDIRS += glx
-endif
+libloader_la_CPPFLAGS = \
+	$(DEFINES) \
+	-I$(top_srcdir)/include \
+	$(VISIBILITY_CFLAGS) \
+	$(LIBDRM_CFLAGS) \
+	$(LIBUDEV_CFLAGS)
 
-if HAVE_EGL_PLATFORM_WAYLAND
-SUBDIRS += egl/wayland
-endif
+libloader_la_LIBADD = \
+	$(LIBDRM_LIBS) \
+	$(LIBUDEV_LIBS)
 
-if HAVE_GBM
-SUBDIRS += gbm
-endif
-
-if HAVE_EGL
-SUBDIRS += egl
-endif
-
-if HAVE_GALLIUM
-SUBDIRS +=			\
-	gallium/auxiliary	\
-	gallium/drivers		\
-	gallium/state_trackers	\
-	gallium/winsys		\
-	gallium/targets
-
-if HAVE_GALLIUM_TESTS
-SUBDIRS +=			\
-	gallium/tests/trivial	\
-	gallium/tests/unit
-endif
-endif
+libloader_la_SOURCES = $(LOADER_C_FILES)

src/loader/SConscript
@@ -1,0 +1,28 @@
+Import('*')
+
+if not env['drm']:
+    Return()
+
+env = env.Clone()
+
+env.Prepend(CPPPATH = [
+    '.',
+    '#include'
+])
+
+env.PkgUseModules('DRM')
+
+if env['udev']:
+    env.PkgUseModules('UDEV')
+    env.Append(CPPDEFINES = ['HAVE_LIBUDEV'])
+
+# parse Makefile.sources
+sources = env.ParseSourceList('Makefile.sources', 'LOADER_C_FILES')
+
+libloader = env.ConvenienceLibrary(
+    target = 'libloader',
+    source = sources,
+)
+
+env.Alias('libloader', libloader)
+Export('libloader')

src/loader/loader.c
@@ -1,0 +1,304 @@
+/*
+ * Copyright (C) 2013 Rob Clark <robclark@freedesktop.org>
+ *
+ * This code is derived from the following files.
+ *
+ * * src/glx/dri3_common.c
+ * Copyright © 2013 Keith Packard
+ *
+ * * src/egl/drivers/dri2/common.c
+ * * src/gbm/backends/dri/driver_name.c
+ * Copyright © 2011 Intel Corporation
+ *
+ * Authors:
+ *    Kristian Høgsberg <krh@bitplanet.net>
+ *    Benjamin Franzke <benjaminfranzke@googlemail.com>
+ *
+ * * src/gallium/targets/egl-static/egl.c
+ * Copyright (C) 2010-2011 LunarG Inc.
+ *
+ * Authors:
+ *    Chia-I Wu <olv@lunarg.com>
+ *
+ * * src/gallium/state_trackers/egl/drm/native_drm.c
+ * Copyright (C) 2010 Chia-I Wu <olv@0xlab.org>
+ *
+ * * src/egl/drivers/dri2/platform_android.c
+ *
+ * Copyright (C) 2010-2011 Chia-I Wu <olvaffe@gmail.com>
+ * Copyright (C) 2010-2011 LunarG Inc.
+ *
+ * Based on platform_x11, which has
+ *
+ * Copyright © 2011 Intel Corporation
+ *
+ * * src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
+ * Copyright 2011 Intel Corporation
+ * Copyright 2012 Francisco Jerez
+ * All Rights Reserved.
+ *
+ * Authors:
+ *    Kristian Høgsberg <krh@bitplanet.net>
+ *    Benjamin Franzke <benjaminfranzke@googlemail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Authors:
+ *    Rob Clark <robclark@freedesktop.org>
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include "loader.h"
+
+#include <xf86drm.h>
+
+#define __IS_LOADER
+#include "pci_ids/pci_id_driver_map.h"
+
+static void default_logger(int level, const char *fmt, ...)
+{
+   if (level >= _LOADER_WARNING) {
+      va_list args;
+      va_start(args, fmt);
+      vfprintf(stderr, fmt, args);
+      va_end(args);
+      fprintf(stderr, "\n");
+   }
+}
+
+static void (*log)(int level, const char *fmt, ...) = default_logger;
+
+#ifdef HAVE_LIBUDEV
+#include <libudev.h>
+
+static inline struct udev_device *
+udev_device_new_from_fd(struct udev *udev, int fd)
+{
+   struct udev_device *device;
+   struct stat buf;
+
+   if (fstat(fd, &buf) < 0) {
+      log(_LOADER_WARNING, "MESA-LOADER: failed to stat fd %d", fd);
+      return NULL;
+   }
+
+   device = udev_device_new_from_devnum(udev, 'c', buf.st_rdev);
+   if (device == NULL) {
+      log(_LOADER_WARNING,
+              "MESA-LOADER: could not create udev device for fd %d", fd);
+      return NULL;
+   }
+
+   return device;
+}
+
+int
+loader_get_pci_id_for_fd(int fd, int *vendor_id, int *chip_id)
+{
+   struct udev *udev = NULL;
+   struct udev_device *device = NULL, *parent;
+   struct stat buf;
+   const char *pci_id;
+
+   *chip_id = -1;
+
+   udev = udev_new();
+   device = udev_device_new_from_fd(udev, fd);
+   if (!device)
+      goto out;
+
+   parent = udev_device_get_parent(device);
+   if (parent == NULL) {
+      log(_LOADER_WARNING, "MESA-LOADER: could not get parent device");
+      goto out;
+   }
+
+   pci_id = udev_device_get_property_value(parent, "PCI_ID");
+   if (pci_id == NULL ||
+       sscanf(pci_id, "%x:%x", vendor_id, chip_id) != 2) {
+      log(_LOADER_WARNING, "MESA-LOADER: malformed or no PCI ID");
+      *chip_id = -1;
+      goto out;
+   }
+
+out:
+   if (device)
+      udev_device_unref(device);
+   if (udev)
+      udev_unref(udev);
+
+   return (*chip_id >= 0);
+}
+
+#elif defined(ANDROID) && !defined(_EGL_NO_DRM)
+
+/* for i915 */
+#include <i915_drm.h>
+/* for radeon */
+#include <radeon_drm.h>
+
+int
+loader_get_pci_id_for_fd(int fd, int *vendor_id, int *chip_id)
+{
+   drmVersionPtr version;
+
+   *chip_id = -1;
+
+   version = drmGetVersion(fd);
+   if (!version) {
+      log(_LOADER_WARNING, "MESA-LOADER: invalid drm fd");
+      return FALSE;
+   }
+   if (!version->name) {
+      log(_LOADER_WARNING, "MESA-LOADER: unable to determine the driver name");
+      drmFreeVersion(version);
+      return FALSE;
+   }
+
+   if (strcmp(version->name, "i915") == 0) {
+      struct drm_i915_getparam gp;
+      int ret;
+
+      *vendor_id = 0x8086;
+
+      memset(&gp, 0, sizeof(gp));
+      gp.param = I915_PARAM_CHIPSET_ID;
+      gp.value = chip_id;
+      ret = drmCommandWriteRead(fd, DRM_I915_GETPARAM, &gp, sizeof(gp));
+      if (ret) {
+         log(_LOADER_WARNING, "MESA-LOADER: failed to get param for i915");
+	 *chip_id = -1;
+      }
+   }
+   else if (strcmp(version->name, "radeon") == 0) {
+      struct drm_radeon_info info;
+      int ret;
+
+      *vendor_id = 0x1002;
+
+      memset(&info, 0, sizeof(info));
+      info.request = RADEON_INFO_DEVICE_ID;
+      info.value = (unsigned long) chip_id;
+      ret = drmCommandWriteRead(fd, DRM_RADEON_INFO, &info, sizeof(info));
+      if (ret) {
+         log(_LOADER_WARNING, "MESA-LOADER: failed to get info for radeon");
+	 *chip_id = -1;
+      }
+   }
+   else if (strcmp(version->name, "nouveau") == 0) {
+      *vendor_id = 0x10de;
+      /* not used */
+      *chip_id = 0;
+   }
+   else if (strcmp(version->name, "vmwgfx") == 0) {
+      *vendor_id = 0x15ad;
+      /* assume SVGA II */
+      *chip_id = 0x0405;
+   }
+
+   drmFreeVersion(version);
+
+   return (*chip_id >= 0);
+}
+
+#else
+
+int
+loader_get_pci_id_for_fd(int fd, int *vendor_id, int *chip_id)
+{
+   return 0;
+}
+
+#endif
+
+
+char *
+loader_get_device_name_for_fd(int fd)
+{
+   char *device_name = NULL;
+#ifdef HAVE_LIBUDEV
+   struct udev *udev;
+   struct udev_device *device;
+   const char *const_device_name;
+
+   udev = udev_new();
+   device = udev_device_new_from_fd(udev, fd);
+   if (device == NULL)
+      return NULL;
+
+   const_device_name = udev_device_get_devnode(device);
+   if (!const_device_name)
+      goto out;
+   device_name = strdup(const_device_name);
+
+out:
+   udev_device_unref(device);
+   udev_unref(udev);
+#endif
+   return device_name;
+}
+
+char *
+loader_get_driver_for_fd(int fd, unsigned driver_types)
+{
+   int vendor_id, chip_id, i, j;
+   char *driver = NULL;
+
+   if (!driver_types)
+      driver_types = _LOADER_GALLIUM | _LOADER_DRI;
+
+   if (!loader_get_pci_id_for_fd(fd, &vendor_id, &chip_id)) {
+      log(_LOADER_WARNING, "failed to get driver name for fd %d", fd);
+      return NULL;
+   }
+
+   for (i = 0; driver_map[i].driver; i++) {
+      if (vendor_id != driver_map[i].vendor_id)
+         continue;
+
+      if (!(driver_types & driver_map[i].driver_types))
+         continue;
+
+      if (driver_map[i].num_chips_ids == -1) {
+         driver = strdup(driver_map[i].driver);
+         goto out;
+      }
+
+      for (j = 0; j < driver_map[i].num_chips_ids; j++)
+         if (driver_map[i].chip_ids[j] == chip_id) {
+            driver = strdup(driver_map[i].driver);
+            goto out;
+         }
+   }
+
+out:
+   log(driver ? _LOADER_INFO : _LOADER_WARNING,
+         "pci id for fd %d: %04x:%04x, driver %s",
+         fd, vendor_id, chip_id, driver);
+   return driver;
+}
+
+void
+loader_set_logger(void (*logger)(int level, const char *fmt, ...))
+{
+   log = logger;
+}

Android.mk
@@ -78,6 +78,7 @@ endif
 ifneq ($(strip $(MESA_GPU_DRIVERS)),)
 
 SUBDIRS := \
+	src/loader \
 	src/mapi \
 	src/glsl \
 	src/mesa \

src/SConscript
@@ -11,6 +11,7 @@ if env['hostonly']:
     # compilation
     Return()
 
+SConscript('loader/SConscript')
 
 # When env['gles'] is set, the targets defined in mapi/glapi/SConscript are not
 # used.  libgl-xlib and libgl-gdi adapt themselves to use the targets defined

src/egl/drivers/dri2/Makefile.am
@@ -22,6 +22,7 @@
 AM_CFLAGS = \
 	-I$(top_srcdir)/include \
 	-I$(top_srcdir)/src/egl/main \
+	-I$(top_srcdir)/src/loader \
 	-I$(top_srcdir)/src/gbm/main \
 	-I$(top_srcdir)/src/gbm/backends/dri \
 	-I$(top_srcdir)/src/egl/wayland/wayland-egl \

commit 26d380da69d3fc3a4ced291028c105c0ac0f1a92

loader: ifdef libdrm specific code and include
Mesa provides the flexibility of building without the
need to have libdrm present on the system. The situation
has regressed with the recent commit

commit 8c2e7fd8460750543367053b1be9368cc38e1d6a
Author: Emil Velikov <emil.l.velikov@gmail.com>
Date:   Fri Jan 10 23:36:16 2014 +0000

    loader: introduce the loader util lib

By isolating libdrm code by #ifndef __NOT_HAVE_DRM_H we
can have libdrm-less builds on across all build systems.

This patch converts Android's _EGL_NO_DRM to __NOT_HAVE_DRM_H
to provide consistency with the other cases within mesa, allows
compilation of libloader on libdrm-less scons and conditionally
links against libdrm if present under automake.

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=73776
BUgzilla: https://bugs.freedesktop.org/show_bug.cgi?id=73777
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

configure.ac
@@ -1935,6 +1935,7 @@ AM_CONDITIONAL(HAVE_MESA_LLVM, test x$MESA_LLVM = x1)
 AC_SUBST([ELF_LIB])
 
 AM_CONDITIONAL(DRICOMMON_NEED_LIBDRM, test "x$DRICOMMON_NEED_LIBDRM" = xyes)
+AM_CONDITIONAL(HAVE_LIBDRM, test "x$have_libdrm" = xyes)
 AM_CONDITIONAL(HAVE_X11_DRIVER, test "x$enable_xlib_glx" = xyes)
 AM_CONDITIONAL(HAVE_OSMESA, test "x$enable_osmesa" = xyes)
 AM_CONDITIONAL(HAVE_GALLIUM_OSMESA, test "x$enable_gallium_osmesa" = xyes)

src/loader/Android.mk
@@ -35,7 +35,7 @@ LOCAL_SRC_FILES := \
 
 # swrast only
 ifeq ($(MESA_GPU_DRIVERS),swrast)
-	LOCAL_CFLAGS += -D_EGL_NO_DRM
+	LOCAL_CFLAGS += -D__NOT_HAVE_DRM_H
 else
 LOCAL_C_INCLUDES += \
 	$(DRM_TOP)/include/drm \

src/loader/Makefile.am
@@ -27,11 +27,20 @@ libloader_la_CPPFLAGS = \
 	$(DEFINES) \
 	-I$(top_srcdir)/include \
 	$(VISIBILITY_CFLAGS) \
-	$(LIBDRM_CFLAGS) \
 	$(LIBUDEV_CFLAGS)
 
 libloader_la_LIBADD = \
-	$(LIBDRM_LIBS) \
 	$(LIBUDEV_LIBS)
+
+if !HAVE_LIBDRM
+libloader_la_CPPFLAGS += \
+	-D__NOT_HAVE_DRM_H
+else
+libloader_la_CPPFLAGS += \
+	$(LIBDRM_CFLAGS)
+
+libloader_la_LIBADD += \
+	$(LIBDRM_LIBS)
+endif
 
 libloader_la_SOURCES = $(LOADER_C_FILES)

src/loader/SConscript
@@ -1,15 +1,12 @@
 Import('*')
 
-if not env['drm']:
-    Return()
-
 env = env.Clone()
 
 env.Prepend(CPPPATH = [
     '#include'
 ])
 
-env.PkgUseModules('DRM')
+env.Append(CPPDEFINES = ['__NOT_HAVE_DRM_H'])
 
 if env['udev']:
     env.PkgUseModules('UDEV')

src/loader/loader.c
@@ -69,7 +69,9 @@
 #include <string.h>
 #include "loader.h"
 
+#ifndef __NOT_HAVE_DRM_H
 #include <xf86drm.h>
+#endif
 
 #define __IS_LOADER
 #include "pci_ids/pci_id_driver_map.h"
@@ -149,7 +151,7 @@ out:
    return (*chip_id >= 0);
 }
 
-#elif defined(ANDROID) && !defined(_EGL_NO_DRM)
+#elif defined(ANDROID) && !defined(__NOT_HAVE_DRM_H)
 
 /* for i915 */
 #include <i915_drm.h>
@@ -267,6 +269,8 @@ loader_get_driver_for_fd(int fd, unsigned driver_types)
       driver_types = _LOADER_GALLIUM | _LOADER_DRI;
 
    if (!loader_get_pci_id_for_fd(fd, &vendor_id, &chip_id)) {
+
+#ifndef __NOT_HAVE_DRM_H
       /* fallback to drmGetVersion(): */
       drmVersionPtr version = drmGetVersion(fd);
 
@@ -279,6 +283,7 @@ loader_get_driver_for_fd(int fd, unsigned driver_types)
       log(_LOADER_INFO, "using driver %s for %d", driver, fd);
 
       drmFreeVersion(version);
+#endif
 
       return driver;
    }