fa75cc4b896ea16efb43c01f0e220a97773c3a2e
José Fonseca <jfonseca@vmware.com>
2014-01-23 13:44:10 +0000

    os/os_thread: Revert pipe_barrier pre-processing logic.
    Whitelist platforms instead of blacklisting, as several pthread
    implementations are missing pthread_barrier_t, in particular MacOSX.

--- src/gallium/auxiliary/os/os_thread.h
+++ src/gallium/auxiliary/os/os_thread.h
@@ -130,7 +130,7 @@ typedef cnd_t pipe_condvar;
  * pipe_barrier
  */
 
-#if defined(HAVE_PTHREAD) && !defined(PIPE_OS_ANDROID)
+#if (defined(PIPE_OS_LINUX) || defined(PIPE_OS_BSD) || defined(PIPE_OS_SOLARIS) || defined(PIPE_OS_HURD)) && !defined(PIPE_OS_ANDROID)
 
 typedef pthread_barrier_t pipe_barrier;
 
