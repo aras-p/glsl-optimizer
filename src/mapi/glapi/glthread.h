94dc91d7ecdec4aadfbbe307dc598bb163af0c74
Brian Paul <brianp@vmware.com>
2014-03-01 10:40:24 -0700

    mesa: remove unused glthread/TSD macros
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/mapi/glapi/glthread.h
+++ src/mapi/glapi/glthread.h
@@ -7,13 +7,6 @@
 extern "C" {
 #endif
 
-#define _glthread_InitTSD(tsd)               u_tsd_init(tsd);
-#define _glthread_DestroyTSD(tsd)            u_tsd_destroy(tsd);
-#define _glthread_GetTSD(tsd)                u_tsd_get(tsd);
-#define _glthread_SetTSD(tsd, ptr)           u_tsd_set(tsd, ptr);
-
-typedef struct u_tsd _glthread_TSD;
-
 #ifdef __cplusplus
 }
 #endif
