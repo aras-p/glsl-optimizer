bbae65e25c25fb83fd84b5145c5f1177b561b38b
Emil Velikov <emil.l.velikov@gmail.com>
2014-03-02 20:59:39 +0000

    automake: introduce install-gallium-links.mk
    This helper script will be used to minimise the duplication
    during link generation across all gallium targets.
    
    v2:
     - Handle vdpau_LTLIBRARIES. Requested by Christian König.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Jon TURNEY <jon.turney@dronecode.org.uk>

---
+++ install-gallium-links.mk
@@ -0,0 +1,23 @@
+# Provide compatibility with scripts for the old Mesa build system for
+# a while by putting a link to the driver into /lib of the build tree.
+
+all-local : .libs/install-gallium-links
+
+.libs/install-gallium-links : $(dri_LTLIBRARIES) $(vdpau_LTLIBRARIES) $(egl_LTLIBRARIES) $(lib_LTLIBRARIES)
+	$(AM_V_GEN)$(MKDIR_P) $(top_builddir)/$(LIB_DIR);	\
+	link_dir=$(top_builddir)/$(LIB_DIR)/gallium;		\
+	if test x$(egl_LTLIBRARIES) != x; then			\
+		link_dir=$(top_builddir)/$(LIB_DIR)/egl;	\
+	fi;							\
+	$(MKDIR_P) $$link_dir;					\
+	file_list=$(dri_LTLIBRARIES:%.la=.libs/%.so*);		\
+	file_list+=$(vdpau_LTLIBRARIES:%.la=.libs/%.so*);	\
+	file_list+=$(egl_LTLIBRARIES:%.la=.libs/%.so*);		\
+	file_list+=$(lib_LTLIBRARIES:%.la=.libs/%.so*);		\
+	for f in $$file_list; do 				\
+		if test -h .libs/$$f; then			\
+			cp -d $$f $$link_dir;			\
+		else						\
+			ln -f $$f $$link_dir;			\
+		fi;						\
+	done && touch $@
