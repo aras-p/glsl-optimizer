e6c565fcc591f4856eff512f72e961ff0686475c
Vinson Lee <vlee@freedesktop.org>
2014-03-10 22:49:51 -0700

    radeon: Fix build.
    Fix build error introduced with commit
    dfa25ea5cd19d5a050a1c94bd7370a2259b9f007.
    
      CC       r600_streamout.lo
    r600_streamout.c:108:6: error: conflicting types for 'r600_set_streamout_targets'
    void r600_set_streamout_targets(struct pipe_context *ctx,
         ^
    ./r600_pipe_common.h:413:6: note: previous declaration is here
    void r600_set_streamout_targets(struct pipe_context *ctx,
         ^
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=76009
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>

--- src/gallium/drivers/radeon/r600_pipe_common.h
+++ src/gallium/drivers/radeon/r600_pipe_common.h
@@ -413,7 +413,7 @@ void r600_streamout_buffers_dirty(struct r600_common_context *rctx);
 void r600_set_streamout_targets(struct pipe_context *ctx,
 				unsigned num_targets,
 				struct pipe_stream_output_target **targets,
-				unsigned *offset);
+				const unsigned *offset);
 void r600_emit_streamout_end(struct r600_common_context *rctx);
 void r600_streamout_init(struct r600_common_context *rctx);
 
