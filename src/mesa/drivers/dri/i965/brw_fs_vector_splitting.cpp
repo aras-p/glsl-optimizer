cf40ebacb113a370c1b2445e881f8dc440a7d8f3
Eric Anholt <eric@anholt.net>
2014-03-25 12:44:01 -0700

    i965: Drop pointless check for variable declarations in splitting.
    We're walking the whole instruction stream, so we know the declaration
    will be found.
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/brw_fs_vector_splitting.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_vector_splitting.cpp
@@ -55,7 +55,6 @@ public:
    {
       this->var = var;
       this->whole_vector_access = 0;
-      this->declaration = false;
       this->mem_ctx = NULL;
    }
 
@@ -63,8 +62,6 @@ public:
 
    /** Number of times the variable is referenced, including assignments. */
    unsigned whole_vector_access;
-
-   bool declaration; /* If the variable had a decl in the instruction stream */
 
    ir_variable *components[4];
 
@@ -139,10 +136,8 @@ ir_vector_reference_visitor::get_variable_entry(ir_variable *var)
 ir_visitor_status
 ir_vector_reference_visitor::visit(ir_variable *ir)
 {
-   variable_entry *entry = this->get_variable_entry(ir);
-
-   if (entry)
-      entry->declaration = true;
+   /* Make sure splitting looks at splitting this variable */
+   (void)this->get_variable_entry(ir);
 
    return visit_continue;
 }
@@ -347,12 +342,12 @@ brw_do_vector_splitting(exec_list *instructions)
       variable_entry *entry = (variable_entry *)node;
 
       if (debug) {
-	 fprintf(stderr, "vector %s@%p: decl %d, whole_access %d\n",
-                 entry->var->name, (void *) entry->var, entry->declaration,
+	 fprintf(stderr, "vector %s@%p: whole_access %d\n",
+                 entry->var->name, (void *) entry->var,
                  entry->whole_vector_access);
       }
 
-      if (!entry->declaration || entry->whole_vector_access) {
+      if (entry->whole_vector_access) {
 	 entry->remove();
       }
    }
