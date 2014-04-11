86b8843e9c952a9cca8fd00c5187c64f5985ac05
Brian Paul <brianp@vmware.com>
2014-04-10 19:03:58 -0600

    softpipe: add PIPE_CAP_MIN/MAX_TEXTURE_GATHER_OFFSET query cases
    To silence compiler warnings.
    
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/drivers/softpipe/sp_screen.c
+++ src/gallium/drivers/softpipe/sp_screen.c
@@ -192,6 +192,9 @@ softpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
       return 0;
    case PIPE_CAP_FAKE_SW_MSAA:
       return 1;
+   case PIPE_CAP_MIN_TEXTURE_GATHER_OFFSET:
+   case PIPE_CAP_MAX_TEXTURE_GATHER_OFFSET:
+      return 0;
    }
    /* should only get here on unhandled cases */
    debug_printf("Unexpected PIPE_CAP %d query\n", param);
