b18f6b9b86531581da567eaa400923e35e22006d
Topi Pohjolainen <topi.pohjolainen@intel.com>
2014-04-19 17:11:10 +0300

    i965/fb: Use meta path for stencil blits
    This is effective only on gen8 for now as previous generations still
    go through blorp.
    
    Cc: "10.2" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Topi Pohjolainen <topi.pohjolainen@intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/intel_fbo.c
+++ src/mesa/drivers/dri/i965/intel_fbo.c
@@ -879,6 +879,15 @@ intel_blit_framebuffer(struct gl_context *ctx,
    if (mask == 0x0)
       return;
 
+   if (mask & GL_STENCIL_BUFFER_BIT) {
+      brw_meta_fbo_stencil_blit(brw_context(ctx),
+                                srcX0, srcY0, srcX1, srcY1,
+                                dstX0, dstY0, dstX1, dstY1);
+      mask &= ~GL_STENCIL_BUFFER_BIT;
+      if (mask == 0x0)
+         return;
+   }
+
    /* Try using the BLT engine. */
    mask = intel_blit_framebuffer_with_blitter(ctx,
                                               srcX0, srcY0, srcX1, srcY1,
