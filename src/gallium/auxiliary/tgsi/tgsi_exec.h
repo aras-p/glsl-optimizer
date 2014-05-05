9b1ae44ae114d0e2ee74e3e8d0ed0bd7ad983c9a
Brian Paul <brianp@vmware.com>
2014-05-05 10:18:49 -0600

    tgsi: add missing switch cases in tgsi_exec_get_shader_param()
    Add cases for PIPE_SHADER_CAP_MAX_SAMPLER_VIEWS and
    PIPE_SHADER_CAP_PREFERRED_IR.  Remove default switch case so we
    learn of missing cases at compile time.
    
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/gallium/auxiliary/tgsi/tgsi_exec.h
+++ src/gallium/auxiliary/tgsi/tgsi_exec.h
@@ -449,11 +449,17 @@ tgsi_exec_get_shader_param(enum pipe_shader_cap param)
       return 1;
    case PIPE_SHADER_CAP_MAX_TEXTURE_SAMPLERS:
       return PIPE_MAX_SAMPLERS;
+   case PIPE_SHADER_CAP_MAX_SAMPLER_VIEWS:
+      return PIPE_MAX_SHADER_SAMPLER_VIEWS;
+   case PIPE_SHADER_CAP_PREFERRED_IR:
+      return PIPE_SHADER_IR_TGSI;
    case PIPE_SHADER_CAP_TGSI_SQRT_SUPPORTED:
       return 1;
-   default:
-      return 0;
    }
+   /* if we get here, we missed a shader cap above (and should have seen
+    * a compiler warning.)
+    */
+   return 0;
 }
 
 #if defined __cplusplus
