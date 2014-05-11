83b4ec03e77645c537d540a12253b3492bad4b59
Rob Clark <robclark@freedesktop.org>
2014-05-11 08:57:38 -0400

    freedreno/a2xx: fix compiler warning
    Signed-off-by: Rob Clark <robclark@freedesktop.org>

--- src/gallium/drivers/freedreno/a2xx/fd2_emit.c
+++ src/gallium/drivers/freedreno/a2xx/fd2_emit.c
@@ -125,7 +125,7 @@ emit_texture(struct fd_ringbuffer *ring, struct fd_context *ctx,
 {
 	unsigned const_idx = fd2_get_const_idx(ctx, tex, samp_id);
 	static const struct fd2_sampler_stateobj dummy_sampler = {};
-	struct fd2_sampler_stateobj *sampler;
+	const struct fd2_sampler_stateobj *sampler;
 	struct fd2_pipe_sampler_view *view;
 
 	if (emitted & (1 << const_idx))
