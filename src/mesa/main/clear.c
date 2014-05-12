7928b946adbcbbb835c0080967bbb538f6bd35dc
Kristian Høgsberg <krh@bitplanet.net>
2014-05-12 15:46:11 -0700

    mesa: Remove glClear optimization based on drawable size
    A drawable size of 0x0 means that we don't have buffers for a drawable yet,
    not that we have a zero-sized buffer.  Core mesa shouldn't be optimizing out
    drawing based on buffer size, since the draw call could be what triggers
    the driver to go and get buffers.  As discussed in the referenced bug report,
    the optimization was added as part of a scatter-shot attempt to fix a
    different problem.  There's no other example in mesa core of using the
    buffer size in this way.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=74005
    Cc: "10.1 10.2" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Kristian Høgsberg <krh@bitplanet.net>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/mesa/main/clear.c
+++ src/mesa/main/clear.c
@@ -180,11 +180,6 @@ _mesa_Clear( GLbitfield mask )
       return;
    }
 
-   if (ctx->DrawBuffer->Width == 0 || ctx->DrawBuffer->Height == 0 ||
-       ctx->DrawBuffer->_Xmin >= ctx->DrawBuffer->_Xmax ||
-       ctx->DrawBuffer->_Ymin >= ctx->DrawBuffer->_Ymax)
-      return;
-
    if (ctx->RasterDiscard)
       return;
 
