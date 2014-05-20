8a1a4855cff206f01645d83f3c19c116156f5879
Chris Forbes <chrisf@ijw.co.nz>
2014-05-20 21:28:21 +1200

    mesa: Add extension enable for ARB_compressed_texture_pixel_storage
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/mesa/main/extensions.c
+++ src/mesa/main/extensions.c
@@ -86,6 +86,7 @@ static const struct extension extension_table[] = {
    { "GL_ARB_buffer_storage",                      o(ARB_buffer_storage),                      GL,             2013 },
    { "GL_ARB_clear_buffer_object",                 o(dummy_true),                              GL,             2012 },
    { "GL_ARB_color_buffer_float",                  o(ARB_color_buffer_float),                  GL,             2004 },
+   { "GL_ARB_compressed_texture_pixel_storage",    o(dummy_true),                              GL,             2011 },
    { "GL_ARB_compute_shader",                      o(ARB_compute_shader),                      GL,             2012 },
    { "GL_ARB_copy_buffer",                         o(dummy_true),                              GL,             2008 },
    { "GL_ARB_conservative_depth",                  o(ARB_conservative_depth),                  GL,             2011 },
