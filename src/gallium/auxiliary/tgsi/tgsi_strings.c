cdeb7004e03afbd5305913b5ae35d72529623507
Ilia Mirkin <imirkin@alum.mit.edu>
2014-05-19 23:54:40 -0400

    tgsi: add GS_INVOCATIONS to property names array
    In commit 4be146b1, I neglected to add the new property to the strings
    array. This leads to the string '(null)' to be printed instead when
    converting a GS shader to text.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Cc: "10.2" <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>

--- src/gallium/auxiliary/tgsi/tgsi_strings.c
+++ src/gallium/auxiliary/tgsi/tgsi_strings.c
@@ -120,7 +120,8 @@ const char *tgsi_property_names[TGSI_PROPERTY_COUNT] =
    "FS_COORD_PIXEL_CENTER",
    "FS_COLOR0_WRITES_ALL_CBUFS",
    "FS_DEPTH_LAYOUT",
-   "VS_PROHIBIT_UCPS"
+   "VS_PROHIBIT_UCPS",
+   "GS_INVOCATIONS",
 };
 
 const char *tgsi_type_names[5] =
