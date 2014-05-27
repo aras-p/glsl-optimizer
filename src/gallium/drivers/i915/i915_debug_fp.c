328e7e77424e9019b7c978fdc78da723fe079102
Stéphane Marchesin <marcheu@chromium.org>
2014-05-26 22:12:17 -0700

    i915g: Fix shader disasm code
    This broke when I separated declarations/shader.

--- src/gallium/drivers/i915/i915_debug_fp.c
+++ src/gallium/drivers/i915/i915_debug_fp.c
@@ -339,7 +339,6 @@ i915_disassemble_program(struct debug_stream *stream,
 
    assert((program[0] & 0x1ff) + 2 == sz);
 
-   program++;
    for (i = 1; i < sz; i += 3, program += 3) {
       unsigned opcode = program[0] & (0x1f << 24);
 
