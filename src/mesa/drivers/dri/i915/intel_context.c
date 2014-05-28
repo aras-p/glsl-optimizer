90b5747856dd116e1a7e0fd4a93781400f276c3e
Lubomir Rintel <lkundrak@v3.sk>
2014-05-28 08:56:12 +0200

    i915: add a missing NULL pointer check
    mesaVisual can be NULL with configless context since this commit:
    
        commit 551d459af421a2eb937e9e16301bb64da4624f89
        Author: Neil Roberts <neil@linux.intel.com>
        Date:   Fri Mar 7 18:05:47 2014 +0000
    
        Add the EGL_MESA_configless_context extension
    ...
        Previously the i965 and i915 drivers were explicitly creating a zeroed visual
        whenever 0 is passed for the EGLConfig.
    
    We attempt to dereference the visual in i915 and now we don't create a
    zeroed-out one one it crashes, breaking at least weston in an i915. There's
    no point in doing so as it would be zero anyway.
    
    v2: Fixed a typo in commit message.  Added some tags.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1100967
    Cc: "10.2" <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/mesa/drivers/dri/i915/intel_context.c
+++ src/mesa/drivers/dri/i915/intel_context.c
@@ -507,7 +507,7 @@ intelInitContext(struct intel_context *intel,
 
    _mesa_meta_init(ctx);
 
-   intel->hw_stencil = mesaVis->stencilBits && mesaVis->depthBits == 24;
+   intel->hw_stencil = mesaVis && mesaVis->stencilBits && mesaVis->depthBits == 24;
    intel->hw_stipple = 1;
 
    intel->RenderIndex = ~0;
