bbaec0f76c714a369014a47cca588fa3c2c9ce1e
Connor Abbott <cwabbott0@gmail.com>
2014-05-27 21:23:02 -0400

    glsl: be more consistent about printing constants
    Make sure that we print the same number of digits when printing 0.0 as
    any other floating-point number. This will make generating expected
    output files for tests easier. To avoid breaking "make check," update
    the generated tests for lower_jumps before the next commit which will
    bring create_test_cases.py in line with them.
    
    Signed-off-by: Connor Abbott <cwabbott0@gmail.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- src/glsl/ir_print_visitor.cpp
+++ src/glsl/ir_print_visitor.cpp
@@ -430,7 +430,7 @@ void ir_print_visitor::visit(ir_constant *ir)
 	 case GLSL_TYPE_FLOAT:
             if (ir->value.f[i] == 0.0f)
                /* 0.0 == -0.0, so print with %f to get the proper sign. */
-               fprintf(f, "%.1f", ir->value.f[i]);
+               fprintf(f, "%f", ir->value.f[i]);
             else if (fabs(ir->value.f[i]) < 0.000001f)
                fprintf(f, "%a", ir->value.f[i]);
             else if (fabs(ir->value.f[i]) > 1000000.0f)
