12029046a276ff6903664a0d0771d85279386360
Jordan Justen <jordan.l.justen@intel.com>
2014-06-09 13:40:01 -0700

    main/cs: Add additional compute shader constant values
    With MESA_EXTENSION_OVERRIDE=GL_ARB_compute_shader, this fixes piglit:
    * arb_compute_shader-minmax
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Chris Forbes <chrisf@ijw.co.nz>

--- src/mesa/main/config.h
+++ src/mesa/main/config.h
@@ -289,6 +289,17 @@
 #define PERFQUERY_HAVE_GPA_EXTENDED_COUNTERS 0
 /*@}*/
 
+/** For GL_ARB_compute_shader */
+/*@{*/
+#define MAX_COMPUTE_UNIFORM_BLOCKS          12
+#define MAX_COMPUTE_TEXTURE_IMAGE_UNITS     16
+#define MAX_COMPUTE_ATOMIC_COUNTER_BUFFERS  8
+#define MAX_COMPUTE_ATOMIC_COUNTERS         8
+#define MAX_COMPUTE_SHARED_MEMORY_SIZE      32768
+#define MAX_COMPUTE_UNIFORM_COMPONENTS      512
+#define MAX_COMPUTE_IMAGE_UNIFORMS          8
+/*@}*/
+
 /*
  * Color channel component order
  * 
