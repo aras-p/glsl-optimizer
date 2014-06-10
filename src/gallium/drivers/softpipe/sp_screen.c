c82d227edd9c2723c8fe6bfb2e5983e8173608e4
Dave Airlie <airlied@redhat.com>
2014-06-10 14:19:18 +1000

    softpipe: bump the softpipe geometry limits
    This just aligns the limits with llvmpipe.
    
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/gallium/drivers/softpipe/sp_screen.c
+++ src/gallium/drivers/softpipe/sp_screen.c
@@ -119,7 +119,7 @@ softpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
       return 16*4;
    case PIPE_CAP_MAX_GEOMETRY_OUTPUT_VERTICES:
    case PIPE_CAP_MAX_GEOMETRY_TOTAL_OUTPUT_COMPONENTS:
-      return 0;
+      return 1024;
    case PIPE_CAP_PRIMITIVE_RESTART:
       return 1;
    case PIPE_CAP_SHADER_STENCIL_EXPORT:
