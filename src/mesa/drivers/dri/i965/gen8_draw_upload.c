7f256c1c705e74d626a668bdfc2b281167a957c2
Kenneth Graunke <kenneth@whitecape.org>
2014-06-14 01:43:26 -0700

    i965: Add missing MOCS setup for 3DSTATE_INDEX_BUFFER on Broadwell.
    Somehow I missed this when adding all of the other MOCS values.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Cc: "10.2" <mesa-stable@lists.freedesktop.org>

--- src/mesa/drivers/dri/i965/gen8_draw_upload.c
+++ src/mesa/drivers/dri/i965/gen8_draw_upload.c
@@ -213,11 +213,9 @@ gen8_emit_index_buffer(struct brw_context *brw)
    if (index_buffer == NULL)
       return;
 
-   perf_debug("Missing MOCS setup for 3DSTATE_INDEX_BUFFER.");
-
    BEGIN_BATCH(5);
    OUT_BATCH(CMD_INDEX_BUFFER << 16 | (5 - 2));
-   OUT_BATCH(brw_get_index_type(index_buffer->type) << 8);
+   OUT_BATCH(brw_get_index_type(index_buffer->type) << 8 | BDW_MOCS_WB);
    OUT_RELOC64(brw->ib.bo, I915_GEM_DOMAIN_VERTEX, 0, 0);
    OUT_BATCH(brw->ib.bo->size);
    ADVANCE_BATCH();
