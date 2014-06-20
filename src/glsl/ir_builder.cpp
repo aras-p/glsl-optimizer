cbd0d643a30901dabc5b3cc84121bafe16f8fc6f
Abdiel Janulgue <abdiel.janulgue@linux.intel.com>
2014-06-20 16:55:03 -0700

    glsl: Implement saturate as ir_unop_saturate
    Now that we have the ir_unop_saturate implemented as a single
    instruction, generate the correct simplified expression.
    
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Signed-off-by: Abdiel Janulgue <abdiel.janulgue@linux.intel.com>

--- src/glsl/ir_builder.cpp
+++ src/glsl/ir_builder.cpp
@@ -271,11 +271,7 @@ clamp(operand a, operand b, operand c)
 ir_expression *
 saturate(operand a)
 {
-   void *mem_ctx = ralloc_parent(a.val);
-
-   return expr(ir_binop_max,
-	       expr(ir_binop_min, a, new(mem_ctx) ir_constant(1.0f)),
-	       new(mem_ctx) ir_constant(0.0f));
+   return expr(ir_unop_saturate, a);
 }
 
 ir_expression *
