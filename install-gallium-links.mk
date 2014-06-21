291d70210dd249c663f8c3b92ac9cf47335630bd
Emil Velikov <emil.l.velikov@gmail.com>
2014-06-21 12:31:47 +0100

    targets/radeonsi/vdpau: convert to static/shared pipe-drivers
    Similar to previous commits, this allows us to minimise some
    of the duplication by compacting all vdpau targets into a
    single library.
    
    v2: Include the radeon winsys only when there is a user for it.
    v3: Correcly include the winsys. Now with extra brown bag :\
    
    Cc: Christian König <christian.koenig@amd.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Tested-by: Thomas Helland <thomashelland90 at gmail.com>

--- install-gallium-links.mk
+++ install-gallium-links.mk
@@ -5,7 +5,7 @@ if BUILD_SHARED
 if HAVE_COMPAT_SYMLINKS
 all-local : .libs/install-gallium-links
 
-.libs/install-gallium-links : $(dri_LTLIBRARIES) $(vdpau_LTLIBRARIES) $(egl_LTLIBRARIES) $(lib_LTLIBRARIES)
+.libs/install-gallium-links : $(dri_LTLIBRARIES) $(egl_LTLIBRARIES) $(lib_LTLIBRARIES)
 	$(AM_V_GEN)$(MKDIR_P) $(top_builddir)/$(LIB_DIR);	\
 	link_dir=$(top_builddir)/$(LIB_DIR)/gallium;		\
 	if test x$(egl_LTLIBRARIES) != x; then			\
@@ -13,7 +13,6 @@ all-local : .libs/install-gallium-links
 	fi;							\
 	$(MKDIR_P) $$link_dir;					\
 	file_list=$(dri_LTLIBRARIES:%.la=.libs/%.so);		\
-	file_list+=$(vdpau_LTLIBRARIES:%.la=.libs/%.$(LIB_EXT)*); \
 	file_list+=$(egl_LTLIBRARIES:%.la=.libs/%.$(LIB_EXT)*);	\
 	file_list+=$(lib_LTLIBRARIES:%.la=.libs/%.$(LIB_EXT)*);	\
 	for f in $$file_list; do 				\
