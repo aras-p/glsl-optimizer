27ee7df8ad16b951d8c54a617a422797767c9242
Ilia Mirkin <imirkin@alum.mit.edu>
2014-07-01 20:05:09 -0400

    nvc0: add missed PIPE_CAP_DRAW_INDIRECT
    Real support will be forthcoming. For now, avoid the unknown cap error
    and compiler warning.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -183,6 +183,7 @@ nvc0_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_TGSI_VS_LAYER:
    case PIPE_CAP_FAKE_SW_MSAA:
    case PIPE_CAP_TGSI_VS_WINDOW_SPACE_POSITION:
+   case PIPE_CAP_DRAW_INDIRECT:
       return 0;
    }
 
