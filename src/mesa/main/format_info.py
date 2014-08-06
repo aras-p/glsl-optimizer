84218b598f429bcc4474aa620da906b7b9aadc2b
Neil Roberts <neil@linux.intel.com>
2014-08-06 16:52:14 +0100

    mesa/format_info: Add support for compressed floating-point formats
    If the name of a compressed texture format has ‘FLOAT’ in it it will now set
    the data type of the format to GL_FLOAT. This will be needed for the BPTC
    half-float formats.
    
    Reviewed-by: Jason Ekstrand <jason.ekstrand@intel.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

--- src/mesa/main/format_info.py
+++ src/mesa/main/format_info.py
@@ -62,7 +62,9 @@ def get_gl_base_format(fmat):
 
 def get_gl_data_type(fmat):
    if fmat.is_compressed():
-      if 'SIGNED' in fmat.name or 'SNORM' in fmat.name:
+      if 'FLOAT' in fmat.name:
+         return 'GL_FLOAT'
+      elif 'SIGNED' in fmat.name or 'SNORM' in fmat.name:
          return 'GL_SIGNED_NORMALIZED'
       else:
          return 'GL_UNSIGNED_NORMALIZED'
