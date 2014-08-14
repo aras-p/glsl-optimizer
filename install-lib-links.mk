4ccd2a9f9b0813887716d38321829175459665e0
Matt Turner <mattst88@gmail.com>
2014-08-14 16:07:26 -0700

    build: Let install-lib-links.mk handle .la files in subdirectories.
    The next patches are going to combine some of the mapi subdirectories'
    Makefiles into a single Makefile, giving better build parallelism.
    
    lib_LTLIBRARIES will be set to something like
    
       lib_LTLIBRARIES = shared-glapi/libglapi.la es2api/libGLESv2.la
    
    and the current code in install-lib-links.mk simply prepends .libs/ and
    replaces the .la in order to create the filenames that it needs to ln/cp
    into the LIBDIR. This doesn't work when the .la file is actually in a
    subdirectory.
    
    This patch fixes this and puts .libs/ in the right place.
    
    Reviewed-by: Kristian Høgsberg <krh@bitplanet.net>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>

--- install-lib-links.mk
+++ install-lib-links.mk
@@ -7,7 +7,7 @@ all-local : .libs/install-mesa-links
 
 .libs/install-mesa-links : $(lib_LTLIBRARIES)
 	$(AM_V_GEN)$(MKDIR_P) $(top_builddir)/$(LIB_DIR);	\
-	for f in $(lib_LTLIBRARIES:%.la=.libs/%.$(LIB_EXT)*); do \
+	for f in $(join $(addsuffix .libs/,$(dir $(lib_LTLIBRARIES))),$(notdir $(lib_LTLIBRARIES:%.la=%.$(LIB_EXT)*))); do \
 		if test -h .libs/$$f; then			\
 			cp -d $$f $(top_builddir)/$(LIB_DIR);	\
 		else						\
