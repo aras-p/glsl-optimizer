8db7dacf29d1b87246d1f29696e34b1854a75bba
Marek Olšák <marek.olsak@amd.com>
2014-08-17 01:36:57 +0200

    rbug: only add textures to the list
    rbug-gui cannot display buffers, so it's pointless to add them.
    
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>

--- src/gallium/drivers/rbug/rbug_objects.c
+++ src/gallium/drivers/rbug/rbug_objects.c
@@ -58,7 +58,8 @@ rbug_resource_create(struct rbug_screen *rb_screen,
    rb_resource->base.screen = &rb_screen->base;
    rb_resource->resource = resource;
 
-   rbug_screen_add_to_list(rb_screen, resources, rb_resource);
+   if (resource->target != PIPE_BUFFER)
+      rbug_screen_add_to_list(rb_screen, resources, rb_resource);
 
    return &rb_resource->base;
 
@@ -71,7 +72,9 @@ void
 rbug_resource_destroy(struct rbug_resource *rb_resource)
 {
    struct rbug_screen *rb_screen = rbug_screen(rb_resource->base.screen);
-   rbug_screen_remove_from_list(rb_screen, resources, rb_resource);
+
+   if (rb_resource->base.target != PIPE_BUFFER)
+      rbug_screen_remove_from_list(rb_screen, resources, rb_resource);
 
    pipe_resource_reference(&rb_resource->resource, NULL);
    FREE(rb_resource);
