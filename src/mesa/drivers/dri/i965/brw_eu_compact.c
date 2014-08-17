45eb06566830eb49551c27262cdd936bf42ec922
Matt Turner <mattst88@gmail.com>
2014-08-17 00:45:27 -0700

    i965: Enable instruction compaction on Gen8+.
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/brw_eu_compact.c
+++ src/mesa/drivers/dri/i965/brw_eu_compact.c
@@ -1099,7 +1099,7 @@ brw_compact_instructions(struct brw_compile *p, int start_offset,
     */
    int old_ip[(p->next_insn_offset - start_offset) / 8];
 
-   if (brw->gen < 6 || brw->gen >= 8)
+   if (brw->gen < 6)
       return;
 
    int src_offset;
