8bd67231797e5d79d72a4e91b37ea81da30c6df3
Marek Olšák <marek.olsak@amd.com>
2014-09-04 15:24:45 +0200

    Revert "r600g,radeonsi: initialize HTILE to fully-expanded state"
    This reverts commit f05fe294e7e8dfb08be172f426252192c0ba17ab.
    
    Apparently the hw doesn't like this. Revert to the "cleared" state.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=83418

--- src/gallium/drivers/radeon/r600_texture.c
+++ src/gallium/drivers/radeon/r600_texture.c
@@ -559,9 +559,7 @@ static void r600_texture_allocate_htile(struct r600_common_screen *rscreen,
 		 * without htile buffer */
 		R600_ERR("Failed to create buffer object for htile buffer.\n");
 	} else {
-		/* Clear to fully-expanded state. */
-		r600_screen_clear_buffer(rscreen, &rtex->htile_buffer->b.b, 0,
-					 htile_size, 0xffffffff);
+		r600_screen_clear_buffer(rscreen, &rtex->htile_buffer->b.b, 0, htile_size, 0);
 	}
 }
 
