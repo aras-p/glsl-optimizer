5cf8d9f54b2e27721f8c79e553377834b3005022
Brian Paul <brianp@vmware.com>
2014-09-10 16:37:24 -0600

    u_vbuf: simple whitespace fix

--- src/gallium/auxiliary/util/u_vbuf.c
+++ src/gallium/auxiliary/util/u_vbuf.c
@@ -357,7 +357,8 @@ u_vbuf_set_vertex_elements_internal(struct u_vbuf *mgr, unsigned count,
    assert(ve);
 
    if (ve != mgr->ve)
-	   pipe->bind_vertex_elements_state(pipe, ve->driver_cso);
+      pipe->bind_vertex_elements_state(pipe, ve->driver_cso);
+
    return ve;
 }
 
