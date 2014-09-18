711623f7c8113d43f2d54ebfe5cbed3d406a3c79
Marek Olšák <marek.olsak@amd.com>
2014-09-18 21:40:02 +0200

    radeonsi: release GS rings at context destruction
    Cc: 10.2 10.3 <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -38,6 +38,8 @@ static void si_destroy_context(struct pipe_context *context)
 
 	si_release_all_descriptors(sctx);
 
+	pipe_resource_reference(&sctx->esgs_ring, NULL);
+	pipe_resource_reference(&sctx->gsvs_ring, NULL);
 	pipe_resource_reference(&sctx->null_const_buf.buffer, NULL);
 	r600_resource_reference(&sctx->border_color_table, NULL);
 
