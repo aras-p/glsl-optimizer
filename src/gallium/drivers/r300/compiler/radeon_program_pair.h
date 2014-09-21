e8053bb65e59ead80f9d6374c6b0927861da931f
Emil Velikov <emil.l.velikov@gmail.com>
2014-09-21 04:00:07 +0100

    gallium/r300: remove obsolete declaration
    The definition of rc_pair_regalloc_inputs_only() is no longer
    around so drop the declaration.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Acked-by: Matt Turner <mattst88@gmail.com>

--- src/gallium/drivers/r300/compiler/radeon_program_pair.h
+++ src/gallium/drivers/r300/compiler/radeon_program_pair.h
@@ -132,7 +132,6 @@ struct radeon_pair_handler;
 void rc_pair_translate(struct radeon_compiler *cc, void *user);
 void rc_pair_schedule(struct radeon_compiler *cc, void *user);
 void rc_pair_regalloc(struct radeon_compiler *cc, void *user);
-void rc_pair_regalloc_inputs_only(struct radeon_compiler *cc, void *user);
 void rc_pair_remove_dead_sources(struct radeon_compiler *c, void *user);
 /*@}*/
 
