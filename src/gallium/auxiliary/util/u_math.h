af4f5a7c97f3ab47bbd21aa7c27d0c0aaf01f959
Marek Olšák <marek.olsak@amd.com>
2014-09-24 18:26:21 +0200

    gallium/util: add util_bitcount64
    I'll need this in radeonsi.
    
    v2: use __builtin_popcountll if available
    
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>

--- src/gallium/auxiliary/util/u_math.h
+++ src/gallium/auxiliary/util/u_math.h
@@ -727,6 +727,18 @@ util_bitcount(unsigned n)
 #endif
 }
 
+
+static INLINE unsigned
+util_bitcount64(uint64_t n)
+{
+#ifdef HAVE___BUILTIN_POPCOUNTLL
+   return __builtin_popcountll(n);
+#else
+   return util_bitcount(n) + util_bitcount(n >> 32);
+#endif
+}
+
+
 /**
  * Reverse bits in n
  * Algorithm taken from:
