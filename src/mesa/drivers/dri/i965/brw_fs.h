b59db8e0f0f424b91b88e0814c8dd96e6695da74
Matt Turner <mattst88@gmail.com>
2014-09-30 23:18:34 -0700

    i965/fs: Remove dead generate_rep_fb_write prototype.
    Added in commit f9dc7aab.

--- src/mesa/drivers/dri/i965/brw_fs.h
+++ src/mesa/drivers/dri/i965/brw_fs.h
@@ -691,7 +691,6 @@ private:
                       GLuint nr);
    void generate_fb_write(fs_inst *inst, struct brw_reg payload);
    void generate_blorp_fb_write(fs_inst *inst);
-   void generate_rep_fb_write(fs_inst *inst);
    void generate_pixel_xy(struct brw_reg dst, bool is_x);
    void generate_linterp(fs_inst *inst, struct brw_reg dst,
 			 struct brw_reg *src);
