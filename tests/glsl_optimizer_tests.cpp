812936bfee5da23a7dac5b9f9f661cb89889874c
Marco Pompili <marcs.pompili@gmail.com>
2015-12-08 22:07:29 +0100

    tests: compilation directives for CheckMetal

--- tests/glsl_optimizer_tests.cpp
+++ tests/glsl_optimizer_tests.cpp
@@ -312,6 +312,7 @@ static bool CheckGLSL (bool vertex, bool gles, const std::string& testName, cons
 	return res;
 }
 
+#ifdef __APPLE__
 
 static bool CheckMetal (bool vertex, bool gles, const std::string& testName, const char* prefix, const std::string& source)
 {
@@ -332,6 +333,7 @@ static bool CheckMetal (bool vertex, bool gles, const std::string& testName, con
 	return true;
 }
 
+#endif
 
 static bool ReadStringFromFile (const char* pathName, std::string& output)
 {
@@ -579,8 +581,12 @@ static bool TestFile (glslopt_ctx* ctx, bool vertex,
 		std::string outputOpt;
 		ReadStringFromFile (outputPath.c_str(), outputOpt);
 
+#		ifdef __APPLE__
+
 		if (res && doCheckMetal && !CheckMetal (vertex, gles, testName, "metal", textOpt.c_str()))
 			res = false;
+
+#		endif
 		
 		if (textOpt != outputOpt)
 		{
