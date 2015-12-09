6b8e99d35e26a317eba62fbc3e4fa797e01a63a4
Marco Pompili <marcs.pompili@gmail.com>
2015-12-09 01:19:29 +0100

    compatible with nodejs v0.12, v4, v5 - nan 2.1.0

--- src/node/binding.cpp
+++ src/node/binding.cpp
@@ -1,22 +1,48 @@
 #include <node.h>
+#include <node_version.h>
 #include <nan.h>
 #include "shader.h"
 #include <glsl_optimizer.h>
 
 using namespace v8;
 
-void InitAll(Handle<Object> exports)
+void InitAll(Local<Object> exports)
 {
-	// Export constants
-	exports->Set(String::NewSymbol("VERTEX_SHADER"), Int32::New(kGlslOptShaderVertex), ReadOnly);
-	exports->Set(String::NewSymbol("FRAGMENT_SHADER"), Int32::New(kGlslOptShaderFragment), ReadOnly);
-	exports->Set(String::NewSymbol("TARGET_OPENGL"), Int32::New(kGlslTargetOpenGL), ReadOnly);
-	exports->Set(String::NewSymbol("TARGET_OPENGLES20"), Int32::New(kGlslTargetOpenGLES20), ReadOnly);
-	exports->Set(String::NewSymbol("TARGET_OPENGLES30"), Int32::New(kGlslTargetOpenGLES30), ReadOnly);
-	
+	// Export constants on node v4 and v5
+#if defined(NODE_MAJOR_VERSION) && (NODE_MAJOR_VERSION > 4)
+	Isolate *isolate = exports->GetIsolate();
+
+	Local<Context> ctx = isolate->GetCurrentContext();
+
+	exports->CreateDataProperty(ctx, String::NewFromUtf8(isolate, "VERTEX_SHADER"), Int32::New(isolate, kGlslOptShaderVertex));
+	exports->CreateDataProperty(ctx, String::NewFromUtf8(isolate, "FRAGMENT_SHADER"), Int32::New(isolate, kGlslOptShaderFragment));
+	exports->CreateDataProperty(ctx, String::NewFromUtf8(isolate, "TARGET_OPENGL"), Int32::New(isolate, kGlslTargetOpenGL));
+	exports->CreateDataProperty(ctx, String::NewFromUtf8(isolate, "TARGET_OPENGLES20"), Int32::New(isolate, kGlslTargetOpenGLES20));
+	exports->CreateDataProperty(ctx, String::NewFromUtf8(isolate, "TARGET_OPENGLES30"), Int32::New(isolate, kGlslTargetOpenGLES30));
+
+#else
+	// Export constants on node v0.12
+	Isolate *isolate = Isolate::GetCurrent();
+
+	//TODO need to set these as read only properties
+	exports->Set(String::NewFromUtf8(isolate, "VERTEX_SHADER"), Int32::New(isolate, kGlslOptShaderVertex));
+	exports->Set(String::NewFromUtf8(isolate, "FRAGMENT_SHADER"), Int32::New(isolate, kGlslOptShaderFragment));
+	exports->Set(String::NewFromUtf8(isolate, "TARGET_OPENGL"), Int32::New(isolate, kGlslTargetOpenGL));
+	exports->Set(String::NewFromUtf8(isolate, "TARGET_OPENGLES20"), Int32::New(isolate, kGlslTargetOpenGLES20));
+	exports->Set(String::NewFromUtf8(isolate, "TARGET_OPENGLES30"), Int32::New(isolate, kGlslTargetOpenGLES30));
+#endif
+
 	// Export classes
 	Compiler::Init(exports);
 	Shader::Init(exports);
 }
+
+/*
+NAN_MODULE_INIT(InitAll)
+{
+	Compiler::Init(target);
+	Shader::Init(target);
+}
+*/
 
 NODE_MODULE(glslOptimizer, InitAll);
